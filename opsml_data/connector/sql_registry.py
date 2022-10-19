3a29062ea3cd7b81ecc80ada5bfc5fb2406a259e
Thorrester <48217609+thorrester@users.noreply.github.com>
2022-10-19 11:56:22 -0400

    refactoring and adding gcs functionality

--- opsml_data/connector/sql_registry.py
+++ opsml_data/connector/sql_registry.py
@@ -1,12 +1,20 @@
 from ast import Raise
 from re import S
 from .data_model import DataModel, SqlDataRegistrySchema, base
+from opsml_data.helpers.utils import GCPCredentials
+from opsml_data.helpers.defaults import defaults
 from .data_formatter import DataFormatter
-from sqlalchemy import select, desc
+from google.cloud.sql.connector import Connector, IPTypes
+import pymysql
+import os
 import sqlalchemy
+from sqlalchemy import select, desc
 import pandas as pd
 import numpy as np
 from typing import Union, Dict, Any
+from pyshipt_logging.logger import ShiptLogging
+
+logger = ShiptLogging.get_default_logger()
 
 
 class TableRegistry:
@@ -18,6 +26,55 @@ class TableRegistry:
 
         # return default
         return SqlDataRegistrySchema
+
+
+class GCPSqlConnector:
+    def __init__(
+        self,
+        instance_name: str,
+        db_name: str,
+        username: str,
+        password: str,
+        gcp_project: str = defaults.GCP_PROJECT,
+        gcp_region: str = defaults.GCP_REGION,
+    ):
+        self._instance_connection_name = f"{gcp_project}:{gcp_region}:{instance_name}"
+        self._db_name = db_name
+        self._username = username
+        self._password = password
+
+        ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+        creds = GCPCredentials(gcp_creds=defaults.GCP_CREDS)
+
+        self.connector = self._set_connector(
+            ip_type=ip_type,
+            creds=creds,
+        )
+
+    def _set_connector(self, ip_type, creds: GCPCredentials):
+        return Connector(
+            ip_type=ip_type,
+            credentials=creds.credentials,
+        )
+
+    def _sql_conn(self) -> pymysql.connections.Connection:
+        conn: pymysql.connections.Connection = self.connector.connect(
+            self._instance_connection_name,
+            "pymysql",
+            user=self._username,
+            password=self._password,
+            db=self._db_name,
+        )
+
+        return conn
+
+    def create_sql_engine(self) -> sqlalchemy.engine.base.Engine:
+        engine = sqlalchemy.create_engine(
+            "mysql+pymysql://",
+            creator=self._sql_conn,
+        )
+        logger.info("Connected to db")
+        return engine
 
 
 class SqlRegistry:
@@ -44,7 +101,37 @@ class SqlRegistry:
             self.schema.__table__.insert(data),
         )
 
-    def write_to_registry(self, data: Union[pd.DataFrame, np.array], table_name: str):
+    def max_table_version(self, table_name: str):
+        sql_statement = (
+            select(self.schema.version)
+            .where(self.schema.table_name == table_name)
+            .order_by(desc(self.schema.version))
+            .limit(1)
+        )
+
+        result = self.submit_query(sql_statement).scalar()
+
+        if result is None:
+            return 0
+
+        return result
+
+    def write_to_registry(
+        self,
+        data: Union[pd.DataFrame, np.array],
+        table_name: str,
+        user_email: str,
+    ):
+
+        max_version = self.max_table_version(
+            table_name=table_name,
+        )
+        data = DataFormatter.convert_data_to_arrow(data)
+        feature_map = DataFormatter.create_table_schema(data)
+
+        # save data to gcs
+
+        model = DataModel(**data)
 
         # convert data
         data = DataFormatter.convert_data_to_arrow(data)
--- opsml_data/helpers/utils.py
+++ opsml_data/helpers/utils.py
@@ -1,7 +1,13 @@
 import base64
 import json
+from turtle import st
 from google.oauth2 import service_account
 from functools import cached_property
+from .defaults import defaults
+from pyshipt_logging import ShiptLogging
+from google.cloud import storage
+
+logger = ShiptLogging.get_logger(__name__)
 
 
 class GCPCredentials:
@@ -16,3 +22,90 @@ class GCPCredentials:
     @cached_property
     def credentials(self):
         return service_account.Credentials.from_service_account_info(self.key)
+
+
+class GCSStorageClient:
+    def __init__(self, gcs_bucket: str):
+        self.credentials = self.get_creds()
+        self.bucket = self.set_storage_bucket(gcs_bucket)
+
+    def get_creds(self):
+
+        if defaults.GCP_CREDS is not None:
+            base_64 = base64.b64decode(
+                defaults.GCP_CREDS,
+            )
+            key = json.loads(
+                base_64.decode("utf-8"),
+            )
+
+            return service_account.Credentials.from_service_account_info(  # noqa
+                key,
+            )
+
+        else:
+            return None
+
+    def list_objects(
+        self,
+        prefix: str = None,
+    ):
+        blobs = self.bucket.list_blobs(prefix=prefix)
+        blobs = [blob.name for blob in blobs]
+        return blobs
+
+    def set_storage_bucket(
+        self,
+        gcs_bucket: str = None,
+    ):
+        storage_client = storage.Client(
+            credentials=self.credentials,
+        )
+
+        bucket = storage_client.bucket(gcs_bucket)
+        if not bucket.exists():
+            bucket.create()
+
+        return bucket
+
+    def download_object(
+        self,
+        blob_path: str = None,
+        destination_filename: str = None,
+    ):
+        blob = self.bucket.blob(blob_path)
+        blob.download_to_filename(destination_filename)
+
+        logger.info(
+            f"Successully downloaded gs://{self.bucket.name}/{blob_path}",
+        )
+
+    def delete_object(
+        self,
+        blob_path: str = None,
+    ):
+        blob = self.bucket.blob(blob_path)
+        blob.delete()
+
+        logger.info(
+            f"Successully deleted gs://{self.bucket.name}/{blob_path}",
+        )
+
+    def parse_gcs_url(
+        self,
+        gcs_url: str,
+    ):
+        split_url = gcs_url.split("/")
+        bucket = split_url[2]
+        blob_path = "/".join(split_url[3:])
+
+        return bucket, blob_path
+
+    def delete_object_from_url(self, gcs_url: str):
+        bucket, blob_path = self.parse_gcs_url(
+            gcs_url,
+        )
+
+        self.delete_object(
+            blob_path,
+        )
