03fe4e5f5760ddf72fa30aaa6219c5d200e0623e
Thorrester <48217609+thorrester@users.noreply.github.com>
2023-01-07 23:14:57 -0500

    fixes

--- opsml_data/__init__.py
+++ opsml_data/__init__.py
@@ -1,6 +1,6 @@
 from opsml_data.connector.snowflake import SnowflakeQueryRunner
-from opsml_data.registry.data_registry import DataCard, DataRegistry
 from opsml_data.drift import DriftDetector, DriftVisualizer
+from opsml_data.registry.data_registry import DataCard, DataRegistry
 
 __all__ = [
     "DataRegistry",
