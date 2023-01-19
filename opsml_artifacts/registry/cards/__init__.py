a375c54a9aed003d0185b8dc1543e118f478f994
Thorrester <48217609+thorrester@users.noreply.github.com>
2023-01-19 11:00:08 -0500

    Refactor for one user experience

--- opsml_artifacts/registry/cards/__init__.py
+++ opsml_artifacts/registry/cards/__init__.py
@@ -1,4 +1,3 @@
 from opsml_artifacts.registry.cards.cards import DataCard, ModelCard
-from opsml_artifacts.registry.model.creator import ModelCardCreator
 
-__all__ = ["DataCard", "ModelCard", "ModelCardCreator"]
+__all__ = ["DataCard", "ModelCard"]
