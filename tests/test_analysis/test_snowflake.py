98f90a85cbb3b419db9b9f08d2ae8e99023bba9e
thorrester <steven.forrester@shipt.com>
2023-01-31 03:02:10 +0000

    update

---
+++ tests/test_analysis/test_snowflake.py
@@ -0,0 +1,50 @@
+import time
+
+import pandas as pd
+
+from opsml_artifacts.connector.snowflake import SnowflakeQueryRunner
+
+
+# remove these once networking is configured
+def test_snowflake_query(test_query):
+
+    runner = SnowflakeQueryRunner()
+    response, query_id = runner.submit_query(query=test_query)
+
+    assert response.status_code == 200
+
+    response = runner.query_status(query_id=query_id)
+    finished = False
+    while not finished:
+        response = runner.query_status(
+            query_id=query_id,
+        )
+
+        status = response.json()["query_status"]
+
+        if status.lower() == "success":
+            break
+
+        if "error" in status.lower():
+            message = response.json()["message"]
+            raise ValueError(message)
+
+        time.sleep(2)
+
+
+def test_snowflake_query_df(test_query):
+
+    runner = SnowflakeQueryRunner()
+    df = runner.query_to_dataframe(query=test_query)
+    assert isinstance(df, pd.DataFrame)
+
+    runner = SnowflakeQueryRunner(on_vpn=True)
+    df = runner.query_to_dataframe(query=test_query)
+    assert isinstance(df, pd.DataFrame)
+
+
+def test_snowflake_file_df():
+
+    runner = SnowflakeQueryRunner()
+    df = runner.query_to_dataframe(sql_file="test_sql.sql")
+    assert isinstance(df, pd.DataFrame)
