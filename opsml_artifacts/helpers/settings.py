48b3aa40317f87987b4301c58c2574fabb2d34ad
thorrester <steven.forrester@shipt.com>
2023-02-07 21:48:23 +0000

    logging

--- opsml_artifacts/helpers/settings.py
+++ opsml_artifacts/helpers/settings.py
@@ -6,22 +6,21 @@ from typing import IO, Any
 
 from pythonjsonlogger.jsonlogger import JsonFormatter
 
+APP_ENV = os.getenv("APP_ENV", "development")
+
 
 class LogFormatter(JsonFormatter):
-    """Custom formattero"""
+    """Custom formatter"""
 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-        self.app_env = os.getenv("APP_ENV", "development")
-
-    def add_fields(self, log_record: dict[str, Any], record: logging.LogRecord, message_dict: dict[str, Any]) -> None:
+    def add_fields(self, log_record, record, message_dict):
+        super().add_fields(log_record, record, message_dict)
+        if not log_record.get("timestamp"):
+            log_record["timestamp"] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        log_record["app_env"] = APP_ENV
         if log_record.get("level"):
             log_record["level"] = log_record["level"].upper()
         else:
             log_record["level"] = record.levelname
-        super().add_fields(log_record, record, message_dict)
-        log_record.setdefault("timestamp", datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
-        log_record.setdefault("app_env", self.app_env)
 
 
 # credit to pyshipt-logging for implementation logic
@@ -40,7 +39,11 @@ class ArtifactLogger:
         stream: IO = sys.stdout,
     ):
         log = logging.getLogger(name)
-        log.addHandler(cls.get_handler(stream=stream))
+
+        # Add a new stream handler if the log is new
+        if len(log.handlers) == 0:
+            log.addHandler(cls.get_handler(stream=stream))
+
         log_level: int = logging.getLevelName("INFO")
         log.setLevel(log_level)
         log.propagate = False
