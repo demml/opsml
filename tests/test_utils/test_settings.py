c35691d2ab53ffbea633ceb6ed6f4363cea3c31c
thorrester <steven.forrester@shipt.com>
2023-03-07 11:53:06 +0000

    testing new settings
    Former-commit-id: f63d11e70d26b32042ed4dd4043ef6351241105e

--- tests/test_utils/test_settings.py
+++ tests/test_utils/test_settings.py
@@ -1,5 +1,16 @@
 from opsml_artifacts.helpers.settings import OpsmlSettings
 
 
-def test_default_settings():
+def test_default_local_settings():
     settings = OpsmlSettings()
+
+    assert settings.storage_client.__class__.__name__ == "LocalStorageClient"
+    local_client = settings.connection_client
+
+    assert local_client.__class__.__name__ == "LocalSQLConnection"
+
+
+def test_default_postgres_settings():
+    settings = OpsmlSettings()
+
+    assert settings.storage_client.__class__.__name__ == "LocalStorageClient"
