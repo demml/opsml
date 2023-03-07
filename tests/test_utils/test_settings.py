047524aa170d060628b1318c6fa9669447d12982
Thorrester <48217609+thorrester@users.noreply.github.com>
2023-03-07 16:30:05 -0500

    removing some google stuff/settings tests
    Former-commit-id: f33652e32d87f69b418329da31d94214bf1e80a0

--- tests/test_utils/test_settings.py
+++ tests/test_utils/test_settings.py
@@ -1,16 +1,28 @@
-from opsml_artifacts.helpers.settings import OpsmlSettings
+from opsml_artifacts.helpers.settings import DefaultSettings
+import os
 
 
 def test_default_local_settings():
-    settings = OpsmlSettings()
 
+    settings = DefaultSettings()
     assert settings.storage_client.__class__.__name__ == "LocalStorageClient"
     local_client = settings.connection_client
-
     assert local_client.__class__.__name__ == "LocalSQLConnection"
 
 
-def test_default_postgres_settings():
-    settings = OpsmlSettings()
+def test_default_http_settings(monkeypatch, mock_gcs_storage_response, mock_gcp_creds):
 
-    assert settings.storage_client.__class__.__name__ == "LocalStorageClient"
+    monkeypatch.setenv(name="OPSML_TRACKING_URL", value="https://fake_url.com")
+    settings = DefaultSettings()
+    assert settings.storage_client.__class__.__name__ == "GCSFSStorageClient"
+
+
+def test_default_postgres_settings(monkeypatch, mock_gcs_storage_response, mock_gcp_creds):
+    url = "postgresql+psycopg2://test_user:test_password@/ds-test-db?host=/cloudsql/test-project:test-region:test-connection"
+    monkeypatch.setenv(name="OPSML_TRACKING_URL", value=url)
+    monkeypatch.setenv(name="OPSML_STORAGE_URL", value="gs://opsml/tet")
+
+    settings = DefaultSettings()
+    assert settings.storage_client.__class__.__name__ == "GCSFSStorageClient"
+
+    assert settings.connection_client.__class__.__name__ == "CloudSqlPostgresql"
