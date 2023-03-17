a1ba0f7495fbdc9a621f77a959054e7a4f1e0ed5
thorrester <steven.forrester@shipt.com>
2023-03-17 18:04:19 +0000

    resolve_default_artifact_root and drone
    Former-commit-id: b7574dcbd10bf4b97b7d1756e3f73693afc2de95

--- opsml_artifacts/app/core/initialize_mlflow.py
+++ opsml_artifacts/app/core/initialize_mlflow.py
@@ -2,6 +2,7 @@
 import sys
 
 import mlflow
+from mlflow.utils.server_cli_utils import resolve_default_artifact_root
 
 from opsml_artifacts.app.core.config import MlFlowConfig
 from opsml_artifacts.helpers.logging import ArtifactLogger
@@ -37,9 +38,7 @@ def initialize_mlflow() -> None:
     registry_store_uri = backend_store_uri  # yes, this is correct. the registry store default is the backend_store_uri
     default_artifact_root = config.MLFLOW_SERVER_ARTIFACT_ROOT
 
-    default_artifact_root = mlflow.utils.server_cli_utils.resolve_default_artifact_root(
-        True, default_artifact_root, backend_store_uri
-    )
+    default_artifact_root = resolve_default_artifact_root(True, default_artifact_root, backend_store_uri)
     try:
         mlflow.server.handlers.initialize_backend_stores(backend_store_uri, registry_store_uri, default_artifact_root)
     except Exception as e:  # pylint: disable=broad-exception-caught
