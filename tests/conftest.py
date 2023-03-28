695fe5aca9535368400212d6bcd52e4de869d5e4
thorrester <sjforrester32@gmail.com>
2023-03-28 21:28:14 +0000

    finally

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,12 +32,16 @@ import joblib
 import pandas as pd
 
 # ml model packages and classes
-from sklearn.linear_model import LinearRegression
+from sklearn.datasets import fetch_openml
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_selection import SelectPercentile, chi2
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.ensemble import StackingRegressor
+from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 import lightgbm as lgb
 
@@ -585,6 +589,40 @@ def sklearn_pipeline() -> tuple[Pipeline, pd.DataFrame]:
     pipe = Pipeline([("preprocess", preprocessor), ("rf", lgb.LGBMRegressor())])
     pipe.fit(train_data, data["y"])
     return pipe, train_data
+
+
+@pytest.fixture(scope="session")
+def sklearn_pipeline_advanced() -> tuple[Pipeline, pd.DataFrame]:
+
+    X, y = fetch_openml("titanic", version=1, as_frame=True, return_X_y=True, parser="pandas")
+
+    numeric_features = ["age", "fare"]
+    numeric_transformer = Pipeline(steps=[("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler())])
+
+    categorical_features = ["embarked", "sex", "pclass"]
+    categorical_transformer = Pipeline(
+        steps=[
+            ("encoder", OneHotEncoder(handle_unknown="ignore")),
+            ("selector", SelectPercentile(chi2, percentile=50)),
+        ]
+    )
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numeric_transformer, numeric_features),
+            ("cat", categorical_transformer, categorical_features),
+        ]
+    )
+
+    clf = Pipeline(steps=[("preprocessor", preprocessor), ("classifier", LogisticRegression())])
+
+    X_train, X_test, y_train, y_test = train_test_split(X[:1000], y[:1000], test_size=0.2, random_state=0)
+
+    features = [*numeric_features, *categorical_features]
+    X_train = X_train[features]
+    y_train = y_train.to_numpy().astype(np.int32)
+
+    clf.fit(X_train, y_train)
+    return clf, X_train[:100]
 
 
 @pytest.fixture(scope="function")
