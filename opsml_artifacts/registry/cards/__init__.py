134745f82ccf5f360b01f71d6d809f56122689d3
thorrester <steven.forrester@shipt.com>
2023-04-10 16:09:56 +0000

    adding tags

--- opsml_artifacts/registry/cards/__init__.py
+++ opsml_artifacts/registry/cards/__init__.py
@@ -1,2 +1,2 @@
-from opsml_artifacts.registry.cards.cards import ArtifactCard
+from opsml_artifacts.registry.cards.cards import ArtifactCard, RunCard
 from opsml_artifacts.registry.cards.types import CardInfo
