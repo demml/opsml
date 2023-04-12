874dd96eef0bdb3f6078c10f90660cdf935efe2e
thorrester <steven.forrester@shipt.com>
2023-04-12 20:57:35 +0000

    added opsml_project read test

--- tests/test_migration/test_migrations.py
+++ tests/test_migration/test_migrations.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch, MagicMock
+from click.testing import CliRunner
+from opsml_artifacts.scripts.update_registries import update_registries
 from opsml_artifacts.registry.sql.connectors.connector import LocalSQLConnection
-from opsml_artifacts.registry.sql.migration.migrate import run_alembic
+from opsml_artifacts.registry.sql.migration.migrate import run_alembic_migrations
 from sqlalchemy.engine.reflection import Inspector
 
 
@@ -16,9 +18,42 @@ def test_migrations(mock_local_engine, experiment_table_to_migrate):
 
         experiment_table_to_migrate.__table__.create(bind=engine, checkfirst=True)
 
-        run_alembic()
+        tables = Inspector.from_engine(engine).get_table_names()
+        assert "OPSML_EXPERIMENT_REGISTRY" in tables
 
-        inspector = Inspector.from_engine(engine)
-        tables = inspector.get_table_names()
+        # should switch name to "OPSML_RUN_REGISTRY"
+        run_alembic_migrations()
+
+        # need to instantiate again
+        tables = Inspector.from_engine(engine).get_table_names()
 
         assert "OPSML_EXPERIMENT_REGISTRY" not in tables
+        assert "OPSML_RUN_REGISTRY" in tables
+
+
+def test_cli(mock_local_engine, experiment_table_to_migrate):
+
+    runner = CliRunner()
+
+    with patch.multiple(
+        "opsml_artifacts.registry.sql.connectors.connector.LocalSQLConnection",
+        get_engine=MagicMock(return_value=mock_local_engine),
+    ) as engine_mock:
+
+        local_client = LocalSQLConnection(tracking_uri="sqlite://")
+        engine = local_client.get_engine()
+
+        experiment_table_to_migrate.__table__.create(bind=engine, checkfirst=True)
+
+        tables = Inspector.from_engine(engine).get_table_names()
+        assert "OPSML_EXPERIMENT_REGISTRY" in tables
+
+        # should switch name to "OPSML_RUN_REGISTRY"
+        result = runner.invoke(update_registries)
+        assert result.exit_code == 0
+
+        # need to instantiate again
+        tables = Inspector.from_engine(engine).get_table_names()
+
+        assert "OPSML_EXPERIMENT_REGISTRY" not in tables
+        assert "OPSML_RUN_REGISTRY" in tables
