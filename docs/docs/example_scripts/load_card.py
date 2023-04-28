e51b69bece231b118d953758c0852e1ebd64eab4
thorrester <steven.forrester@shipt.com>
2023-04-28 19:19:37 +0000

    update docs
    Former-commit-id: 355a3abae624f19ef81410a9f139dea8f19cdd95

--- docs/docs/example_scripts/load_card.py
+++ docs/docs/example_scripts/load_card.py
@@ -5,5 +5,5 @@ model_regitstry = CardRegistry(registry_name="model")
 example_record = model_regitstry.list_cards(name="linnerrud", as_dataframe=False)[0]
 
 model_card = model_regitstry.load_card(uid=example_record.get("uid"))
-
-print(model_card.dict())
+print(model_card.version)
+# > 1.0.0
