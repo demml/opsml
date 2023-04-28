5c24d0ab96a0538a36a15d9c1962d4c7a3da0731
thorrester <steven.forrester@shipt.com>
2023-04-28 17:07:56 +0000

    fix api load

--- opsml/registry/sql/records.py
+++ opsml/registry/sql/records.py
@@ -230,7 +230,6 @@ class LoadedModelRecord(LoadRecord):
         storage_spec = ArtifactStorageSpecs(save_path=values["modelcard_uri"])
 
         storage_client.storage_spec = storage_spec
-
         model_card_definition = load_record_artifact_from_storage(
             storage_client=storage_client,
             artifact_type=ARBITRARY_ARTIFACT_TYPE,
