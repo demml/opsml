9c397b0f2b00e9c7ebf2eef17f5da5b68954f0c0
thorrester <steven.forrester@shipt.com>
2023-04-28 19:34:23 +0000

    challenger docstring
    Former-commit-id: 7afb2305710c8b24a4078a36c539fd74ecdcabb2

--- docs/docs/example_scripts/load_card.py
+++ docs/docs/example_scripts/load_card.py
@@ -1,9 +1,10 @@
 from opsml.registry import CardRegistry
 
-model_regitstry = CardRegistry(registry_name="model")
+model_registry = CardRegistry(registry_name="model")
 
-example_record = model_regitstry.list_cards(name="linnerrud", as_dataframe=False)[0]
+example_record = model_registry.list_cards(name="linnerrud", as_dataframe=False)[0]
 
-model_card = model_regitstry.load_card(uid=example_record.get("uid"))
+model_card = model_registry.load_card(uid=example_record.get("uid"))
 print(model_card.version)
 # > 1.0.0
+model_registry.register_card()
