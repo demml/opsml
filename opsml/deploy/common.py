76148c5d4758840e6339b32f1fc5d2ad3e4543b9
thorrester <steven.forrester@shipt.com>
2023-04-30 11:53:08 +0000

    loader

--- opsml/deploy/common.py
+++ opsml/deploy/common.py
@@ -1,4 +1,11 @@
+from enum import Enum
 from pydantic import BaseModel, validator
+
+
+class OnnxDataType(str, Enum):
+    NUMPY_ARRAY = "NUMPY_ARRAY"
+    PANDAS_DATAFRAME = "PANDAS_DATAFRAME"
+    DICT = "DICT"
 
 
 class LoadedApiModelDef(BaseModel):
