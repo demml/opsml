187e82c638719270ee8156427e7c4bcc2934aa10
thorrester <steven.forrester@shipt.com>
2023-05-03 14:09:10 +0000

    typing
    Former-commit-id: 53b1a8f7fdabdd48461e0fb531beabda13015bb8

--- opsml/deploy/common.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-from pydantic import BaseModel, validator
-
-
-class OnnxDataType(str, Enum):
-    NUMPY_ARRAY = "NUMPY_ARRAY"
-    PANDAS_DATAFRAME = "PANDAS_DATAFRAME"
-    DICT = "DICT"
