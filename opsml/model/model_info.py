5be7c97e6f43b8855b01c1c35a105071158d0b28
thorrester <steven.forrester@shipt.com>
2023-05-23 20:23:41 +0000

    refactor challenger
    Former-commit-id: ad17d7a917f0e9fb150663408105992d599cba56

--- opsml/model/model_info.py
+++ opsml/model/model_info.py
@@ -228,9 +228,9 @@ class FloatTypeConverter:
         for feature, feature_type in zip(data.columns, data.dtypes):
             if not self.convert_all:
                 if DataDtypes.FLOAT64 in str(feature_type):
-                    data[feature] = data[feature].astype(np.float32)
+                    data[feature] = data.loc[:, [feature]].astype(np.float32)
             else:
-                data[feature] = data[feature].astype(np.float32)
+                data[feature] = data.loc[:, [feature]].astype(np.float32)
         return data
 
     def _convert_array(self, data: NDArray) -> NDArray:
