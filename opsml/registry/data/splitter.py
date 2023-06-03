4d8fb0e116362d50f5dd75cf1b66edfc2b56280e
thorrester <steven.forrester@shipt.com>
2023-06-03 18:21:02 +0000

    add trim
    Former-commit-id: 6e69c973f9b54da4c398c39ae1bc67de9e2328f0

--- opsml/registry/data/splitter.py
+++ opsml/registry/data/splitter.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Tuple, Union
 
+import polars as pl
 import numpy as np
 import pandas as pd
 import pyarrow as pa
@@ -26,6 +27,7 @@ class SplitModel(BaseModel):
     label: str
     column: Optional[str] = None
     column_value: Optional[Any] = None
+    inequality: Optional[str] = None
     start: Optional[int] = None
     stop: Optional[int] = None
     indices: Optional[List[int]] = None
@@ -41,6 +43,15 @@ class SplitModel(BaseModel):
             value = list(value)
 
         return value
+    
+    @validator("inequality", pre=True)
+    def trim_whitespace(cls, value):  # pylint: disable=no-self-argument
+        """Trims whitespace from equality signs"""
+
+        if value is not None:
+            value = value.strip()
+
+        return value
 
 
 class Splitter:
@@ -49,7 +60,7 @@ class Splitter:
         split_attributes: Dict[str, Any],
         dependent_vars: Optional[List[Union[int, str]]] = None,
     ):
-        self.split_attributes = SplitModel(**split_attributes)
+        self.split_attr = SplitModel(**split_attributes)
         self.dependent_vars = dependent_vars
 
     def split(self, data):
@@ -60,15 +71,41 @@ class Splitter:
         raise NotImplementedError
 
 
+class PolarsColumnSplitter(Splitter):
+    def split(self, data: pl.DataFrame) -> Tuple[str, Data]:
+        if self.dependent_vars is not None:
+            x_cols = data.columns
+            for var in self.dependent_vars:
+                x_cols.remove(var)
+
+            x = data.filter(pl.col(self.split_attr.column) == self.split_attr.column_value).select(x_cols)
+            y = data.filter(pl.col(self.split_attr.column) == self.split_attr.column_value).select(
+                self.dependent_vars
+            )
+
+            return self.split_attr.label, Data(X=x, y=y)
+
+        return self.split_attr.label, Data(
+            X=data.filter(pl.col(self.split_attr.column) == self.split_attr.column_value)
+        )
+
+    @staticmethod
+    def validate(data_type: type, split_dict: Dict[str, Any]):
+        return data_type == pl.DataFrame and split_dict.get("column") is not None
+
+
 class PandasIndexSplitter(Splitter):
     def split(self, data: pd.DataFrame) -> Tuple[str, Data]:
         if self.dependent_vars is not None:
             x = data[data.columns[~data.columns.isin(self.dependent_vars)]]
             y = data[data.columns[data.columns.isin(self.dependent_vars)]]
 
-            return self.split_attributes.label, Data(X=x, y=y)
+            return self.split_attr.label, Data(
+                X=x.iloc[self.split_attr.indices],
+                y=y.iloc[self.split_attr.indices],
+            )
 
-        return self.split_attributes.label, Data(X=data.iloc[self.split_attributes.indices])
+        return self.split_attr.label, Data(X=data.iloc[self.split_attr.indices])
 
     @staticmethod
     def validate(data_type: type, split_dict: Dict[str, Any]):
@@ -81,12 +118,12 @@ class PandasRowSplitter(Splitter):
             x = data[data.columns[~data.columns.isin(self.dependent_vars)]]
             y = data[data.columns[data.columns.isin(self.dependent_vars)]]
 
-            return self.split_attributes.label, Data(
-                X=x[self.split_attributes.start : self.split_attributes.stop],
-                y=y[self.split_attributes.start : self.split_attributes.stop],
+            return self.split_attr.label, Data(
+                X=x[self.split_attr.start : self.split_attr.stop],
+                y=y[self.split_attr.start : self.split_attr.stop],
             )
 
-        return self.split_attributes.label, Data(X=data[self.split_attributes.start : self.split_attributes.stop])
+        return self.split_attr.label, Data(X=data[self.split_attr.start : self.split_attr.stop])
 
     @staticmethod
     def validate(data_type: type, split_dict: Dict[str, Any]):
@@ -99,13 +136,17 @@ class PandasColumnSplitter(Splitter):
             x = data[data.columns[~data.columns.isin(self.dependent_vars)]]
             y = data[data.columns[data.columns.isin(self.dependent_vars)]]
 
-            return self.split_attributes.label, Data(
-                X=x.loc[data[self.split_attributes.column] == self.split_attributes.column_value],
-                y=y.loc[data[self.split_attributes.column] == self.split_attributes.column_value],
-            )
+            if self.split_attr.inequality is None:
+                return self.split_attr.label, Data(
+                    X=x.loc[data[self.split_attr.column] == self.split_attr.column_value],
+                    y=y.loc[data[self.split_attr.column] == self.split_attr.column_value],
+                )
+                
+            # leave off hear. find way to remove white
+            elif  self.split_attr.inequality. :
 
-        data_split = Data(X=data.loc[data[self.split_attributes.column] == self.split_attributes.column_value])
-        return self.split_attributes.label, data_split
+        data_split = Data(X=data.loc[data[self.split_attr.column] == self.split_attr.column_value])
+        return self.split_attr.label, data_split
 
     @staticmethod
     def validate(data_type: type, split_dict: Dict[str, Any]):
@@ -114,7 +155,7 @@ class PandasColumnSplitter(Splitter):
 
 class PyArrowIndexSplitter(Splitter):
     def split(self, data: pa.Table) -> Tuple[str, Data]:
-        return self.split_attributes.label, Data(X=data.take(self.split_attributes.indices))
+        return self.split_attr.label, Data(X=data.take(self.split_attr.indices))
 
     @staticmethod
     def validate(data_type: type, split_dict: Dict[str, Any]):
@@ -123,7 +164,7 @@ class PyArrowIndexSplitter(Splitter):
 
 class NumpyIndexSplitter(Splitter):
     def split(self, data: np.ndarray) -> Tuple[str, Data]:
-        return self.split_attributes.label, Data(X=data[self.split_attributes.indices])
+        return self.split_attr.label, Data(X=data[self.split_attr.indices])
 
     @staticmethod
     def validate(data_type: type, split_dict: Dict[str, Any]):
@@ -132,8 +173,8 @@ class NumpyIndexSplitter(Splitter):
 
 class NumpyRowSplitter(Splitter):
     def split(self, data: np.ndarray) -> Tuple[str, Data]:
-        data_split = data[self.split_attributes.start : self.split_attributes.stop]
-        return self.split_attributes.label, Data(X=data_split)
+        data_split = data[self.split_attr.start : self.split_attr.stop]
+        return self.split_attr.label, Data(X=data_split)
 
     @staticmethod
     def validate(data_type: type, split_dict: Dict[str, Any]):
@@ -146,23 +187,25 @@ class DataSplitter:
         split_attributes: Dict[str, Any],
         dependent_vars: Optional[List[Union[int, str]]] = None,
     ):
-        self.split_attributes = split_attributes
+        self.split_attr = split_attributes
         self.dependent_vars = dependent_vars
 
     def split(self, data: Union[pd.DataFrame, np.ndarray]):
+        print(type(data))
+        print(self.split_attr)
         splitter = next(
             (
                 splitter
                 for splitter in Splitter.__subclasses__()
                 if splitter.validate(
                     data_type=type(data),
-                    split_dict=self.split_attributes,
+                    split_dict=self.split_attr,
                 )
             )
         )
 
         data_splitter = splitter(
-            split_attributes=self.split_attributes,
+            split_attributes=self.split_attr,
             dependent_vars=self.dependent_vars,
         )
         return data_splitter.split(data=data)
