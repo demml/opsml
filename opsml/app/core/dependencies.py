73232abf45b1b3d930d505baacb1c7a716a8f907
thorrester <steven.forrester@shipt.com>
2023-06-09 18:03:31 +0000

    add token dep to cards
    Former-commit-id: a3141c5f57805cb5482903cae9a65bd609599914

--- opsml/app/core/dependencies.py
+++ opsml/app/core/dependencies.py
@@ -1,8 +1,14 @@
 from typing import Annotated
+from fastapi import Header, HTTPException
 
-from fastapi import Depends, FastAPI, Header, HTTPException
+from opsml.app.core.config import config
+from opsml.helpers.logging import ArtifactLogger
+
+logger = ArtifactLogger.get_logger(__name__)
 
 
-def verify_token(X_Authentication: Annotated[str, Header()]):
-    if x_token != "fake-super-secret-token":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
+def verify_token(x_token: Annotated[str, Header()]):
+    logger.info(x_token)
+    if config.APP_ENV == "production":
+        if x_token != config.PROD_AUTHENITCATION_TOKEN:
+            raise HTTPException(status_code=400, detail="Cannot perform write operation on Prod from non-prod")
