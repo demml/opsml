b9c0de4d41bf9121e3ce25fb06e75d099f300f6c
thorrester <steven.forrester@shipt.com>
2023-06-11 11:19:37 +0000

    add
    Former-commit-id: 0785a48b6ec7006cb60e734c0570d8206c4e7a3d

--- tests/test_docs/test_all_examples.py
+++ tests/test_docs/test_all_examples.py
@@ -231,3 +231,62 @@ def test_data_splits():
 
     splits = datacard.split_data()
     assert splits.train.X.shape[0] == 3
+
+
+def test_data_profile(db_registries):
+    # Data
+    from sklearn.datasets import load_linnerud
+
+    # Opsml
+    from opsml.registry import CardInfo, DataCard, CardRegistry
+
+    data, target = load_linnerud(return_X_y=True, as_frame=True)
+    data["Pulse"] = target.Pulse
+
+    card_info = CardInfo(name="linnerrud", team="opsml", user_email="user@email.com")
+    data_card = DataCard(info=card_info, data=data)
+
+    data_card.create_data_profile(sample_perc=0.5)  # you can specify a sampling percentage between 0 and 1
+
+    # if youd like to view you're report, you can export it to html or json
+    # Jupyter notebooks will render the html without needing to save (just call data_card.data_profile)
+    # data_card.data_profile.to_file("my_report.html")
+
+    # Registering card will automatically save the report and its html
+    data_registry = db_registries["data"]
+    data_registry.register_card(card=data_card)
+
+    from ydata_profiling import ProfileReport
+    from opsml.registry import DataCard
+
+    data, target = load_linnerud(return_X_y=True, as_frame=True)
+    data["Pulse"] = target.Pulse
+
+    data_profile = ProfileReport(data, title="Profiling Report")
+
+    card_info = CardInfo(name="linnerrud", team="opsml", user_email="user@email.com")
+    data_card = DataCard(info=card_info, data=data, data_profile=data_profile)
+
+    from sklearn.datasets import load_linnerud
+    import numpy as np
+
+    # Opsml
+    from opsml.registry import CardInfo, DataCard
+    from opsml.profile import DataProfiler
+
+    data, target = load_linnerud(return_X_y=True, as_frame=True)
+    data["Pulse"] = target.Pulse
+
+    # Simulate creating 1st DataCard
+    card_info = CardInfo(name="linnerrud", team="opsml", user_email="user@email.com")
+    data_card = DataCard(info=card_info, data=data)
+    data_card.create_data_profile()
+
+    # Simulate creating 2nd DataCard
+    data2 = data * np.random.rand(data.shape[1])
+    card_info = CardInfo(name="linnerrud", team="opsml", user_email="user@email.com")
+    data_card2 = DataCard(info=card_info, data=data2)
+    data_card2.create_data_profile()
+
+    comparison = DataProfiler.compare_reports(reports=[data_card.data_profile, data_card2.data_profile])
+    # comparison.to_file("comparison_report.html")
