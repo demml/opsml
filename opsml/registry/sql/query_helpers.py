0d221ee220b7b8d16a869b52687517b9ee101b19
thorrester <steven.forrester@shipt.com>
2023-08-13 19:58:25 +0000

    fix date handling
    Former-commit-id: 641b38d1ea703d7ae48297b6d66eab19dc610144

--- opsml/registry/sql/query_helpers.py
+++ opsml/registry/sql/query_helpers.py
@@ -13,10 +13,10 @@ from sqlalchemy.sql.expression import ColumnElement
 
 from opsml.helpers.logging import ArtifactLogger
 from opsml.registry.sql.semver import get_version_to_search
-from opsml.registry.sql.sql_schema import REGISTRY_TABLES, YEAR_MONTH_DATE, TableSchema
+from opsml.registry.sql.sql_schema import REGISTRY_TABLES, TableSchema
+from opsml.registry.sql.records import YEAR_MONTH_DATE
 
 logger = ArtifactLogger.get_logger(__name__)
-
 
 SqlTableType = Optional[Iterable[Union[ColumnElement[Any], FromClause, int]]]
 
