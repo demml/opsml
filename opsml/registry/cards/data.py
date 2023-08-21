b50411c50942b456cf22950d5d9a2319bb7a46fc
thorrester <steven.forrester@shipt.com>
2023-08-21 19:02:30 +0000

    update datacard
    Former-commit-id: e8fd59eb7080fa0d2f0ef7253393e2d4e094f10f

--- opsml/registry/cards/data.py
+++ opsml/registry/cards/data.py
@@ -8,6 +8,7 @@ import polars as pl
 from pyarrow import Table
 from pydantic import field_validator
 
+
 from opsml.helpers.logging import ArtifactLogger
 from opsml.helpers.utils import (
     FindPath,
@@ -31,8 +32,6 @@ from opsml.registry.storage.types import ArtifactStorageSpecs
 
 logger = ArtifactLogger.get_logger(__name__)
 storage_client = settings.storage_client
-
-SUPPORTED_DATA_TYPES = Union[np.ndarray, pd.DataFrame, Table, pl.DataFrame]
 
 
 class DataCard(ArtifactCard):
@@ -89,7 +88,7 @@ class DataCard(ArtifactCard):
 
     """
 
-    data: Optional[SUPPORTED_DATA_TYPES] = None
+    data: Optional[Union[np.ndarray, pd.DataFrame, Table, pl.DataFrame]] = None
     data_splits: List[DataSplit] = []
     feature_map: Optional[Dict[str, Optional[Any]]] = None
     data_type: Optional[str] = None
@@ -109,15 +108,9 @@ class DataCard(ArtifactCard):
         else:
             data_uri = uris.get("data_uri")
 
-        data = info.data.get("data")
-
-        if data is None and not bool(info.data.get("sql_logic")):
+        if info.data.get("data") is None and not bool(info.data.get("sql_logic")):
             if data_uri is None:
-                raise ValueError(
-                    """DataCards require either data, sql_logic or a data_uri.\n
-                    DataCards support polars dataframes, pandas dataframes, numpy arrays and parquet tables.
-                    """
-                )
+                raise ValueError("Data or sql logic must be supplied when no data_uri is present")
 
         return uris
 
