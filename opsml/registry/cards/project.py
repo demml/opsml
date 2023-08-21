ba13d4f8abcad5a5217f98a62ddc93bf3a83275e
thorrester <steven.forrester@shipt.com>
2023-08-21 19:04:03 +0000

    add license
    Former-commit-id: 2cf7a093fbabbc74c76add26a5f63af13b47d8f3

--- opsml/registry/cards/project.py
+++ opsml/registry/cards/project.py
@@ -1,5 +1,7 @@
 # pylint: disable=too-many-lines
-
+# Copyright (c) Shipt, Inc.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 from typing import Optional
 
 from pydantic import field_validator, ValidationInfo
