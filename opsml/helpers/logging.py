f252e7fb1fdccac6f0d31d8f670574c124ca0ef4
thorrester <steven.forrester@shipt.com>
2023-09-21 20:51:05 +0000

    testing logging
    Former-commit-id: 1ffbbe8b7e46ba120c912bfd666e8bbc2279e7b0

--- opsml/helpers/logging.py
+++ opsml/helpers/logging.py
@@ -2,17 +2,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import logging
 import os
-import sys
-from datetime import datetime
-from typing import IO
-from typing import Optional
 from rusty_logger import Logger, JsonConfig, LogConfig
-
-APP_ENV = os.getenv("APP_ENV", "development")
-
-Logger.get_logger()
 
 
 class ArtifactLogger(Logger):
