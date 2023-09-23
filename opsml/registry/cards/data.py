8d53557c8301e3cd3b12ca51587ae3a94db3bd7e
thorrester <steven.forrester@shipt.com>
2023-09-23 19:40:06 +0000

    lints

--- opsml/registry/cards/data.py
+++ opsml/registry/cards/data.py
@@ -2,7 +2,7 @@
 # Copyright (c) Shipt, Inc.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from typing import Any, Dict, List, Optional, Union, cast
+from typing import Dict, List, Optional, Union, cast
 import os
 import numpy as np
 import pandas as pd
