7b0d14a4b7d8d26bb8cc510808e2aa0fff0e1119
thorrester <steven.forrester@shipt.com>
2023-10-18 19:49:29 +0000

    formatting

--- opsml/app/core/event_handlers.py
+++ opsml/app/core/event_handlers.py
@@ -50,9 +50,9 @@ def _shutdown_registries(app: FastAPI):
 
 
 def _log_url_and_storage():
-    logger.info(f"OpsML tracking url: {config.TRACKING_URI}")
-    logger.info(f"OpsML storage url: {config.STORAGE_URI}")
-    logger.info(f"Environment: {config.APP_ENV}")
+    logger.info("OpsML tracking url: {}", config.TRACKING_URI)
+    logger.info("OpsML storage url: {}", config.STORAGE_URI)
+    logger.info("Environment: {}", config.APP_ENV)
 
 
 def start_app_handler(app: FastAPI) -> Callable[[], None]:
