e41737fbf7e45f4a3096d885fce120ce790f612a
thorrester <steven.forrester@shipt.com>
2023-10-24 13:42:33 +0000

    update storage logic

--- opsml/registry/storage/storage_system.py
+++ opsml/registry/storage/storage_system.py
@@ -414,6 +414,7 @@ class LocalStorageClient(StorageClient):
 
     def list_files(self, storage_uri: str) -> FilePath:
         path = Path(storage_uri)
+
         if path.is_dir():
             paths = [str(p) for p in path.rglob("*")]
             return paths
@@ -440,15 +441,23 @@ class LocalStorageClient(StorageClient):
         return shutil.copyfile(read_path, write_path)
 
     def download(self, rpath: str, lpath: str, recursive: bool = False, **kwargs) -> Optional[str]:
-        files = kwargs.get("files", None)
+        local_path = Path(lpath)
+        read_path = Path(rpath)
 
+        # check if files have been passed (used with downloading artifacts)
+        files = kwargs.get("files", [])
         if len(files) == 1:
-            filename = os.path.basename(files[0])
+            filepath = Path(files[0])
 
-            if os.path.isdir(lpath):
-                lpath = os.path.join(lpath, filename)
+            if local_path.is_dir():
+                lpath = str(local_path / filepath.name)
+            return self.copy(read_path=str(filepath), write_path=lpath)
 
-            return self.copy(read_path=files[0], write_path=lpath)
+        # check if trying to copy single file to directory
+        if read_path.is_file():
+            lpath = str(local_path / read_path.name)
+            return self.copy(read_path=rpath, write_path=lpath)
+
         return self.copy(read_path=rpath, write_path=lpath)
 
     def delete(self, read_path: str) -> None:
@@ -590,15 +599,17 @@ class ApiStorageClient(LocalStorageClient):
         recursive: bool = False,
     ) -> str:
         for file_ in files:
-            read_dir = os.path.dirname(file_)
-            local_dir = read_dir.replace(rpath, lpath)
-            filename = os.path.basename(file_)
+            path = Path(file_)
+            read_dir = str(path.parents[0])
+
+            if path.is_dir():
+                continue  # folder name path gets added to files list when use local storage client
 
             self.api_client.stream_download_file_request(
                 route=ApiRoutes.DOWNLOAD_FILE,
-                local_dir=local_dir,
+                local_dir=read_dir.replace(rpath, lpath),
                 read_dir=read_dir,
-                filename=filename,
+                filename=path.name,
             )
 
         return lpath
