a43bae35c2ebeca0c8ca19401a52e857f2c5ce40
thorrester <steven.forrester@shipt.com>
2023-10-25 23:28:48 +0000

    update pydantic models
    Former-commit-id: 436fbb4e7611375a7786f19f2abcc12ab999a1ed

--- opsml/app/routes/pydantic_models.py
+++ opsml/app/routes/pydantic_models.py
@@ -2,12 +2,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 from typing import Any, Dict, List, Optional, Union
-
+from fastapi import Form, File, UploadFile
 from pydantic import BaseModel, Field, model_validator
 from opsml.model.challenger import BattleReport
 from opsml.registry.cards.types import METRICS
 from opsml.registry.sql.base.registry_base import VersionType
 from opsml.registry.sql.semver import CardVersion
+from opsml.registry.cards.audit import Comment, AuditSections
 
 
 class StorageUri(BaseModel):
@@ -16,6 +17,12 @@ class StorageUri(BaseModel):
 
 class HealthCheckResult(BaseModel):
     is_alive: bool
+
+
+class ListTeamNameInfo(BaseModel):
+    teams: Optional[List[str]] = None
+    selected_team: Optional[str] = None
+    names: Optional[List[str]] = None
 
 
 class DebugResponse(BaseModel):
@@ -54,6 +61,14 @@ class UidExistsRequest(BaseModel):
 
 class UidExistsResponse(BaseModel):
     uid_exists: bool
+
+
+class TeamsResponse(BaseModel):
+    teams: List[str] = []
+
+
+class NamesResponse(BaseModel):
+    names: List[str] = []
 
 
 class ListCardRequest(BaseModel):
@@ -200,3 +215,139 @@ class CompareMetricResponse(BaseModel):
     challenger_name: str
     challenger_version: str
     report: Dict[str, List[BattleReport]]
+
+
+def form_body(cls):
+    args = []
+    params = cls.__signature__.parameters
+    for param in params.values():
+        if param.name == "audit_file":
+            arg = param.replace(default=File(None))
+        elif param.default is not None:
+            arg = param.replace(default=Form(...))
+        else:
+            arg = param.replace(default=Form(None))
+        args.append(arg)
+
+    cls.__signature__ = cls.__signature__.replace(parameters=args)
+    return cls
+
+
+@form_body
+class CommentSaveRequest(BaseModel):
+    uid: str
+    name: str
+    email: str
+    team: str
+    selected_model_name: str
+    selected_model_team: str
+    selected_model_version: str
+    selected_model_email: str
+    comment_name: str
+    comment_text: str
+
+
+@form_body
+class AuditFormRequest(BaseModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+    team: Optional[str] = None
+    uid: Optional[str] = None
+    version: Optional[str] = None
+    status: Optional[str] = None
+    selected_model_name: str
+    selected_model_team: str
+    selected_model_version: str
+    selected_model_email: str
+    audit_file: Optional[UploadFile] = None
+    comments: Optional[str] = None
+    business_understanding_1: Optional[str] = None
+    business_understanding_2: Optional[str] = None
+    business_understanding_3: Optional[str] = None
+    business_understanding_4: Optional[str] = None
+    business_understanding_5: Optional[str] = None
+    business_understanding_6: Optional[str] = None
+    business_understanding_7: Optional[str] = None
+    business_understanding_8: Optional[str] = None
+    business_understanding_9: Optional[str] = None
+    business_understanding_10: Optional[str] = None
+    data_understanding_1: Optional[str] = None
+    data_understanding_2: Optional[str] = None
+    data_understanding_3: Optional[str] = None
+    data_understanding_4: Optional[str] = None
+    data_understanding_5: Optional[str] = None
+    data_understanding_6: Optional[str] = None
+    data_understanding_7: Optional[str] = None
+    data_understanding_8: Optional[str] = None
+    data_understanding_9: Optional[str] = None
+    data_preparation_1: Optional[str] = None
+    data_preparation_2: Optional[str] = None
+    data_preparation_3: Optional[str] = None
+    data_preparation_4: Optional[str] = None
+    data_preparation_5: Optional[str] = None
+    data_preparation_6: Optional[str] = None
+    data_preparation_7: Optional[str] = None
+    data_preparation_8: Optional[str] = None
+    data_preparation_9: Optional[str] = None
+    data_preparation_10: Optional[str] = None
+    modeling_1: Optional[str] = None
+    modeling_2: Optional[str] = None
+    modeling_3: Optional[str] = None
+    modeling_4: Optional[str] = None
+    modeling_5: Optional[str] = None
+    modeling_6: Optional[str] = None
+    modeling_7: Optional[str] = None
+    modeling_8: Optional[str] = None
+    modeling_9: Optional[str] = None
+    modeling_10: Optional[str] = None
+    modeling_11: Optional[str] = None
+    modeling_12: Optional[str] = None
+    evaluation_1: Optional[str] = None
+    evaluation_2: Optional[str] = None
+    evaluation_3: Optional[str] = None
+    evaluation_4: Optional[str] = None
+    evaluation_5: Optional[str] = None
+    deployment_ops_1: Optional[str] = None
+    deployment_ops_2: Optional[str] = None
+    deployment_ops_3: Optional[str] = None
+    deployment_ops_4: Optional[str] = None
+    deployment_ops_5: Optional[str] = None
+    deployment_ops_6: Optional[str] = None
+    deployment_ops_7: Optional[str] = None
+    deployment_ops_8: Optional[str] = None
+    deployment_ops_9: Optional[str] = None
+    deployment_ops_10: Optional[str] = None
+    deployment_ops_11: Optional[str] = None
+    deployment_ops_12: Optional[str] = None
+    deployment_ops_13: Optional[str] = None
+    deployment_ops_14: Optional[str] = None
+    deployment_ops_15: Optional[str] = None
+    deployment_ops_16: Optional[str] = None
+    deployment_ops_17: Optional[str] = None
+    deployment_ops_18: Optional[str] = None
+    deployment_ops_19: Optional[str] = None
+    deployment_ops_20: Optional[str] = None
+    deployment_ops_21: Optional[str] = None
+    deployment_ops_22: Optional[str] = None
+    misc_1: Optional[str] = None
+    misc_2: Optional[str] = None
+    misc_3: Optional[str] = None
+    misc_4: Optional[str] = None
+    misc_5: Optional[str] = None
+    misc_6: Optional[str] = None
+    misc_7: Optional[str] = None
+    misc_8: Optional[str] = None
+    misc_9: Optional[str] = None
+    misc_10: Optional[str] = None
+
+
+class AuditReport(BaseModel):
+    name: Optional[str] = None
+    team: Optional[str] = None
+    user_email: Optional[str] = None
+    version: Optional[str] = None
+    uid: Optional[str] = None
+    status: Optional[bool] = False
+    audit: Optional[Dict[str, Any]] = AuditSections().model_dump()  # type: ignore
+    timestamp: Optional[str] = None
+    comments: List[Optional[Comment]] = []
