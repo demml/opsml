59fe2512d09ed7a9db22e9f57ff0d3ad4c1393ff
thorrester <steven.forrester@shipt.com>
2023-10-25 23:42:06 +0000

    update utils

--- opsml/app/routes/utils.py
+++ opsml/app/routes/utils.py
@@ -1,14 +1,118 @@
 # Copyright (c) Shipt, Inc.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from typing import Any, Dict
+from typing import Any, Dict, Optional, List, Tuple
+import os
 from pathlib import Path
+import traceback
+from functools import wraps
 from streaming_form_data.targets import FileTarget
-
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+from opsml.app.routes.pydantic_models import ListTeamNameInfo
+from opsml.registry import CardRegistries, RunCard, CardRegistry
 from opsml.helpers.logging import ArtifactLogger
 from opsml.registry.storage.storage_system import LocalStorageClient, StorageClientType
 
 logger = ArtifactLogger.get_logger()
+# Constants
+PARENT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+TEMPLATE_PATH = os.path.abspath(os.path.join(PARENT_DIR, "templates"))
+
+
+templates = Jinja2Templates(directory=TEMPLATE_PATH)
+
+
+def get_model_versions(registry: CardRegistry, model: str, team: str) -> List[str]:
+    """Returns a list of model versions for a given team and model
+
+    Args:
+        registry:
+            The registry to query
+        model:
+            The model to query
+        team:
+            The team to query
+    Returns:
+        A list of model versions
+    """
+
+    return [card["version"] for card in registry.list_cards(name=model, team=team, as_dataframe=False)]
+
+
+def get_names_teams_versions(registry: CardRegistry, team: str, name: str) -> Tuple[List[str], List[str], List[str]]:
+    """Helper functions to get the names, teams, and versions for a given registry
+
+    Args:
+        registry:
+            The registry to query
+        team:
+            The team to query
+        name:
+            The name to query
+    Returns:
+        A tuple of names, teams, and versions
+    """
+
+    teams = registry._registry.unique_teams  # pylint: disable=protected-access
+    versions = get_model_versions(registry, name, team)
+    names = registry._registry.get_unique_card_names(team=team)  # pylint: disable=protected-access
+    return names, teams, versions
+
+
+def get_runcard_from_model(
+    registries: CardRegistries,
+    name: Optional[str] = None,
+    version: Optional[str] = None,
+    uid: Optional[str] = None,
+) -> Optional[RunCard]:
+    """Loads the runcard associated with a modelcard
+
+    Args:
+        registries:
+            CardRegistries object
+        name:
+            Name of the model
+        version:
+            Version of the model
+        uid:
+            UID of the model
+    Returns:
+        RunCard object
+    """
+    modelcard = registries.model.list_cards(
+        name=name,
+        version=version,
+        uid=uid,
+    )[0]
+
+    run_uid = modelcard.get("runcard_uid", None)
+
+    if run_uid is not None:
+        return registries.run.load_card(uid=run_uid)
+
+    return None
+
+
+def error_to_500(func):
+    """Function for wrapping errors in the opsml UI"""
+
+    @wraps(func)
+    async def wrapper(request: Request, *args, **kwargs):
+        try:
+            return await func(request, *args, **kwargs)
+        except Exception as exc:  # pylint: disable=broad-exception-caught
+            trace_back = traceback.format_exc()
+            logger.error("exceptions: {} {}", exc, trace_back)
+            return templates.TemplateResponse(
+                "include/500.html",
+                {
+                    "request": request,
+                    "error_message": str(exc),
+                },
+            )
+
+    return wrapper
 
 
 def get_real_path(current_path: str, proxy_root: str, storage_root: str) -> str:
@@ -80,3 +184,23 @@ class ExternalFileTarget(FileTarget):
 
     def on_start(self):
         self._fd = self.storage_client.open(self.filepath, self._mode)
+
+
+def list_team_name_info(registry: CardRegistry, team: Optional[str] = None) -> ListTeamNameInfo:
+    """Returns dictionary of items"""
+
+    all_teams = registry._registry.unique_teams  # pylint: disable=protected-access
+
+    if not bool(all_teams):
+        default_team = None
+    else:
+        default_team = all_teams[0]
+
+    team = team or default_team
+    names = registry._registry.get_unique_card_names(team=team)  # pylint: disable=protected-access
+
+    return ListTeamNameInfo(
+        teams=all_teams,
+        selected_team=team,
+        names=names,
+    )
