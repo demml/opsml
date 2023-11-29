0165e24936fd2a1ae90a9ac01fc798528a2c909c
thorrester <steven.forrester@shipt.com>
2023-11-29 01:18:53 +0000

    add project routes

--- opsml/app/routes/projects.py
+++ opsml/app/routes/projects.py
@@ -11,7 +11,7 @@ from fastapi import APIRouter, Body, HTTPException, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
 
-from opsml.app.routes.route_helpers import RunRouteHelper
+from opsml.app.routes.route_helpers import ProjectRouteHelper
 from opsml.helpers.logging import ArtifactLogger
 from opsml.app.routes.utils import error_to_500
 
@@ -23,12 +23,12 @@ TEMPLATE_PATH = os.path.abspath(os.path.join(PARENT_DIR, "templates"))
 templates = Jinja2Templates(directory=TEMPLATE_PATH)
 
 router = APIRouter()
-run_route_helper = RunRouteHelper()
+project_route_helper = ProjectRouteHelper()
 
 
-@router.get("/runs/list/", response_class=HTMLResponse)
+@router.get("/projects/list/", response_class=HTMLResponse)
 @error_to_500
-async def data_list_homepage(request: Request, project: Optional[str] = None):
+async def data_list_homepage(request: Request, project: Optional[str] = None, run_uid: Optional[str] = None):
     """UI home for listing models in model registry
 
     Args:
@@ -40,4 +40,4 @@ async def data_list_homepage(request: Request, project: Optional[str] = None):
         200 if the request is successful. The body will contain a JSON string
         with the list of models.
     """
-    return run_route_helper.get_homepage(request=request, project=project)
+    return project_route_helper.get_project_run(request=request, project=project, run_uid=run_uid)
