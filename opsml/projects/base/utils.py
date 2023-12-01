903db57317299b9f01960c0beaf951ee84cddaa3
thorrester <steven.forrester@shipt.com>
2023-12-01 01:31:28 +0000

    correct project id

--- opsml/projects/base/utils.py
+++ opsml/projects/base/utils.py
@@ -15,7 +15,7 @@ def get_project_id_from_registry(project_registry: CardRegistry, info: ProjectIn
         as_dataframe=False,
     )
     if bool(projects):
-        return projects[0]["project_id"]
+        return f'{projects[0]["team"]}:{projects[0]["name"]}'
 
     card = ProjectCard(
         name=info.name,
