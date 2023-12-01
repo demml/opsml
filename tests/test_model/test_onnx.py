3cddc5ec47b9ba29bbe17683462c771df6a7940b
Damon Allison <damon@shipt.com>
2023-12-01 11:29:12 -0600

    Adds tests back for apple silicon
    Former-commit-id: 783742618435705b618b3a56c6636debb91fa718

--- tests/test_model/test_onnx.py
+++ tests/test_model/test_onnx.py
@@ -143,11 +143,9 @@ def test_sklearn_models(model_and_data):
     model_predict(model_and_data)
 
 
-@pytest.mark.skipif(sys.platform == "darwin", reason="Not supported on apple silicon")
 @pytest.mark.parametrize(
     "model_and_data",
     [
-        # Not supported on apple silicon
         lazy_fixture("load_pytorch_resnet"),  # pytorch resent trained with numpy array
         lazy_fixture("load_pytorch_language"),  # huggingface automodel "distil-bert" trained with dictionary
         lazy_fixture("deeplabv3_resnet50"),  # deeplabv3_resnet50 trained with numpy array
@@ -157,17 +155,15 @@ def test_model_pytorch_predict(model_and_data):
     model_predict(model_and_data)
 
 
-@pytest.mark.skipif(sys.platform == "darwin", reason="Not supported on apple silicon")
 @pytest.mark.skipif(sys.platform == "win32", reason="No tf test with wn_32")
 @pytest.mark.parametrize(
     "model_and_data",
     [
-        # Not supported on apple silicon
         lazy_fixture("load_transformer_example"),  # keras transformer example
         lazy_fixture("load_multi_input_keras_example"),  # keras multi input model
     ],
 )
-def _test_tensorflow_predict(model_and_data):
+def test_tensorflow_predict(model_and_data):
     model_predict(model_and_data)
 
 
@@ -229,7 +225,6 @@ def test_byo_onnx(model_and_data):
     pred_orig = predictor.predict_with_model(model, record)
 
 
-@pytest.mark.skipif(sys.platform == "darwin", reason="Not supported on apple silicon")
 @pytest.mark.parametrize(
     "model_and_data",
     [
