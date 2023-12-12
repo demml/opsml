7acf6db95612eead45e4b4ae4095c3bf81d36b55
Damon Allison <damon@shipt.com>
2023-12-12 12:45:55 -0600

    Mock gcp creds
    Former-commit-id: b0f66eadb70be8df291482a83a9bfd3935811d61

--- tests/test_utils/test_settings.py
+++ tests/test_utils/test_settings.py
@@ -23,14 +23,14 @@ def test_default_local_settings() -> None:
     assert isinstance(settings.connection_client, LocalSQLConnection)
 
 
-def test_default_http_settings(mock_gcs_storage_response) -> None:
+def test_default_http_settings(mock_gcs_storage_response, mock_gcp_creds) -> None:
     cfg = OpsmlConfig(opsml_tracking_uri="http://testserver", opsml_storage_uri="gs://google")
     settings = DefaultSettings(cfg)
 
     assert isinstance(settings.storage_client, GCSFSStorageClient)
 
 
-def test_default_postgres_settings(mock_gcs_storage_response) -> None:
+def test_default_postgres_settings(mock_gcs_storage_response, mock_gcp_creds) -> None:
     tracking_uri = "postgresql+psycopg2://test_user:test_password@/ds-test-db?host=/cloudsql/test-project:test-region:test-connection"
     storage_uri = "gs://opsml/test"
     cfg = OpsmlConfig(opsml_tracking_uri=tracking_uri, opsml_storage_uri=storage_uri)
