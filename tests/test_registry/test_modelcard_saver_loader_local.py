dc611fb74266e4414bddc84d65a63a769e492aed
thorrester <steven.forrester@shipt.com>
2024-01-09 19:06:53 +0000

    update tests

--- tests/test_registry/test_modelcard_saver_loader_local.py
+++ tests/test_registry/test_modelcard_saver_loader_local.py
@@ -2,6 +2,8 @@ import uuid
 from pathlib import Path
 from typing import cast
 
+from transformers import Pipeline
+
 from opsml.cards import Description, ModelCard, ModelCardMetadata
 from opsml.model import (
     HuggingFaceModel,
@@ -14,7 +16,7 @@ from opsml.model import (
 from opsml.storage.card_loader import CardLoader
 from opsml.storage.card_saver import save_card_artifacts
 from opsml.types import CommonKwargs, RegistryType, SaveName
-from transformers import Pipeline
+
 
 def test_save_huggingface_modelcard(huggingface_torch_distilbert: HuggingFaceModel):
     model: HuggingFaceModel = huggingface_torch_distilbert
@@ -475,6 +477,7 @@ def test_save_tensorflow_multi_input_modelcard(
     assert loaded_card.interface.onnx_model is not None
     assert loaded_card.interface.onnx_model.sess is not None
 
+
 def test_save_huggingface_pipeline_modelcard(huggingface_text_classification_pipeline: HuggingFaceModel):
     model: HuggingFaceModel = huggingface_text_classification_pipeline
 
@@ -513,23 +516,22 @@ def test_save_huggingface_pipeline_modelcard(huggingface_text_classification_pip
     loaded_card = cast(ModelCard, loader.load_card())
     assert isinstance(loaded_card, ModelCard)
     assert loaded_card.interface.model is None
-        
+
     # attempt to load onnx model before loading model
     loaded_card.load_onnx_model()
     assert loaded_card.onnx_model is not None
     assert loaded_card.onnx_model.sess is not None
     assert isinstance(loaded_card.onnx_model.sess, Pipeline)
-    
+
     # loading onnx model should also load preprocessors
     assert type(loaded_card.preprocessor) == type(modelcard.interface.tokenizer)
-    assert loaded_card.interface.model is None # model should still be none
-
+    assert loaded_card.interface.model is None  # model should still be none
 
     loaded_card.load_model()
     assert type(loaded_card.model) == type(modelcard.interface.model)
     assert isinstance(loaded_card.model, Pipeline)
-    
-    
+
+
 def test_save_huggingface_vit_pipeline_modelcard(huggingface_vit_pipeline: HuggingFaceModel):
     model, _ = huggingface_vit_pipeline
 
@@ -571,23 +573,22 @@ def test_save_huggingface_vit_pipeline_modelcard(huggingface_vit_pipeline: Huggi
     assert isinstance(loaded_card, ModelCard)
     assert loaded_card.interface.model is None
     assert loaded_card.interface.feature_extractor is None
-        
+
     # attempt to load onnx model before loading model
     loaded_card.load_onnx_model()
     assert loaded_card.onnx_model is not None
     assert loaded_card.onnx_model.sess is not None
     assert isinstance(loaded_card.onnx_model.sess, Pipeline)
-    
+
     # loading onnx model should also load preprocessors
     assert type(loaded_card.preprocessor) == type(modelcard.interface.feature_extractor)
-    assert loaded_card.interface.model is None # model should still be none
-
+    assert loaded_card.interface.model is None  # model should still be none
 
     loaded_card.load_model()
     assert type(loaded_card.model) == type(modelcard.interface.model)
     assert isinstance(loaded_card.model, Pipeline)
-    
+
     metadata = loaded_card.model_metadata
-    
+
     assert getattr(metadata, "feature_extractor_uri", None) is not None
     assert getattr(metadata, "feature_extractor_name", None) is not None
