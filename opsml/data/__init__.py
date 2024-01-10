92abc5d81486d57c7d9151c146679afbb1f106b8
thorrester <steven.forrester@shipt.com>
2024-01-10 14:34:26 +0000

    update custom interface loading

--- opsml/data/__init__.py
+++ opsml/data/__init__.py
@@ -4,5 +4,14 @@ from opsml.data.interfaces._pandas import PandasData
 from opsml.data.interfaces._polars import PolarsData
 from opsml.data.interfaces._sql import SqlData
 from opsml.data.interfaces._torch import TorchData
+from opsml.data.interfaces._base import DataInterface
 
-__all__ = ["ArrowData", "NumpyData", "PandasData", "PolarsData", "SqlData", "TorchData"]
+__all__ = [
+    "ArrowData",
+    "NumpyData",
+    "PandasData",
+    "PolarsData",
+    "SqlData",
+    "TorchData",
+    "DataInterface",
+]
