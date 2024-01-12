58288bfe49f5012cf977b434238129ebff17fea2
Damon Allison <damon@shipt.com>
2024-01-12 13:29:11 -0600

    tests

--- tests/test_storage/test_storage.py
+++ tests/test_storage/test_storage.py
@@ -25,12 +25,27 @@ def test_local_storage_client_crud(tmp_path: Path, local_storage_client: Storage
     rpath2 = Path(tmp_path, "some/nested/folder/structure/cats.jpg")
 
     local_storage_client.put(lpath, rpath)
+    local_storage_client.put(lpath, rpath)  # verify overwrite
     assert local_storage_client.exists(rpath)
 
     local_storage_client.copy(rpath, rpath2)
     assert local_storage_client.exists(rpath2)
     assert len(local_storage_client.find(tmp_path)) == 2
+    assert [str(rpath), str(rpath2)] == local_storage_client.find(tmp_path)
+
+    local_storage_client.rm(rpath)
+    with pytest.raises(FileNotFoundError):
+        local_storage_client.rm(invalid_path)
+    assert len(local_storage_client.find(tmp_path)) == 1
+    assert str(rpath2) == local_storage_client.find(tmp_path)[0]
 
 
 def test_local_storage_client_trees(tmp_path: Path, local_storage_client: StorageClient):
-    pass
+    # TODO(@damon): Finish trees
+    child = Path(tmp_path, "child")
+    grand_child = Path(tmp_path, "grandchild")
+    for path in [tmp_path, child, grand_child]:
+        path.mkdir(parents=True, exist_ok=True)
+        Path(path, "test.txt").write_text("hello, world")
+
+    assert len(local_storage_client.find(tmp_path)) == 3
