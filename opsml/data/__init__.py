975adb376d2dd3cf07a74c7dbcda22e321d2afbb
thorrester <steven.forrester@shipt.com>
2024-01-15 15:04:36 +0000

    formatting
    Former-commit-id: ab47367c8cc6cee7757e0e6ad4ea9e0970d03e50

--- opsml/data/__init__.py
+++ opsml/data/__init__.py
@@ -1,12 +1,12 @@
 from opsml.data.interfaces._arrow import ArrowData
 from opsml.data.interfaces._base import DataInterface
-from opsml.data.interfaces.custom_data.base import Dataset
 from opsml.data.interfaces._image import ImageData
 from opsml.data.interfaces._numpy import NumpyData
 from opsml.data.interfaces._pandas import PandasData
 from opsml.data.interfaces._polars import PolarsData
 from opsml.data.interfaces._sql import SqlData
 from opsml.data.interfaces._torch import TorchData
+from opsml.data.interfaces.custom_data.base import Dataset
 from opsml.data.splitter import DataSplit
 
 try:
