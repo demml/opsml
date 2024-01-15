026086704850f9811b81717e1af82b8b77596d77
thorrester <steven.forrester@shipt.com>
2024-01-15 20:31:35 +0000

    docs
    Former-commit-id: 9a6ed4b93699c07924fc0115d7215a082743613d

--- opsml/cards/data.py
+++ opsml/cards/data.py
@@ -126,7 +126,7 @@ class DataCard(ArtifactCard):
                 Percentage is expressed as a decimal (e.g. 1 = 100%, 0.5 = 50%, etc.)
 
         """
-        assert isinstance(self.interface, DataInterface), "Data profile can only be created for a DataInferace subclasses"
+        assert isinstance(self.interface, DataInterface), "Data profile can only be created for a DataInterface subclasses"
         self.interface.create_data_profile(sample_perc, self.name)
 
     def split_data(self) -> DataHolder:
