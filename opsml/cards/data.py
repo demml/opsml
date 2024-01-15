6c3b477474d921387399f7d2f719e8334f8df0c4
thorrester <steven.forrester@shipt.com>
2024-01-15 20:30:57 +0000

    docs
    Former-commit-id: 0f5a5e52e90665712adf5347306348c475c012b0

--- opsml/cards/data.py
+++ opsml/cards/data.py
@@ -63,7 +63,7 @@ class DataCard(ArtifactCard):
             kwargs:
                 Keyword arguments to pass to the data loader
 
-            ---- Supported kwargs ----
+            ---- Supported kwargs for ImageData and TextData ----
 
             split:
                 Split to use for data. If not provided, then all data will be loaded.
@@ -126,9 +126,7 @@ class DataCard(ArtifactCard):
                 Percentage is expressed as a decimal (e.g. 1 = 100%, 0.5 = 50%, etc.)
 
         """
-        assert isinstance(
-            self.interface, DataInterface
-        ), "Data profile can only be created for a DataInferace subclasses"
+        assert isinstance(self.interface, DataInterface), "Data profile can only be created for a DataInferace subclasses"
         self.interface.create_data_profile(sample_perc, self.name)
 
     def split_data(self) -> DataHolder:
@@ -146,9 +144,7 @@ class DataCard(ArtifactCard):
     @property
     def data(self) -> Any:
         """Returns data"""
-        assert isinstance(
-            self.interface, DataInterface
-        ), "Data attribute is only supported for DataInterface subclasses"
+        assert isinstance(self.interface, DataInterface), "Data attribute is only supported for DataInterface subclasses"
         return self.interface.data
 
     @property
