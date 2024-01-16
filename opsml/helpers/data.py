6917c2ef4f75951d960ad1a33bb0c62a946eccd8
thorrester <steven.forrester@shipt.com>
2024-01-16 15:29:58 +0000

    pipeline example

--- opsml/helpers/data.py
+++ opsml/helpers/data.py
@@ -10,6 +10,7 @@ def create_fake_data(
     n_samples: int = 100,
     n_features: int = 10,
     n_classes: int = 2,
+    n_categorical_features: int = 0,
     task_type: str = "classification",
     random_state: int = 42,
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
@@ -30,14 +31,27 @@ def create_fake_data(
     Returns:
         Tuple of pd.DataFrame
     """
+    num_features = n_features - n_categorical_features
+
     np.random.seed(random_state)
-    X = np.random.randn(n_samples, n_features)  # pylint: disable=invalid-name
+    X = np.random.randn(n_samples, num_features)  # pylint: disable=invalid-name
+
+    if n_categorical_features > 0:
+        cat_cols = np.random.randint(0, n_classes, size=(n_samples, n_categorical_features)).astype(str)
+
     y = np.random.randint(0, n_classes, n_samples)  # pylint: disable=invalid-name
     if task_type == "regression":
         y = np.random.randn(n_samples)  # pylint: disable=invalid-name
 
     # rename columns
-    X = pd.DataFrame(X, columns=[f"col_{i}" for i in range(n_features)])  # pylint: disable=invalid-name
+    X = pd.DataFrame(X, columns=[f"col_{i}" for i in range(num_features)])  # pylint: disable=invalid-name
+
+    if n_categorical_features > 0:
+        cat_df = pd.DataFrame(cat_cols, columns=[f"cat_col_{i}" for i in range(n_categorical_features)])
+
+        # add to X
+        X = pd.concat([X, cat_df], axis=1)
+
     y = pd.DataFrame(y, columns=["target"])  # pylint: disable=invalid-name
 
-    return pd.DataFrame(X), pd.DataFrame(y)
+    return X, pd.DataFrame(y)
