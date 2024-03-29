d86f91d80f5c7aaf49b5f4ed0c3cd3bb27fd3f7b
Thorrester <steven.forrester@shipt.com>
2024-03-29 17:26:45 -0400

    flexbox

--- opsml/app/static/src/routes/opsml/model/card/+page.svelte
+++ opsml/app/static/src/routes/opsml/model/card/+page.svelte
@@ -2,7 +2,7 @@
 
   import { TabGroup, Tab } from '@skeletonlabs/skeleton';
   import Fa from 'svelte-fa'
-  import { faTag } from '@fortawesome/free-solid-svg-icons'
+  import { faTag, faIdCard, faFolderOpen } from '@fortawesome/free-solid-svg-icons'
   import { onMount } from 'svelte';
   import { keymap } from "@codemirror/view"
 
@@ -66,8 +66,8 @@
 
 <div class="flex flex-1 flex-col">
 
-  <div class="pl-12 sm:pl-20 pt-6 sm:pt-8 bg-slate-50 w-full pb-8 border-b">
-    <h1 class="flex flex-row flex-wrap ... items-center text-lg">
+  <div class="pl-4 md:pl-20 pt-6 sm:pt-8 bg-slate-50 w-full pb-8 border-b">
+    <h1 class="flex flex-row flex-wrap items-center text-lg">
       <div class="group flex flex-none items-center">
         <a class="font-semibold text-gray-800 hover:text-secondary-500" href="/opsml/{registry}?repository={repository}">{repository}</a>
         <div class="mx-0.5 text-gray-800">/</div>
@@ -76,19 +76,20 @@
     </h1>
     <div class="pt-2 flex flex-wrap flex-row items-center gap-3">
       <div>
-        <a href="/opsml/{registry}/card?name={name}&repository={repository}&version={metadata.model_version}" class="badge h-7 bg-surface-100 border border-surface-300 hover:bg-gradient-to-b from-surface-50 to-primary-100">
+        <a href="/opsml/{registry}/card?name={name}&repository={repository}&version={metadata.model_version}" class="badge h-7 border border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100">
           <Fa class="h-7" icon={faTag} color="#4b3978"/>
           <span class="text-primary-500">{metadata.model_version}</span>
         </a>
       </div>
-
-      <CardBadgeinterface={metadata.interface}/>
+      <div>
+        <CardBadge artifactInterface={metadata.model_interface}/>
+      </div>
     </div>
   </div>
 
-
-  <div class="flex flex-row flex-initial">
-    <div class="w-2/3 border-r border-grey-100 pl-12 sm:pl-20 ">
+  
+  <div class="flex flex-wrap">
+    <div class="w-full md:w-2/3 border-r border-grey-100 pl-4 md:pl-20">
       {#if !hasReadme}
         <div class="mt-5 mr-5 py-24 bg-gradient-to-b from-secondary-50 to-white rounded-lg text-center items-center">
           <p class="mb-1">No card README found</p>
@@ -103,10 +104,49 @@
           <div id="editor" class="h-96"></div>
       {/if}
     </div>
-    <div class="flex flex-row flex-wrap w-1/3 mt-5">
-      <div class="px-8">
-        <header class="text-lg font-bold">Metadata</header>
+    <div class="w-full md:w-1/3 mt-5 pl-4">
+      <div class="pl-4">
+        <header class="mb-2 text-lg font-bold">Metadata</header>
+
+        <!-- UID -->
+        <div class="flex flex-row gap-1 items-center">
+          <div class="w-8">
+            <Fa class="h-12" icon={faIdCard} color="#04cd9b"/>
+          </div>
+          <div class="w-32 font-semibold text-gray-500">ID:</div>
+          <div class="w-48 ">0uyoiulkjhoiuho</div>
+        </div>
+
+        <!-- name -->
+        <div class="flex flex-row gap-1 items-center">
+          <div class="w-8">
+            <Fa class="h-12" icon={faIdCard} color="#04cd9b"/>
+          </div>
+          <div class="w-32 font-semibold text-gray-500">Name:</div>
+          <div class="w-48 ">{metadata.model_name}</div>
+        </div>
+
+        <!-- version -->
+        <div class="flex flex-row gap-1 items-center">
+          <div class="w-8">
+            <Fa class="h-12" icon={faIdCard} color="#04cd9b"/>
+          </div>
+          <div class="w-32 font-semibold text-gray-500">Version:</div>
+          <div class="w-48 ">{metadata.model_version}</div>
+        </div>
+
+
+        <!-- repository -->
+        <div class="flex flex-row gap-1 items-center">
+          <div class="w-8">
+            <Fa class="h-12" icon={faFolderOpen} color="#04cd9b"/>
+          </div>
+          <div class="w-32 font-semibold text-gray-500">Respository:</div>
+          <div class="w-48 ">{metadata.model_repository}</div>
+        </div>
+
       </div>
+      
     </div>
 
   </div>
