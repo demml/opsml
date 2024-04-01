07283213ec46b0e10cc2f4973c46a5ebf9dcbe50
Thorrester <steven.forrester@shipt.com>
2024-04-01 12:57:56 -0400

    file pathing and ui

--- opsml/app/static/src/routes/opsml/model/card/files/+page.svelte
+++ opsml/app/static/src/routes/opsml/model/card/files/+page.svelte
@@ -23,9 +23,9 @@
 <div class="flex flex-wrap">
   <div class="w-full md-full px-4 pt-8 md:px-20">
     <div class="bg-gradient-to-b from-primary-50 to-white flex rounded-t-lg border border-gray px-3 py-2">
-      <div class="flex flex-row gap-1 items-center">
+      <div class="flex flex-row justify-between gap-1 items-center">
         <div class="text-primary-500 font-semibold">Files for {name}</div>
-        <div class="text-gray-500 justify-end"> {modifiedAt}</div>
+        <div class="text-gray-500"> {modifiedAt}</div>
       </div>
     </div>
   </div>
