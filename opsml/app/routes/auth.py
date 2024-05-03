b6ba2cbfb9dfbb93aadd6b7f1fac306c7c8545f5
Thorrester <steven.forrester@shipt.com>
2024-05-03 12:08:13 -0400

    adding in routes
    Former-commit-id: c3ec30aa13a7872c828edf5af92eb6673e2ef183

--- opsml/app/routes/auth.py
+++ opsml/app/routes/auth.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import APIRouter, Request, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
-
+import bcrypt
 from opsml.helpers.logging import ArtifactLogger
+from opsml.settings.config import config
+from opsml.types import RegistryType
+from typing import Annotated, Union
+from opsml.registry.sql.base.server import ServerAuthRegistry
+from datetime import datetime, timedelta, timezone
 
 logger = ArtifactLogger.get_logger()
 
@@ -22,20 +27,44 @@ class TokenData(BaseModel):
     username: str | None = None
 
 
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    is_active: bool = False
-
-
-class UserInDB(User):
-    hashed_password: str
-
-
 # def verify_password(plain_password, hashed_password):
 # return pwd_context.verify(plain_password, hashed_password)
 
+# only set auth routes if auth is enabled
+
+if config.opsml_auth:
+    router = APIRouter()
+
+    @router.post("/token", response_model=Token)
+    async def login_for_access_token(request: Request, username: str, password: str):
+        pass
+
+    @app.post("/token")
+    async def login_for_access_token(
+        request: Request,
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    ) -> Token:
+        db: ServerAuthRegistry = request.app.state.auth_db
+
+        user = db.get_user(form_data.username)
+
+        if user is None:
+            # reroute to login/register page
+            pass
+
+        # check if password is correct
+        authenicated = db.authenticate_user(user, form_data.password)
+
+        if not authenicated:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+        return Token(access_token=access_token, token_type="bearer")
 
 # def get_password_hash(password):
 # return pwd_context.hash(password)
