dfd0b90593ee171de6bab767a593ddf0961490f9
Thorrester <sjforrester32@gmail.com>
2024-08-23 16:43:04 -0400

    lints

--- tests/test_app/test_client.py
+++ tests/test_app/test_client.py
@@ -37,7 +37,10 @@ def test_debug(test_app: TestClient) -> None:
 
 
 def test_register_data(
-    api_registries: CardRegistries, api_storage_client: client.StorageClient, pandas_data: PandasData
+    test_app: TestClient,
+    api_registries: CardRegistries,
+    api_storage_client: client.StorageClient,
+    pandas_data: PandasData,
 ) -> None:
     assert pandas_data.data is not None
 
@@ -87,6 +90,16 @@ def test_register_data(
     assert "mlops" in info.repositories
     assert info.names is not None
     assert "test-df" in info.names
+
+    # test ui routes for cards
+    response = test_app.get("/opsml/data")
+    assert response.status_code == 200
+
+    # test ui routes for cards
+    response = test_app.get(
+        f"/opsml/data/card?name={datacard.name}&repository={datacard.repository}&version={datacard.version}&uid={datacard.uid}"
+    )
+    assert response.status_code == 200
 
 
 def test_register_major_minor(api_registries: CardRegistries, numpy_data: NumpyData) -> None:
@@ -425,10 +438,22 @@ def test_card_list_fail(test_app: TestClient) -> None:
 
 
 ##### Test ui routes
-def test_homepage(test_app: TestClient) -> None:
+def test_ui(test_app: TestClient) -> None:
     """Test settings"""
 
     response = test_app.get("/opsml")
+    assert response.status_code == 200
+
+    response = test_app.get("/opsml")
+    assert response.status_code == 200
+
+    response = test_app.get("/opsml/error/page?message=blah")
+    assert response.status_code == 200
+
+    response = test_app.get("/opsml/auth/login")
+    assert response.status_code == 200
+
+    response = test_app.get("/opsml/auth/register")
     assert response.status_code == 200
 
 
