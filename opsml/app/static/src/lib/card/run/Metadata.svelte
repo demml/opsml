9c15e78ba050ce9f4e2d017babd43da55b2b2988
Thorrester <sjforrester32@gmail.com>
2024-08-28 22:46:00 -0400

    ui logic
    Former-commit-id: 8eb706e2abdb3a24fdd922c53234df397d3ef50a

--- opsml/app/static/src/lib/card/run/Metadata.svelte
+++ opsml/app/static/src/lib/card/run/Metadata.svelte
@@ -3,16 +3,33 @@
   import { type RunCard , type Card } from "$lib/scripts/types";
   import { loadModal } from "$lib";
   import Fa from 'svelte-fa'
-  import { faTag, faFileContract, faCircleInfo} from '@fortawesome/free-solid-svg-icons'
+  import { faTag, faFileContract, faCircleInfo, faComputer} from '@fortawesome/free-solid-svg-icons'
 
   
   import { type ModalStore} from '@skeletonlabs/skeleton';
   import CodeModal from '$lib/components/CodeModal.svelte';
   import type { ModalComponent, ModalSettings } from '@skeletonlabs/skeleton';
+  import { python } from "svelte-highlight/languages";
+  import MetadataPill from "$lib/components/MetadataPill.svelte";
+  import MetadataPillList from "$lib/components/MetadataPillList.svelte";
   
   export let metadata: RunCard;
   export let card: Card;
   let registry: string = "run";
+
+  let memory: string = `${(metadata.compute_environment.memory / (1024**3)).toFixed(1)} GB`;
+  let disk_space: string = `${(metadata.compute_environment.disk_space / (1024**3)).toFixed(2)} GB`;
+  
+  function getGpuMemory(memory: Map<string, number>): string {
+    let gpu_memory: number = 0;
+    const keys = Object.keys(memory);
+
+    keys.forEach((key) => {
+      gpu_memory += memory[key];
+    });
+  
+    return `${(gpu_memory / (1024**3)).toFixed(2)} GB`;
+  }
   
   const modalStore: ModalStore = loadModal();
   
@@ -52,34 +69,85 @@
   </div>
   
   <div class="flex flex-col space-y-1">
-    <div class="inline-flex items-center overflow-hidden rounded-lg border border-darkpurple text-sm w-fit">
-      <div class="border-r border-darkpurple px-2 text-darkpurple bg-primary-50 italic">ID</div> 
-      <div class="flex px-1.5 bg-surface-50 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 text-darkpurple">
-        {card.uid}
-      </div>
-    </div>
-  
-    <div class="inline-flex items-center overflow-hidden rounded-lg border border-darkpurple text-sm w-fit">
-      <div class="border-r border-darkpurple px-2 text-darkpurple bg-primary-50 italic">Name</div> 
-      <div class="flex px-1.5 bg-surface-50 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 text-darkpurple">
-        {metadata.name}
-      </div>
-    </div>
-  
-    <div class="inline-flex items-center overflow-hidden rounded-lg border border-darkpurple text-sm w-fit">
-      <div class="border-r border-darkpurple px-2 text-darkpurple bg-primary-50 italic">Repository</div> 
-      <div class="flex px-1.5 bg-surface-50 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 text-darkpurple">
-        {metadata.repository}
-      </div>
-    </div>
 
-    <div class="inline-flex items-center overflow-hidden rounded-lg border border-darkpurple text-sm w-fit">
-      <div class="border-r border-darkpurple px-2 text-darkpurple bg-primary-50 italic">Version</div> 
-      <div class="flex px-1.5 bg-surface-50 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 text-darkpurple">
-        {metadata.version}
-      </div>
+    <MetadataPill title="ID" value={card.uid} colspan={undefined}/>
+    <MetadataPill title="Name" value={metadata.name} colspan={undefined}/>
+    <MetadataPill title="Repository" value={metadata.repository} colspan={undefined}/>
+    <MetadataPill title="Version" value={metadata.version} colspan={undefined}/>
+
+  </div>
+
+  <div class="flex flex-row items-center mb-2 pt-2 border-b-2 border-gray-400">
+    <Fa icon={faComputer} color="#04cd9b"/>
+    <header class="pl-2 text-darkpurple text-lg font-bold">Compute</header>
+  </div>
+
+  <div class="px-2 pb-2 mb-2 border-dashed border-2 border-gray-400 rounded-md shadow-inner">
+    
+    <header class="pl-2 text-darkpurple text-md font-semibold">System</header>
+    <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 items-center gap-1">
+
+      <MetadataPill title="CPU count" value={metadata.compute_environment.cpu_count} colspan={undefined}/>
+      <MetadataPill title="Memory" value={memory} colspan={undefined}/>
+      <MetadataPill title="Disk Space" value={disk_space} colspan="lg:col-span-2 xl:col-span-2"/>
+
     </div>
   </div>
+
+  <div class="px-2 pb-2 mb-2 border-dashed border-2 border-gray-400 rounded-md shadow-inner">
+    
+    <header class="pl-2 text-darkpurple text-md font-semibold">Architecture</header>
+    <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3  items-center gap-1">
+
+      <MetadataPill title="System" value={metadata.compute_environment.system} colspan={undefined}/>
+      <MetadataPill title="Release" value={metadata.compute_environment.release} colspan={undefined}/>
+
+      <MetadataPill 
+        title="Bits" 
+        value={metadata.compute_environment.architecture_bits} 
+        colspan={undefined}/>
+    </div>
+  </div>
+
+  <div class="px-2 pb-2 mb-2 border-dashed border-2 border-gray-400 rounded-md shadow-inner">
+    
+    <header class="pl-2 text-darkpurple text-md font-semibold">Python</header>
+    <div class="grid grid-cols-1 lg:grid-cols-2 items-center gap-1 overflow-hidden">
+
+      <MetadataPill 
+      title="Python" 
+      value={metadata.compute_environment.python_version}
+      colspan="col-span-2"/>
+
+      <MetadataPill 
+      title="Compiler" 
+      value={metadata.compute_environment.python_compiler}
+      colspan="col-span-2"/>
+
+    </div>
+  </div>
+
+  {#if metadata.compute_environment.gpu_count > 0}
+    <div class="px-2 pb-2 mb-2 border-dashed border-2 border-gray-400 rounded-md shadow-inner">
+      
+      <header class="pl-2 text-darkpurple text-md font-semibold">GPU</header>
+      <div class="grid grid-cols-1 lg:grid-cols-2 items-center gap-1 overflow-hidden">
+
+        <MetadataPill 
+        title="GPU count" 
+        value={metadata.compute_environment.gpu_count}
+        colspan={undefined}/>
+
+        <MetadataPill 
+        title="GPU memory" 
+        value={getGpuMemory(metadata.compute_environment.gpu_device_memory)}
+        colspan={undefined}/>
+
+      </div>
+    </div>
+  {/if}
+
+
 
   {#if card.datacard_uids.length > 0 || card.modelcard_uids.length > 0}
 
@@ -91,37 +159,11 @@
   <div class="flex flex-col space-y-1">
 
     {#if card.datacard_uids.length > 0}
-      <div class="inline-flex items-center overflow-hidden rounded-lg border border-darkpurple text-sm w-fit">
-        <div class="border-r border-darkpurple px-2 text-darkpurple bg-primary-50 italic">DataCards</div> 
-        <div class="flex px-1.5 bg-surface-50 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 text-darkpurple">
-            <div class="flex flex-col">
-                {#each card.datacard_uids as uid}
-                  <div>
-                    <a href="/opsml/data/card?uid={uid}" class="badge h-6 border text-primary-500 bg-surface-50 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 overflow-auto">
-                      {uid.slice(0, 7)}
-                    </a>
-                  </div>
-                {/each}
-            </div>
-        </div>
-      </div>
+      <MetadataPillList title="DataCards" uids={card.datacard_uids} type="data" />
     {/if}
 
     {#if card.modelcard_uids.length > 0}
-      <div class="inline-flex items-center overflow-hidden rounded-lg border border-darkpurple text-sm w-fit">
-        <div class="border-r border-darkpurple px-2 text-darkpurple bg-primary-50 italic">ModelCards</div> 
-        <div class="flex px-1.5 bg-surface-50 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 text-darkpurple">
-            <div class="flex flex-col">
-                {#each card.modelcard_uids as uid}
-                  <div>
-                    <a href="/opsml/model/card?uid={uid}" class="badge h-6 border text-primary-500 bg-surface-50 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 overflow-auto">
-                      {uid.slice(0, 7)}
-                    </a>
-                  </div>
-                {/each}
-            </div>
-        </div>
-      </div>
+      <MetadataPillList title="ModelCards" uids={card.modelcard_uids} type="model" />
     {/if}
   </div>
   {/if}
---
+++ opsml/app/static/src/lib/components/MetadataPillList.svelte
@@ -0,0 +1,26 @@
+
+
+<script lang="ts">
+    export let title: string;
+    export let uids: string[];
+    export let type: string;
+  
+  </script>
+  
+  
+  <div class="inline-flex items-center overflow-hidden rounded-lg border border-darkpurple text-sm w-fit h-7 bg-primary-50">
+    <div class="border-r border-darkpurple px-2 text-darkpurple bg-primary-50 italic">{title}</div> 
+    <div class="flex px-1.5 bg-surface-50 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 text-darkpurple">
+        <div class="flex flex-col">
+            {#each uids as uid}
+              <div>
+                <a href="/opsml/{type}/card?uid={uid}" class="badge border text-primary-500 bg-surface-100 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 overflow-auto">
+                  {uid.slice(0, 7)}
+                </a>
+              </div>
+            {/each}
+        </div>
+    </div>
+  </div>
+  
+  
