807e601d189c6bf89a575478e1633b3b4e2610c7
Thorrester <sjforrester32@gmail.com>
2024-09-02 16:35:07 -0400

    no tension
    Former-commit-id: ae9b06d49fa64067372fb9ebdf8c5d7ea6071bda

--- opsml/app/static/src/lib/scripts/utils.ts
+++ opsml/app/static/src/lib/scripts/utils.ts
@@ -1223,7 +1223,6 @@ export function createTimeSeriesChart(
     backgroundColor: backgroundColors[2],
     pointRadius: 2,
     fill: true,
-    tension: 0.4,
   });
 
   return buildTimeChart(x, datasets, "Time", y_label, false);
@@ -1246,7 +1245,6 @@ export function createTimeSeriesGroupedChart(
       backgroundColor: colors[index + 1],
       pointRadius: 2,
       fill: false,
-      tension: 0.4,
     });
   });
 
