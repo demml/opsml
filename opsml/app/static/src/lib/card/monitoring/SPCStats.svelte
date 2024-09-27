038be892458dddc585da80656b93b44a7b8937f8
Thorrester <sjforrester32@gmail.com>
2024-09-27 11:01:58 -0400

    redoing layout
    Former-commit-id: f84302bf3b5c5fa546df6d41914d028eb76b40af

---
+++ opsml/app/static/src/lib/card/monitoring/SPCStats.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+
+    import { type FeatureDriftProfile } from "$lib/scripts/types";
+  
+    export let feature_profile: FeatureDriftProfile;
+
+  </script>
+  
+    <table class="table-compact table-cell-fit table-hover text-xs text-center min-w-full">
+      <thead class="bg-primary-200 sticky top-0">
+        <tr>
+          <th class="text-sm text-center py-2">Zone</th>
+          <th class="text-sm text-center py-2">Lower</th>
+          <th class="text-sm text-center py-2">Upper</th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr class="even:bg-gray-100">
+            <td class="text-sm">Center</td>
+            <td class="text-sm">{feature_profile.center}</td>
+            <td class="text-sm">{feature_profile.center}</td>
+          </tr>
+          <tr class="even:bg-gray-100">
+            <td class="text-sm">Zone 1</td>
+            <td class="text-sm">{feature_profile.one_lcl}</td>
+            <td class="text-sm">{feature_profile.one_ucl}</td>
+          </tr>
+          <tr class="even:bg-gray-100">
+            <td class="text-sm">Zone 2</td>
+            <td class="text-sm">{feature_profile.two_lcl}</td>
+            <td class="text-sm">{feature_profile.two_ucl}</td>
+          </tr>
+          <tr class="even:bg-gray-100">
+            <td class="text-sm">Zone 3</td>
+            <td class="text-sm">{feature_profile.three_lcl}</td>
+            <td class="text-sm">{feature_profile.three_ucl}</td>
+          </tr>
+      </tbody>
+    </table>
+
+  
