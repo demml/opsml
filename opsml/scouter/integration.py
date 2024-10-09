7ed13c3b0eacebf41ef4caa4b4e512c6a9523dd1
Thorrester <sjforrester32@gmail.com>
2024-10-09 14:03:48 -0400

    add files
    Former-commit-id: 072c9dcd5c1ca1ee6b2f446429a9178084e1b911

---
+++ opsml/scouter/integration.py
@@ -0,0 +1,142 @@
+from functools import cached_property
+from typing import Any, Dict, Optional, Union
+
+from opsml.scouter import DriftType
+from opsml.scouter.client import ScouterApiClient
+from opsml.scouter.server import SCOUTER_SERVER_CLIENT as scouter_server_client
+from opsml.scouter.server import ScouterServerClient
+from opsml.settings.config import config
+from opsml.storage import client
+from opsml.storage.client import ApiStorageClient
+
+
+def _set_client() -> Optional[Union[ScouterServerClient, ScouterApiClient]]:
+    """Helper function to set the client based whether opsml is running in server or client mode"""
+
+    if config.is_tracking_local:
+        if scouter_server_client is None:
+            return None
+
+        assert isinstance(scouter_server_client, ScouterServerClient)
+        return scouter_server_client
+
+    assert isinstance(client.storage_client, ApiStorageClient)
+    return ScouterApiClient()
+
+
+class ScouterClient:
+    """Helper class for scouter integration"""
+
+    def __init__(self) -> None:
+        """Setup scouter client. Will also check if scouter server is up"""
+        self._client = _set_client()
+        self._scouter_set = self._client is not None and self.healthcheck()
+
+    @property
+    def server_running(self) -> bool:
+        """Check if scouter server is running"""
+        return self._scouter_set
+
+    @cached_property
+    def _scouter_client(self) -> Union[ScouterServerClient, ScouterApiClient]:
+        assert self._client is not None
+        return self._client
+
+    def healthcheck(self) -> bool:
+        """Checks if scouter server is up
+
+        Returns:
+            True if server is up, False otherwise
+        """
+        assert self._client is not None
+        return self._client.healthcheck()
+
+    def update_profile_status(self, repository: str, name: str, version: str, status: bool) -> None:
+        """Sets the profile to active
+
+        Args:
+            repository:
+                Model repository
+            name:
+                Model name
+            version:
+                Model version
+            status:
+                Status to set
+        """
+
+        self._scouter_client.update_drift_profile_status(
+            repository=repository,
+            name=name,
+            version=version,
+            status=status,
+        )
+
+    def insert_drift_profile(self, drift_profile: str, drift_type: DriftType) -> None:
+        """Inserts drift profile into scouter server
+
+        Args:
+            drift_profile:
+                Drift profile to insert
+            drift_type:
+                Drift type
+        """
+        self._scouter_client.insert_drift_profile(
+            drift_profile=drift_profile,
+            drift_type=drift_type,
+        )
+
+    def update_drift_profile(
+        self,
+        repository: str,
+        name: str,
+        version: str,
+        drift_profile: str,
+        drift_type: DriftType,
+        save: bool = False,
+    ) -> None:
+        """Updates drift profile into scouter server
+
+        Args:
+            repository:
+                Model repository
+            name:
+                Model name
+            version:
+                Model version
+            save:
+                Save flag
+            drift_profile:
+                Drift profile
+            drift_type:
+                Drift type
+        """
+
+        self._scouter_client.update_drift_profile(
+            repository=repository,
+            name=name,
+            version=version,
+            save=save,
+            drift_profile=drift_profile,
+            drift_type=drift_type,
+        )
+
+    def get_drift_profile(self, repository: str, name: str, version: str) -> Optional[Dict[str, Any]]:
+        """Get drift profile from scouter server
+
+        Args:
+            repository:
+                Model repository
+            name:
+                Model name
+            version:
+                Model version
+
+        Returns:
+            Drift profile
+        """
+        return self._scouter_client.get_drift_profile(
+            repository=repository,
+            name=name,
+            version=version,
+        )
---
+++ opsml/scouter/server.py
@@ -0,0 +1,340 @@
+"""This is the Scouter Server Integration that is used when opsml is running in server mode.
+Scouter is a separate server that is used to store drift profiles and monitor model drift.
+"""
+
+import json
+from typing import Any, Dict, List, Optional, Union, cast
+
+from opsml.helpers.logging import ArtifactLogger
+from opsml.scouter import DriftType
+from opsml.settings.config import config
+from opsml.storage.api import ApiClient, RequestType
+
+logger = ArtifactLogger.get_logger()
+
+
+class ScouterRoutes:
+    DRIFT = "drift"
+    FEATURE_DISTRIBUTION = "feature/distribution"
+    HEALTHCHECK = "healthcheck"
+    PROFILE = "profile"
+    PROFILE_STATUS = "profile/status"
+    ALERTS = "alerts"
+    ALERT_METRICS = "alerts/metrics"
+
+
+class ScouterServerClient(ApiClient):
+    def healthcheck(self) -> bool:
+        """Checks if scouter server is up
+
+        Returns:
+            True if server is up, False otherwise
+        """
+        response = self.request(route=ScouterRoutes.HEALTHCHECK, request_type=RequestType.GET)
+
+        return cast(str, response["message"].lower()) == "alive"
+
+    def insert_drift_profile(self, drift_profile: str, drift_type: DriftType) -> None:
+        """Inserts drift profile into scouter server
+
+        Args:
+            drift_profile:
+                Drift profile to insert
+            drift_type:
+                Drift type
+        """
+        data = {"profile": json.loads(drift_profile), "drift_type": drift_type.value}
+        self.request(route=ScouterRoutes.PROFILE, request_type=RequestType.POST, json=data)
+
+    def get_drift_profile(self, repository: str, name: str, version: str) -> Optional[Dict[str, Any]]:
+        """Get drift profile from scouter server
+
+        Args:
+            repository:
+                Model repository
+            name:
+                Model name
+            version:
+                Model version
+
+        Returns:
+            Drift profile
+        """
+        response = self.request(
+            route=ScouterRoutes.PROFILE,
+            request_type=RequestType.GET,
+            params={
+                "repository": repository,
+                "name": name,
+                "version": version,
+            },
+        )
+
+        if response["status"] == "error":
+            return None
+
+        return cast(Dict[str, Any], response["data"])
+
+    def update_drift_profile(
+        self, repository: str, name: str, version: str, drift_profile: str, drift_type: DriftType, save: bool = False
+    ) -> Dict[str, str]:
+        """Updates drift profile into scouter server
+
+        Args:
+            name:
+                Model name
+            repository:
+                Model repository
+            version:
+                Model version
+            drift_profile:
+                Drift profile to insert
+            drift_type:
+                Drift type
+            save:
+                Save flag
+        """
+        save  # unused arg to avoid linting error and maintain compatibility with the ApiClient
+        logger.info("Updating scouter server drift profile for {}/{}/{}", repository, name, version)
+
+        data = {"profile": json.loads(drift_profile), "drift_type": drift_type.value}
+        return self.request(route=ScouterRoutes.PROFILE, request_type=RequestType.PUT, json=data)
+
+    def update_drift_profile_status(self, repository: str, name: str, version: str, status: bool) -> Dict[str, str]:
+        """Updates drift profile status into scouter server
+
+        Args:
+            update_request:
+                UpdateProfileStatus object
+        """
+        return self.request(
+            route=ScouterRoutes.PROFILE_STATUS,
+            request_type=RequestType.PUT,
+            json={
+                "repository": repository,
+                "name": name,
+                "version": version,
+                "status": status,
+            },
+        )
+
+    def get_drift_values(
+        self,
+        repository: str,
+        name: str,
+        version: str,
+        time_window: str,
+        max_data_points: int,
+        feature: Optional[str] = None,
+    ) -> Dict[str, Any]:
+        """Get drift values from scouter server
+
+        Args:
+            repository:
+                Model repository
+            name:
+                Model name
+            version:
+                Model version
+            time_window:
+                Time window
+            max_data_points:
+                Maximum data points
+            feature:
+                Feature to get drift values for
+
+        Returns:
+            Drift values
+        """
+        params = {
+            "repository": repository,
+            "name": name,
+            "version": version,
+            "time_window": time_window,
+            "max_data_points": max_data_points,
+        }
+
+        if feature:
+            params["feature"] = feature
+
+        response = self.request(route=ScouterRoutes.DRIFT, request_type=RequestType.GET, params=params)
+
+        return cast(Dict[str, Any], response["data"])
+
+    def get_feature_distribution(
+        self,
+        repository: str,
+        name: str,
+        version: str,
+        time_window: str,
+        max_data_points: int,
+        feature: str,
+    ) -> Dict[str, Any]:
+        """Get feature distribution from scouter server
+
+        Args:
+            repository:
+                Model repository
+            name:
+                Model name
+            version:
+                Model version
+            time_window:
+                Time window
+            max_data_points:
+                Maximum data points
+            feature:
+                Feature to get drift values for
+
+        Returns:
+            Drift values
+        """
+        params = {
+            "repository": repository,
+            "name": name,
+            "version": version,
+            "time_window": time_window,
+            "max_data_points": max_data_points,
+            "feature": feature,
+        }
+
+        try:
+            response = self.request(
+                route=ScouterRoutes.FEATURE_DISTRIBUTION,
+                request_type=RequestType.GET,
+                params=params,
+            )
+
+            return cast(Dict[str, Any], response["data"])
+
+        except Exception:  # pylint: disable=broad-except
+            return {}
+
+    def get_monitoring_alerts(
+        self, repository: str, name: str, version: str, active: bool, limit: int
+    ) -> List[Dict[str, Any]]:
+        """Get monitoring alerts from scouter server
+
+        Args:
+            repository:
+                Model repository
+            name:
+                Model name
+            version:
+                Model version
+            active:
+                Active alerts
+            limit:
+                Maximum number of alerts to return
+
+        Returns:
+            Monitoring alerts
+        """
+
+        params = {
+            "repository": repository,
+            "name": name,
+            "version": version,
+            "active": active,
+            "limit": limit,
+        }
+
+        try:
+            response = self.request(
+                route=ScouterRoutes.ALERTS,
+                request_type=RequestType.GET,
+                params=params,
+            )
+
+            return cast(List[Dict[str, Any]], response["data"])
+
+        except Exception:  # pylint: disable=broad-except
+            return []
+
+    def update_monitoring_alerts(self, id_num: int, status: str) -> Dict[str, str]:
+        """Get monitoring alerts from scouter server
+
+        Args:
+            id_num:
+                Monitoring alert id
+            status:
+                Status of monitoring alert
+
+        Returns:
+            Monitoring alerts
+        """
+
+        try:
+            response = self.request(
+                route=ScouterRoutes.ALERTS,
+                request_type=RequestType.PUT,
+                json={"id": id_num, "status": status},
+            )
+
+            return cast(Dict[str, str], response)
+
+        except Exception:  # pylint: disable=broad-except
+            return {"message": "Failed to update monitoring alert", "status": "error"}
+
+    def get_alert_metrics(
+        self,
+        repository: str,
+        name: str,
+        version: str,
+        time_window: str,
+        max_data_points: int,
+    ) -> Dict[str, Union[List[str], List[int]]]:
+        """Get monitoring alerts from scouter server
+
+        Args:
+            repository:
+                Model repository
+            name:
+                Model name
+            version:
+                Model version
+            time_window:
+                Time window
+            max_data_points:
+                Maximum data points
+
+        Returns:
+            Alert metrics is a given time period
+        """
+
+        params = {
+            "repository": repository,
+            "name": name,
+            "version": version,
+            "time_window": time_window,
+            "max_data_points": max_data_points,
+        }
+
+        try:
+            response = self.request(
+                route=ScouterRoutes.ALERT_METRICS,
+                request_type=RequestType.GET,
+                params=params,
+            )
+
+            return cast(Dict[str, Union[List[str], List[int]]], response["data"])
+
+        except Exception:  # pylint: disable=broad-except
+            return {
+                "created_at": [],
+                "alert_count": [],
+                "active": [],
+                "acknowledged": [],
+            }
+
+
+SCOUTER_SERVER_CLIENT = None
+if config.scouter_server_uri is not None:
+    SCOUTER_SERVER_CLIENT = ScouterServerClient(
+        base_url=config.scouter_server_uri,
+        username=config.scouter_username,
+        password=config.scouter_password,
+        use_auth=config.scouter_auth,
+        token=None,
+        path_prefix=config.scouter_path_prefix,
+    )
