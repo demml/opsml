2ce3e1d9171e070aef41c93348e34e1bd497c365
Thorrester <sjforrester32@gmail.com>
2024-10-11 17:07:09 -0400

    adding accordian for observability
    Former-commit-id: b60510b8cec20aab41d1f1a8b9a1795c76268a34

--- opsml/app/static/src/lib/card/monitoring/Observability.svelte
+++ opsml/app/static/src/lib/card/monitoring/Observability.svelte
@@ -5,16 +5,19 @@
     export let routeViz: RouteVizData[];
 </script>
 
-<Accordion>
-	<AccordionItem open>
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<svelte:fragment slot="summary">(summary)</svelte:fragment>
-		<svelte:fragment slot="content">(content)</svelte:fragment>
-	</AccordionItem>
-	<AccordionItem>
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<svelte:fragment slot="summary">(summary)</svelte:fragment>
-		<svelte:fragment slot="content">(content)</svelte:fragment>
-	</AccordionItem>
-	<!-- ... -->
-</Accordion>
+
+<div class="flex flex-col pt-2 pb-16">
+    <div class="text-primary-500 text-2xl font-bold pt-2 pb-1">Observability</div>
+    <div class="border border-2 border-primary-500 w-full mb-2"></div>
+    <Accordion>
+        {#each routeViz as route}
+
+            <AccordionItem class="bg-surface-50 rounded-lg border border-2 border-primary-500">
+                <svelte:fragment slot="summary"><p class="text-primary-500 text-lg font-bold">{route.route_name}</p></svelte:fragment>
+                <svelte:fragment slot="content">(content)</svelte:fragment>
+            </AccordionItem>
+            
+        {/each}
+        <!-- ... -->
+    </Accordion>
+</div>
