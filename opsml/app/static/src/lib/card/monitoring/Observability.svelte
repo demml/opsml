cb4ac805a4187d6b4c4561cb120c45ae176b9e2b
Thorrester <sjforrester32@gmail.com>
2024-10-11 16:50:01 -0400

    add observability visualizations
    Former-commit-id: 05bfcaf084d47c2a4e2be6015553590cb189e307

---
+++ opsml/app/static/src/lib/card/monitoring/Observability.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { RouteVizData } from '$lib/scripts/monitoring/utils';
+    import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+
+    export let routeViz: RouteVizData[];
+</script>
+
+<Accordion>
+	<AccordionItem open>
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<svelte:fragment slot="summary">(summary)</svelte:fragment>
+		<svelte:fragment slot="content">(content)</svelte:fragment>
+	</AccordionItem>
+	<AccordionItem>
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<svelte:fragment slot="summary">(summary)</svelte:fragment>
+		<svelte:fragment slot="content">(content)</svelte:fragment>
+	</AccordionItem>
+	<!-- ... -->
+</Accordion>
