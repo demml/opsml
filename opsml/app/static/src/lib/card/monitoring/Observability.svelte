f354bf36824f6fe245f48a80f718d385636d6768
Thorrester <sjforrester32@gmail.com>
2024-10-14 10:42:35 -0400

    move things around to minimize loading

--- opsml/app/static/src/lib/card/monitoring/Observability.svelte
+++ opsml/app/static/src/lib/card/monitoring/Observability.svelte
@@ -43,37 +43,38 @@
             <AccordionItem class="bg-surface-50 rounded-lg border border-2 border-primary-500">
               <svelte:fragment slot="summary"><p class="text-primary-500 text-lg font-bold">{route.route_name}</p></svelte:fragment>
               <svelte:fragment slot="content">
-                <div class="grid grid-cols-1 lg:grid-cols-5 gap-4">
-                  <!-- First Parent Column -->
-                  <div class="lg:col-span-1">
-                    <div class="grid grid-cols-1 gap-4">
-                      <div class="card bg-white p-4 rounded-lg shadow">
-                        <h3 class="text-lg font-bold text-center text-secondary-500">Requests (thousands)</h3>
-                          <p class="text-lg text-center">{route.requests_per_sec/1000}</p>
-                      </div>
-                      <div class="card bg-white p-4 rounded-lg shadow">
-                        <h3 class="text-lg font-bold text-center text-scouter_red">Errors</h3>
-                          <p class="text-lg text-center">{route.errors}</p>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- Second Parent Column -->
-                  <div class="lg:col-span-4">
-                    <div class="grid grid-cols-2 gap-4">
-                            <SpcTimeChartDiv
-                            data={route.requestViz.data}
-                            id="Requests/sec"
-                            options={route.requestViz.options}
-                            minHeight="min-h-[300px]"
-                            maxHeight="max-h-[300px]"
-                          />
-                      <div class="card bg-white p-4 rounded-lg shadow">
-                        <h3 class="text-lg font-bold">Placeholder</h3>
-                        <p>Placeholder</p>
-                      </div>
-                    </div>
+
+              <div class="grid grid-cols-2 gap-4">
+                <div class="flex justify-center">
+                  <div class="card bg-white p-4 rounded-lg shadow">
+                    <h3 class="text-lg font-bold text-center text-secondary-500">Requests (thousands)</h3>
+                    <p class="text-lg text-center">{route.requests_per_sec / 1000}</p>
                   </div>
                 </div>
+                <div class="flex justify-center">
+                  <div class="card bg-white p-4 rounded-lg shadow">
+                    <h3 class="text-lg font-bold text-center text-scouter_red">Errors</h3>
+                    <p class="text-lg text-center">{route.errors}</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="grid grid-cols-2 gap-4">
+                <SpcTimeChartDiv
+                  data={route.requestViz.data}
+                  id="Requests/sec"
+                  options={route.requestViz.options}
+                  minHeight="min-h-[300px]"
+                  maxHeight="max-h-[300px]"
+                />
+                <div class="card bg-white p-4 rounded-lg shadow">
+                  <h3 class="text-lg font-bold">Placeholder</h3>
+                  <p>Placeholder</p>
+                </div>
+              </div>
+            </div>
+
+
               </svelte:fragment>
             </AccordionItem>
               
