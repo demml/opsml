84c39d9c6526c13b192d5e8660f0b275dfcfe7c9
Thorrester <sjforrester32@gmail.com>
2024-10-18 12:03:48 -0400

    formatting and tests
    Former-commit-id: 34f8efdb517fdf510d13cb8ca70dc50c85e26206

--- opsml/app/routes/scouter.py
+++ opsml/app/routes/scouter.py
@@ -88,7 +88,9 @@ def insert_profile(request: Request, payload: DriftProfileRequest) -> Success:
         return Success()
     except Exception as error:
         logger.error(f"Failed to insert drift profile: {error}")
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to insert drift profile") from error
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to insert drift profile"
+        ) from error
 
 
 @router.put("/scouter/drift/profile", name="update_drift_profile", response_model=ProfileUpdateResponse)
@@ -149,7 +151,9 @@ def update_profile(request: Request, payload: DriftProfileUpdateRequest) -> Prof
         return ProfileUpdateResponse()
     except Exception as error:
         logger.error(f"Failed to insert drift profile: {error}")
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to insert drift profile") from error
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to insert drift profile"
+        ) from error
 
 
 @router.get("/scouter/drift/profile", name="get_profile", response_model=GetDriftProfileResponse)
@@ -182,7 +186,9 @@ def get_profile(
         return GetDriftProfileResponse(profile=profile)
     except Exception as error:
         logger.error(f"Failed to get drift profile: {error}")
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to get drift profile") from error
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to get drift profile"
+        ) from error
 
 
 @router.get("/scouter/drift/values", name="get_drift", response_model=DriftResponse)
@@ -232,7 +238,9 @@ def get_drift_values(
         return DriftResponse(**values)
     except Exception as error:
         logger.error(f"Failed to get drift values: {error}")
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to get drift values") from error
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to get drift values"
+        ) from error
 
 
 @router.get("/scouter/feature/distribution", name="feature distribution", response_model=FeatureDistribution)
@@ -449,7 +457,9 @@ def update_profile_status(
         ) from error
 
 
-@router.get("/scouter/observability/metrics", name="monitoring observability metrics", response_model=ObservabilityMetrics)
+@router.get(
+    "/scouter/observability/metrics", name="monitoring observability metrics", response_model=ObservabilityMetrics
+)
 def get_observability_metrics(
     request: Request,
     repository: str,
--- tests/test_scouter/test_client.py
+++ tests/test_scouter/test_client.py
@@ -1,8 +1,9 @@
 from unittest import mock
 
+from starlette.testclient import TestClient
+
 from opsml.registry import CardRegistries
 from opsml.scouter.client import ScouterApiClient
-from starlette.testclient import TestClient
 from opsml.scouter.types import ObservabilityMetric
 
 
@@ -17,7 +18,9 @@ def test_healthcheck(server: mock.MagicMock, mock_request: mock.MagicMock, api_r
 
 @mock.patch("opsml.scouter.server.ScouterServerClient.update_drift_profile_status")
 @mock.patch("opsml.scouter.integration.ScouterClient.server_running")
-def test_update_profile_status(server: mock.MagicMock, mock_request: mock.MagicMock, api_registries: CardRegistries) -> None:
+def test_update_profile_status(
+    server: mock.MagicMock, mock_request: mock.MagicMock, api_registries: CardRegistries
+) -> None:
     server.return_value = True
     mock_request.return_value = {"status": "success", "message": "Profile updated"}
     client = ScouterApiClient()
