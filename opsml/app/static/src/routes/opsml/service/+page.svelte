c2a9388d10d3f9e9a2cff0e4b955605a3bacdc51
Thorrester <sjforrester32@gmail.com>
2024-10-17 22:26:03 -0400

    testing service

--- opsml/app/static/src/routes/opsml/service/+page.svelte
+++ opsml/app/static/src/routes/opsml/service/+page.svelte
@@ -1,145 +1,246 @@
-<script lang="ts">
-
-  import logo from "$lib/images/opsml-logo.png";
-  import Fa from 'svelte-fa'
-  import { faAdd } from '@fortawesome/free-solid-svg-icons'
-
-  let svgClass: string = "flex-none w-3 mr-0.5 fill-secondary-500 dark:fill-secondary-500";
-
-  let cardBasket: any = [];
-  let serviceName: string = '';
-  let modelName: string = '';
-  let addModel: boolean = false;
-
-  async function handleRegister() {
-    console.log('Registering service', serviceName);
-  }
-
-  async function addModelToService() {
-    addModel = true;
-  }
-
-  let hoveringOverBasket;
-
-  function dragStart(event, model) {
-		// The data we want to make available when the element is dropped
-    // is the index of the item being dragged and
-    // the index of the basket from which it is leaving.
-		const data = {model};
-   	event.dataTransfer.setData('text/plain', JSON.stringify(data));
-	}
-
-
-  function drop(event) {
-		event.preventDefault();
-    console.log('event')
-    const json = event.dataTransfer.getData("text/plain");
-		const data = JSON.parse(json);
-    console.log(data);
-    
-		//
-		//// Remove the item from one basket.
-		//// Splice returns an array of the deleted elements, just one in this case.
-		//const [item] = cardBasket[data.serviceIndex].items.splice(data.serviceItemIndex, 1);
-		//
-    //// Add the item to the drop target basket.
-		//cardBasket[serviceIndex].items.push(item);
-		//cardBasket = cardBasket;
-		//
-		//hoveringOverBasket = null;
-	}
-
-</script>
-
-<div class="flex flex-wrap bg-white min-h-screen mb-8">
-
-  <div class="w-full md:w-1/3 mt-4 ml-4 pl-2 md:ml-12">
-
-    <a draggable={true} on:dragstart={event => dragStart(event, "hello")} class= "block card max-w-96 border p-4 bg-gray-50 rounded-2xl  hover:border-solid hover:border hover:border-secondary-500 dark:bg-surface-700 dark:hover:bg-surface-600" href="#">
-      <header class="flex items-center ml-0.5 mb-0.5" title="blah/blah">
-        <h4 class="truncate font-boing">blah/blah</h4>
-      </header>
-    
-      <div class="mr-1 ml-0.5 flex items-center overflow-hidden whitespace-nowrap text-sm leading-tight">
-        <svg class="flex-none w-3 mr-1 {svgClass}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path d="M 12 0 C 5.371094 0 0 5.371094 0 12 C 0 18.628906 5.371094 24 12 24 C 18.628906 24 24 18.628906 24 12 C 24 5.371094 18.628906 0 12 0 Z M 12 2 C 17.523438 2 22 6.476563 22 12 C 22 17.523438 17.523438 22 12 22 C 6.476563 22 2 17.523438 2 12 C 2 6.476563 6.476563 2 12 2 Z M 10.9375 3.875 L 10.5 12.0625 L 10.59375 12.9375 L 16.75 18.375 L 17.71875 17.375 L 12.625 11.96875 L 12.1875 3.875 Z"></path>
-        </svg>
-        <span class="truncate text-black dark:text-white">
-          <time datetime={ Date() } >
-            Last updated
-          </time>
-        </span>
-        <span class="px-1.5 text-black dark:text-white">- </span>
-        <svg class="flex-none w-3 mr-0.5 {svgClass}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-          <path d="M8 3a3 3 0 0 0-1 5.83v6.34a3.001 3.001 0 1 0 2 0V15a2 2 0 0 1 2-2h1a5.002 5.002 0 0 0 4.927-4.146A3.001 3.001 0 0 0 16 3a3 3 0 0 0-1.105 5.79A3.001 3.001 0 0 1 12 11h-1c-.729 0-1.412.195-2 .535V8.83A3.001 3.001 0 0 0 8 3Z"/>
-        </svg>
-        <div class="text-black dark:text-white"> {10} versions</div>
-      </div>
-    </a>
-  </div>
-
-  <div class="min-h-screen  bg-primary-500 w-[2px]"></div>
-
-  <div class="flex flex-col w-full md:w-7/12 mt-5">
-
-    <section class="pt-4 border-gray-100 col-span-full flex-1 pb-16 items-center pl-8">
-
-      <form class="z-10 mx-auto rounded-2xl bg-slate-100 border shadow p-4 md:px-5" on:submit|preventDefault={handleRegister}>
+<script>
+    import { writable } from 'svelte/store';
   
-        <img alt="OpsML logo" class="mx-auto -mt-12 mb-2 w-20" src={logo}>
-        <h2 class="pt-1 pb-4 text-center text-3xl font-bold text-primary-500">Model Service</h2>
-        
-        <div class="mb-8 grid grid-cols-1 gap-3">
+    let searchQuery = '';
+    let objects = [
+      { id: 1, name: 'Object 1' },
+      { id: 2, name: 'Object 2' },
+      { id: 3, name: 'Object 3' },
+      { id: 4, name: 'Object 4' }
+    ];
+    let filteredObjects = [...objects];
+    let nodes = writable([]);
+    let cards = writable([]);
+    let connections = writable([]);
+    let newObjectName = '';
+    let offsetX = 0;
+    let offsetY = 0;
+    let connectingNode = null;
+    let tempConnection = { x1: 0, y1: 0, x2: 0, y2: 0 };
   
-          <label class="text-primary-500">Service Name
-            <input
-              class="input rounded-lg bg-slate-200 hover:bg-slate-100"
-              type="text" 
-              placeholder="Service Name"
-              bind:value={serviceName}
-            />
-          </label>
-
-          {#if addModel}
-            <label class="text-primary-500 pb-4">Model Name
-              <input
-                class="input rounded-lg bg-slate-200 hover:bg-slate-100"
-                type="text" 
-                placeholder="Model Name"
-                bind:value={modelName}
-              />
-            </label>
-
-            <input
-                class="input rounded-lg bg-slate-200 hover:bg-slate-100"
-                type="text" 
-                placeholder="Model"
-                bind:value={modelName}
-                on:drop={event => drop(event)}
-              />
-
-
+    // Filter objects based on search query
+    $: filteredObjects = objects.filter(obj => obj.name.toLowerCase().includes(searchQuery.toLowerCase()));
+  
+    function handleDragStart(event, object) {
+      event.dataTransfer.setData('application/json', JSON.stringify(object));
+      event.dataTransfer.effectAllowed = 'move';
+    }
+  
+    function handleDrop(event) {
+      const object = JSON.parse(event.dataTransfer.getData('application/json'));
+      const dropZoneRect = event.target.getBoundingClientRect();
+      const x = event.clientX - dropZoneRect.left;
+      const y = event.clientY - dropZoneRect.top;
+  
+      // Check if the drop target is a valid "route" element
+      if (event.target.classList.contains('route')) {
+        nodes.update(n => {
+          if (!n.find(node => node.id === object.id)) {
+            return [...n, { id: object.id, label: object.name, x, y, linkedCards: [], linkedCardsText: '' }];
+          }
+          return n;
+        });
+      }
+    }
+  
+    function handleCardDrop(event) {
+      const object = JSON.parse(event.dataTransfer.getData('application/json'));
+      cards.update(c => {
+        if (!c.find(card => card.id === object.id)) {
+          return [...c, { id: object.id, name: object.name }];
+        }
+        return c;
+      });
+    }
+  
+    function allowDrop(event) {
+      event.preventDefault();
+      event.dataTransfer.dropEffect = 'move';
+    }
+  
+    function handleNodeDrag(event, node) {
+      const dropZoneRect = event.target.parentElement.getBoundingClientRect();
+      const x = event.clientX - dropZoneRect.left - offsetX;
+      const y = event.clientY - dropZoneRect.top - offsetY;
+  
+      nodes.update(n => {
+        const index = n.findIndex(n => n.id === node.id);
+        if (index !== -1) {
+          n[index] = { ...n[index], x, y };
+        }
+        return n;
+      });
+    }
+  
+    function handleNodeDragStart(event, node) {
+      // Create an invisible element to use as the drag image
+      const img = new Image();
+      img.src = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='; // 1x1 pixel transparent gif
+      event.dataTransfer.setDragImage(img, 0, 0);
+  
+      // Calculate the offset between the mouse position and the top-left corner of the node
+      const nodeRect = event.target.getBoundingClientRect();
+      offsetX = event.clientX - nodeRect.left;
+      offsetY = event.clientY - nodeRect.top;
+    }
+  
+    function handleDragEnter(event) {
+      event.preventDefault();
+    }
+  
+    function addNewObject() {
+      nodes.update(n => [...n, { id: Date.now(), label: newObjectName, x: 50, y: 50, linkedCards: [], linkedCardsText: '' }]);
+      newObjectName = '';
+    }
+  
+    function handleCardFieldDrop(event, node) {
+      const object = JSON.parse(event.dataTransfer.getData('application/json'));
+      nodes.update(n => {
+        const index = n.findIndex(n => n.id === node.id);
+        if (index !== -1) {
+          if (!n[index].linkedCards) {
+            n[index].linkedCards = [];
+          }
+          if (!n[index].linkedCards.includes(object.name)) {
+            n[index].linkedCards.push(object.name);
+            n[index].linkedCardsText = n[index].linkedCards.join(', ');
+          }
+        }
+        return n;
+      });
+    }
+  
+    function deleteNode(nodeId) {
+      nodes.update(n => n.filter(node => node.id !== nodeId));
+      connections.update(c => c.filter(conn => conn.from !== nodeId && conn.to !== nodeId));
+    }
+  
+    function startConnection(event, node) {
+      connectingNode = node;
+      const rect = event.target.getBoundingClientRect();
+      tempConnection.x1 = rect.left + rect.width / 2;
+      tempConnection.y1 = rect.top + rect.height / 2;
+      tempConnection.x2 = tempConnection.x1;
+      tempConnection.y2 = tempConnection.y1;
+    }
+  
+    function updateTempConnection(event) {
+      if (connectingNode) {
+        tempConnection.x2 = event.clientX;
+        tempConnection.y2 = event.clientY;
+      }
+    }
+  
+    function completeConnection(event, targetNode) {
+      if (connectingNode && connectingNode.id !== targetNode.id) {
+        connections.update(c => [...c, { from: connectingNode.id, to: targetNode.id }]);
+      }
+      connectingNode = null;
+      tempConnection = { x1: 0, y1: 0, x2: 0, y2: 0 };
+    }
+  </script>
+  
+  <div class="flex h-screen">
+    <div id="model-search" class="w-1/4 p-4 border-r border-gray-300">
+      <input
+        type="text"
+        class="w-full p-2 mb-4 border border-gray-300 rounded"
+        placeholder="Search objects..."
+        bind:value={searchQuery}
+      />
+      {#each filteredObjects as object}
+        <div
+          class="p-2 mb-2 bg-gray-200 rounded cursor-grab"
+          draggable="true"
+          on:dragstart={(event) => handleDragStart(event, object)}
+        >
+          {object.name}
+        </div>
+      {/each}
+    </div>
+    <div class="w-3/4 p-4 flex flex-col relative">
+      <div id="model-service" class="flex-1 border-b border-gray-300 p-4">
+        <h2 class="text-xl mb-4">Model Service</h2>
+        <div
+          class="h-full border-2 border-dashed border-gray-300 flex items-center justify-center relative"
+          on:drop={handleCardDrop}
+          on:dragover={allowDrop}
+          on:dragenter={handleDragEnter}
+        >
+          {#if $cards.length === 0}
+            <p>Drag objects here to create cards</p>
+          {:else}
+            {#each $cards as card}
+              <div
+                class="p-4 mb-2 bg-white border rounded shadow cursor-grab"
+                draggable="true"
+                on:dragstart={(event) => handleDragStart(event, card)}
+              >
+                <p>{card.name}</p>
+              </div>
+            {/each}
           {/if}
-
-          <div class="flex flex-row items-center align-center">
-            <button type="button" class="m-1 btn btn-sm bg-darkpurple text-white mr-2" on:click={() => addModelToService() }>
-              <Fa class="h-3" icon={faAdd}/>
-              <header class="text-white text-xs">Add Model</header>
-            </button>
-  
-          </div>
-
-  
         </div>
-  
-        <div class="grid justify-items-center">
-          <button type="submit" class="btn bg-primary-500 text-white rounded-lg md:w-72 justify-self-center mb-2">
-            <span>Register Service</span>
-          </button>
+      </div>
+      <div id="api-builder" class="flex-1 p-4 relative">
+        <div
+          class="h-full border-2 border-dashed border-gray-300 flex flex-col items-center justify-center relative"
+          on:drop={handleDrop}
+          on:dragover={allowDrop}
+          on:dragenter={handleDragEnter}
+        >
+          {#if $nodes.length === 0}
+            <p>Drag objects here</p>
+          {:else}
+            {#each $nodes as node}
+              <div
+                class="route p-2 mb-2 bg-gray-300 rounded cursor-grab absolute"
+                style="left: {node.x}px; top: {node.y}px;"
+                draggable="true"
+                on:drag={(event) => handleNodeDrag(event, node)}
+                on:dragstart={(event) => handleNodeDragStart(event, node)}
+                on:dragover={(event) => event.preventDefault()}
+                on:dragenter={(event) => event.preventDefault()}
+                on:dblclick={() => startConnection(node)}
+                on:mouseup={() => completeConnection(node)}
+              >
+                <label class="block mb-1">Name</label>
+                <input type="text" class="mb-2 p-1 border rounded" bind:value={node.label} placeholder="Name" />
+                <label class="block mb-1">Model(s)</label>
+                <textarea
+                  class="p-2 bg-white border rounded mb-2 w-full"
+                  bind:value={node.linkedCardsText}
+                  on:input={(event) => node.linkedCards = event.target.value.split(',').map(s => s.trim())}
+                  on:drop={(event) => handleCardFieldDrop(event, node)}
+                  on:dragover={allowDrop}
+                  on:dragenter={handleDragEnter}
+                ></textarea>
+                <button class="p-1 bg-red-500 text-white rounded" on:click={() => deleteNode(node.id)}>Delete</button>
+              </div>
+            {/each}
+          {/if}
+          <button class="absolute bottom-4 right-4 p-2 bg-blue-500 text-white rounded" on:click={addNewObject}>+</button>
         </div>
-      </form>
-    </section>
+        <svg class="absolute inset-0 w-full h-full pointer-events-none">
+          {#each $connections as conn}
+            {#if $nodes.find(n => n.id === conn.from) && $nodes.find(n => n.id === conn.to)}
+              <line
+                x1={$nodes.find(n => n.id === conn.from).x + 50}
+                y1={$nodes.find(n => n.id === conn.from).y + 25}
+                x2={$nodes.find(n => n.id === conn.to).x + 50}
+                y2={$nodes.find(n => n.id === conn.to).y + 25}
+                stroke="black"
+                stroke-width="2"
+                marker-end="url(#arrowhead)"
+              />
+            {/if}
+          {/each}
+          <defs>
+            <marker id="arrowhead" markerWidth="10" markerHeight="7" refX="0" refY="3.5" orient="auto">
+              <polygon points="0 0, 10 3.5, 0 7" />
+            </marker>
+          </defs>
+        </svg>
+      </div>
+    </div>
   </div>
-
-</div>
-
