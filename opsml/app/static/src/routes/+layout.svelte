15536372bbd4dbb7a062d218b0230c418ee0b155
Thorrester <sjforrester32@gmail.com>
2024-10-22 16:11:18 -0400

    Update +layout.svelte
    Former-commit-id: 672c3dfd1c9a538fce84c8ce4a5fc4b09cbe5f5d

--- opsml/app/static/src/routes/+layout.svelte
+++ opsml/app/static/src/routes/+layout.svelte
@@ -5,25 +5,35 @@
   import favicon from "$lib/images/opsml-green.ico";
   import { initializeStores, Toast, Modal, storePopup } from '@skeletonlabs/skeleton';
   import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+  import { authStore, type AuthState } from '$lib/scripts/auth/newAuthStore';
+  import { setupAuth } from '$lib/scripts/auth/setupAuth';
+  import { onMount } from 'svelte';
+  import { goto } from "$app/navigation";
+  import { CommonPaths } from "$lib/scripts/types";
 
   storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
   initializeStores();
 
+  /** @type {import('./$types').LayoutData} */
+	export let data;
+
+  let auth: AuthState;
+  $: auth = data.auth
 
 
   // Run setupAuth on mount
-  //onMount(async () => {
-  //  await setupAuth();
-//
-  //  console.log("auth.isAuthenticated: ", auth.isAuthenticated);
-  //  console.log("auth.requireAuth: ", auth.requireAuth);
-//
-  //  // Check if user is logged in
-  //  if (auth.requireAuth && !auth.isAuthenticated) {
-  //    goto(CommonPaths.LOGIN + "?redirect=" + window.location.pathname);
-  //  }
-//
-  //});
+  onMount(async () => {
+    await setupAuth();
+
+    console.log("auth.isAuthenticated: ", auth.isAuthenticated);
+    console.log("auth.requireAuth: ", auth.requireAuth);
+
+    // Check if user is logged in
+    if (auth.requireAuth && !auth.isAuthenticated) {
+      goto(CommonPaths.LOGIN + "?redirect=" + window.location.pathname);
+    }
+
+  });
   
 </script>
 
