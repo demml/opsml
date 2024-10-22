49674c06e16b1b598dd96c274f0e49eaa73bf375
Thorrester <sjforrester32@gmail.com>
2024-10-22 16:20:29 -0400

    setup auth

--- opsml/app/static/src/lib/scripts/auth/authManager.ts
+++ opsml/app/static/src/lib/scripts/auth/authManager.ts
@@ -1,5 +1,6 @@
 // src/stores/authStore.ts
 import { writable, get } from "svelte/store";
+import { CommonPaths } from "$lib/scripts/types";
 
 export interface OpsmlAuth {
   opsml_auth: boolean;
@@ -75,6 +76,54 @@ export async function login(username: string, password: string) {
       });
     } else {
       throw new Error("Login failed");
+    }
+  }
+}
+
+export async function setupAuth() {
+  console.log("setupAuth");
+  // check if authState is stored in localStorage (for page refresh)
+  const storedAuthState = localStorage.getItem("authState");
+  if (storedAuthState) {
+    const authState: AuthState = JSON.parse(storedAuthState);
+    setAuthState(authState);
+    return;
+  }
+
+  // check if we are in test mode
+  if (import.meta.env.VITE_TEST) {
+    setAuthState({
+      ...initialAuthState,
+      isAuthenticated: false,
+    });
+    return;
+
+    // all other cases
+  } else {
+    // await opsm/verify
+    let response = (await fetch(CommonPaths.VERIFY, {
+      method: "GET", // default, so we can ignore
+    })) as Response;
+
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    } else {
+      let data = (await response.json()) as OpsmlAuth;
+
+      // define auth type. If okta_auth is true, then we need to set up the OktaConfig
+      // if not, then we don't need to set up the OktaConfig and default to basic auth
+      let authType: string = "basic";
+
+      setAuthState({
+        authType: authType,
+        requireAuth: data.opsml_auth,
+        isAuthenticated: false,
+        state: {
+          user: undefined,
+          access_token: undefined,
+          refresh_token: undefined,
+        },
+      });
     }
   }
 }
