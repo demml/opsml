5e44ca527c41dd2d619a374f8b19593baadd3518
Thorrester <sjforrester32@gmail.com>
2024-11-01 11:33:37 -0400

    update paths

--- opsml/app/routes/healthcheck.py
+++ opsml/app/routes/healthcheck.py
@@ -11,7 +11,7 @@ router = APIRouter()
 
 
 @router.get("/healthcheck", response_model=HealthCheckResult, name="healthcheck")
-def get_healthcheck() -> HealthCheckResult:
+async def get_healthcheck() -> HealthCheckResult:
     return HealthCheckResult(is_alive=True)
 
 
@@ -28,5 +28,5 @@ async def debug() -> DebugResponse:
     "/error",
     description="An endpoint that will return a 500 error for debugging and alert testing",
 )
-def get_error() -> None:
+async def get_error() -> None:
     raise HTTPException(status_code=500)
