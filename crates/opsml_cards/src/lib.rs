8e68a8746aa1c9afdc2fb62f5e0f7f4cd4923f34
Thorrester <sjforrester32@gmail.com>
2024-12-25 08:26:18 -0500

    typing
    Former-commit-id: 3d204c67a8f2206b645a55ad42a9b594b6ce793d

---
+++ crates/opsml_cards/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod data;
+pub mod model;
+pub mod run;
+
+pub use run::*;
--- crates/opsml_utils/src/utils.rs
+++ crates/opsml_utils/src/utils.rs
@@ -1,6 +1,15 @@
 use chrono::{NaiveDateTime, Timelike};
+use colored_json::{Color, ColorMode, ColoredFormatter, PrettyFormatter, Styler};
 use opsml_error::error::UtilError;
+use pyo3::exceptions::{PyTypeError, PyValueError};
+use pyo3::{prelude::*, types::PyAnyMethods};
 use regex::Regex;
+
+use pyo3::types::{PyBool, PyDict, PyDictMethods, PyFloat, PyInt, PyList, PyString};
+use pyo3::IntoPyObjectExt;
+use serde::Serialize;
+use serde_json::{json, Value};
+use std::path::PathBuf;
 
 use uuid::Uuid;
 
@@ -82,6 +91,169 @@ pub fn get_utc_timestamp() -> i64 {
 
 pub fn get_utc_datetime() -> NaiveDateTime {
     chrono::Utc::now().naive_utc()
+}
+
+pub struct PyHelperFuncs {}
+
+impl PyHelperFuncs {
+    pub fn __str__<T: Serialize>(object: T) -> String {
+        match ColoredFormatter::with_styler(
+            PrettyFormatter::default(),
+            Styler {
+                key: Color::Rgb(75, 57, 120).foreground(),
+                string_value: Color::Rgb(4, 205, 155).foreground(),
+                float_value: Color::Rgb(4, 205, 155).foreground(),
+                integer_value: Color::Rgb(4, 205, 155).foreground(),
+                bool_value: Color::Rgb(4, 205, 155).foreground(),
+                nil_value: Color::Rgb(4, 205, 155).foreground(),
+                ..Default::default()
+            },
+        )
+        .to_colored_json(&object, ColorMode::On)
+        {
+            Ok(json) => json,
+            Err(e) => format!("Failed to serialize to json: {}", e),
+        }
+        // serialize the struct to a string
+    }
+
+    pub fn __json__<T: Serialize>(object: T) -> String {
+        match serde_json::to_string_pretty(&object) {
+            Ok(json) => json,
+            Err(e) => format!("Failed to serialize to json: {}", e),
+        }
+    }
+
+    pub fn save_to_json<T>(model: T, mut path: PathBuf) -> Result<(), UtilError>
+    where
+        T: Serialize,
+    {
+        // serialize the struct to a string
+        let json =
+            serde_json::to_string_pretty(&model).map_err(|_| UtilError::SerializationError)?;
+
+        // ensure .json extension
+        path.set_extension("json");
+
+        if !path.exists() {
+            // ensure path exists, create if not
+            let parent_path = path.parent().ok_or(UtilError::GetParentPathError)?;
+
+            std::fs::create_dir_all(parent_path).map_err(|_| UtilError::CreateDirectoryError)?;
+        }
+
+        std::fs::write(path, json).map_err(|_| UtilError::WriteError)?;
+
+        Ok(())
+    }
+}
+
+pub fn json_to_pyobject(py: Python, value: &Value, dict: &Bound<'_, PyDict>) -> PyResult<()> {
+    match value {
+        Value::Object(map) => {
+            for (k, v) in map {
+                let py_value = match v {
+                    Value::Null => py.None(),
+                    Value::Bool(b) => b.into_py_any(py).unwrap(),
+                    Value::Number(n) => {
+                        if let Some(i) = n.as_i64() {
+                            i.into_py_any(py).unwrap()
+                        } else if let Some(f) = n.as_f64() {
+                            f.into_py_any(py).unwrap()
+                        } else {
+                            return Err(PyValueError::new_err("Invalid number"));
+                        }
+                    }
+                    Value::String(s) => s.into_py_any(py).unwrap(),
+                    Value::Array(arr) => {
+                        let py_list = PyList::empty(py);
+                        for item in arr {
+                            let py_item = json_to_pyobject_value(py, item)?;
+                            py_list.append(py_item)?;
+                        }
+                        py_list.into_py_any(py).unwrap()
+                    }
+                    Value::Object(_) => {
+                        let nested_dict = PyDict::new(py);
+                        json_to_pyobject(py, v, &nested_dict)?;
+                        nested_dict.into_py_any(py).unwrap()
+                    }
+                };
+                dict.set_item(k, py_value)?;
+            }
+        }
+        _ => return Err(PyValueError::new_err("Root must be an object")),
+    }
+    Ok(())
+}
+
+pub fn json_to_pyobject_value(py: Python, value: &Value) -> PyResult<PyObject> {
+    Ok(match value {
+        Value::Null => py.None(),
+        Value::Bool(b) => b.into_py_any(py).unwrap(),
+        Value::Number(n) => {
+            if let Some(i) = n.as_i64() {
+                i.into_py_any(py).unwrap()
+            } else if let Some(f) = n.as_f64() {
+                f.into_py_any(py).unwrap()
+            } else {
+                return Err(PyValueError::new_err("Invalid number"));
+            }
+        }
+        Value::String(s) => s.into_py_any(py).unwrap(),
+        Value::Array(arr) => {
+            let py_list = PyList::empty(py);
+            for item in arr {
+                let py_item = json_to_pyobject_value(py, item)?;
+                py_list.append(py_item)?;
+            }
+            py_list.into_py_any(py).unwrap()
+        }
+        Value::Object(_) => {
+            let nested_dict = PyDict::new(py);
+            json_to_pyobject(py, value, &nested_dict)?;
+            nested_dict.into_py_any(py).unwrap()
+        }
+    })
+}
+
+pub fn pyobject_to_json(obj: &Bound<'_, PyAny>) -> PyResult<Value> {
+    if obj.is_instance_of::<PyDict>() {
+        let dict = obj.downcast::<PyDict>()?;
+        let mut map = serde_json::Map::new();
+        for (key, value) in dict.iter() {
+            let key_str = key.extract::<String>()?;
+            let json_value = pyobject_to_json(&value)?;
+            map.insert(key_str, json_value);
+        }
+        Ok(Value::Object(map))
+    } else if obj.is_instance_of::<PyList>() {
+        let list = obj.downcast::<PyList>()?;
+        let mut vec = Vec::new();
+        for item in list.iter() {
+            vec.push(pyobject_to_json(&item)?);
+        }
+        Ok(Value::Array(vec))
+    } else if obj.is_instance_of::<PyString>() {
+        let s = obj.extract::<String>()?;
+        Ok(Value::String(s))
+    } else if obj.is_instance_of::<PyFloat>() {
+        let f = obj.extract::<f64>()?;
+        Ok(json!(f))
+    } else if obj.is_instance_of::<PyBool>() {
+        let b = obj.extract::<bool>()?;
+        Ok(json!(b))
+    } else if obj.is_instance_of::<PyInt>() {
+        let i = obj.extract::<i64>()?;
+        Ok(json!(i))
+    } else if obj.is_none() {
+        Ok(Value::Null)
+    } else {
+        Err(PyTypeError::new_err(format!(
+            "Unsupported type: {}",
+            obj.get_type().name()?
+        )))
+    }
 }
 
 #[cfg(test)]
