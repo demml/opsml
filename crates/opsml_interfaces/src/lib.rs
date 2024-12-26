0a397e973cce32da7f2774ecaba16d3d57a0cb1f
Thorrester <sjforrester32@gmail.com>
2024-12-26 10:13:58 -0500

    interfaces
    Former-commit-id: ca925ea97135baf77b322a024e58d3c975d57064

--- crates/opsml_interfaces/src/lib.rs
+++ crates/opsml_interfaces/src/lib.rs
@@ -2,6 +2,5 @@ pub mod data;
 pub mod model;
 pub mod types;
 
-pub use data::*;
 pub use model::*;
 pub use types::*;
---
+++ crates/opsml_interfaces/src/model/vowpal/interface.rs
@@ -0,0 +1,53 @@
+use crate::Feature;
+use pyo3::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[pyclass]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct VowpalWabbitInterfaceMetadata {
+    #[pyo3(get)]
+    pub task_type: String,
+    #[pyo3(get)]
+    pub model_type: String,
+    #[pyo3(get)]
+    pub data_type: String,
+    #[pyo3(get)]
+    pub modelcard_uid: String,
+    #[pyo3(get)]
+    pub feature_map: HashMap<String, Feature>,
+    #[pyo3(get)]
+    pub arguments: String,
+    #[pyo3(get)]
+    pub sample_data_interface_type: String,
+    #[pyo3(get)]
+    pub metadata: HashMap<String, String>,
+}
+
+#[pymethods]
+impl VowpalWabbitInterfaceMetadata {
+    #[new]
+    #[allow(clippy::too_many_arguments)]
+    #[pyo3(signature = (task_type, model_type, data_type, modelcard_uid, feature_map, arguments, sample_data_interface_type, metadata=None))]
+    pub fn new(
+        task_type: String,
+        model_type: String,
+        data_type: String,
+        modelcard_uid: String,
+        feature_map: HashMap<String, Feature>,
+        arguments: String,
+        sample_data_interface_type: String,
+        metadata: Option<HashMap<String, String>>,
+    ) -> Self {
+        VowpalWabbitInterfaceMetadata {
+            task_type,
+            model_type,
+            data_type,
+            modelcard_uid,
+            feature_map,
+            arguments,
+            sample_data_interface_type,
+            metadata: metadata.unwrap_or_default(),
+        }
+    }
+}
