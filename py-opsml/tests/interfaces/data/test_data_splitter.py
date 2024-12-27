f500006ad62aa6caacd1c4b837dae210612ec8a8
Thorrester <sjforrester32@gmail.com>
2024-12-27 10:47:53 -0500

    pandas inequalities
    Former-commit-id: 6f5b425dd95a5856c20cfe76aa6988b41c6f6799

--- py-opsml/tests/interfaces/data/test_data_splitter.py
+++ py-opsml/tests/interfaces/data/test_data_splitter.py
@@ -1,4 +1,5 @@
 import polars as pl
+import pandas as pd
 from opsml import (
     ColType,
     ColValType,
@@ -12,6 +13,7 @@ from opsml import (
     DataType,
     Inequality,
 )
+import datetime
 
 
 def test_polars_equal_column_split(polars_dataframe: pl.DataFrame):
@@ -128,3 +130,182 @@ def test_polars_greater_than_column_split(polars_dataframe: pl.DataFrame):
     assert list(split.keys())[0] == "train"
     assert split["train"].x.shape == (2, 2)
     assert split["train"].y.shape == (2, 1)
+
+
+def test_polars_index_split(polars_dataframe: pl.DataFrame):
+    data_split = DataSplit(
+        label="train",
+        indice_split=IndiceSplit(
+            indices=[0, 3, 5],
+        ),
+    )
+    splitter = DataSplitter()
+
+    split = splitter.split_data(
+        split=data_split,
+        data=polars_dataframe,
+        data_type=DataType.Polars,
+        dependent_vars=[],
+    )
+
+    assert split is not None
+    assert isinstance(split["train"], Data)
+    assert list(split.keys())[0] == "train"
+    assert split["train"].x.shape == (3, 3)
+    assert split["train"].x["foo"].to_list() == [1, 4, 6]
+
+
+def test_polars_start_stop_split(polars_dataframe: pl.DataFrame):
+    data_split = DataSplit(
+        label="train",
+        start_stop_split=StartStopSplit(start=3, stop=5),
+    )
+    splitter = DataSplitter()
+
+    split = splitter.split_data(
+        split=data_split,
+        data=polars_dataframe,
+        data_type=DataType.Polars,
+        dependent_vars=[],
+    )
+
+    assert split is not None
+    assert isinstance(split["train"], Data)
+    assert list(split.keys())[0] == "train"
+    assert split["train"].x.shape == (2, 3)
+    assert split["train"].x["foo"].to_list() == [4, 5]
+
+
+def test_pandas_inequality_split(pandas_dataframe: pd.DataFrame):
+    eq_col_split = ColumnSplit(
+        column_name="animals",
+        column_value="Horse",
+        column_type=ColType.Builtin,
+    )
+
+    data_split = DataSplit(label="train", column_split=eq_col_split)
+    splitter = DataSplitter()
+
+    split = splitter.split_data(
+        split=data_split,
+        data=pandas_dataframe,
+        data_type=DataType.Pandas,
+        dependent_vars=[],
+    )
+
+    assert split is not None
+    assert isinstance(split["train"], Data)
+    assert list(split.keys())[0] == "train"
+    assert split["train"].x.shape == (2, 4)
+
+    lessthan_eq_col_split = ColumnSplit(
+        column_name="n_legs",
+        column_value=3,
+        column_type=ColType.Builtin,
+        inequality="<=",
+    )
+
+    data_split = DataSplit(label="train", column_split=lessthan_eq_col_split)
+    splitter = DataSplitter()
+
+    split = splitter.split_data(
+        split=data_split,
+        data=pandas_dataframe,
+        data_type=DataType.Pandas,
+        dependent_vars=[],
+    )
+
+    assert split is not None
+    assert isinstance(split["train"], Data)
+    assert list(split.keys())[0] == "train"
+    assert split["train"].x.shape == (2, 4)
+
+    lessthan_eq_col_split = ColumnSplit(
+        column_name="n_legs",
+        column_value=3,
+        column_type=ColType.Builtin,
+        inequality="<",
+    )
+
+    data_split = DataSplit(label="train", column_split=lessthan_eq_col_split)
+    splitter = DataSplitter()
+
+    split = splitter.split_data(
+        split=data_split,
+        data=pandas_dataframe,
+        data_type=DataType.Pandas,
+        dependent_vars=[],
+    )
+
+    assert split is not None
+    assert isinstance(split["train"], Data)
+    assert list(split.keys())[0] == "train"
+    assert split["train"].x.shape == (2, 4)
+
+    col_split = ColumnSplit(
+        column_name="n_legs",
+        column_value=5,
+        column_type=ColType.Builtin,
+        inequality=">=",
+    )
+
+    data_split = DataSplit(label="train", column_split=col_split)
+    splitter = DataSplitter()
+
+    split = splitter.split_data(
+        split=data_split,
+        data=pandas_dataframe,
+        data_type=DataType.Pandas,
+        dependent_vars=[],
+    )
+
+    assert split is not None
+    assert isinstance(split["train"], Data)
+    assert list(split.keys())[0] == "train"
+    assert split["train"].x.shape == (4, 4)
+
+    col_split = ColumnSplit(
+        column_name="n_legs",
+        column_value=5,
+        column_type=ColType.Builtin,
+        inequality=">",
+    )
+
+    data_split = DataSplit(label="train", column_split=col_split)
+    splitter = DataSplitter()
+
+    split = splitter.split_data(
+        split=data_split,
+        data=pandas_dataframe,
+        data_type=DataType.Pandas,
+        dependent_vars=[],
+    )
+
+    assert split is not None
+    assert isinstance(split["train"], Data)
+    assert list(split.keys())[0] == "train"
+    assert split["train"].x.shape == (2, 4)
+
+
+def test_pandas_timestamp(pandas_dataframe: pd.DataFrame):
+    col_split = ColumnSplit(
+        column_name="timestamp",
+        column_value=datetime.datetime(2022, 1, 1).timestamp(),
+        column_type=ColType.Timestamp,
+        inequality=">",
+    )
+
+    data_split = DataSplit(label="train", column_split=col_split)
+    splitter = DataSplitter()
+
+    split = splitter.split_data(
+        split=data_split,
+        data=pandas_dataframe,
+        data_type=DataType.Pandas,
+        dependent_vars=[],
+    )
+
+    assert split is not None
+    assert isinstance(split["train"], Data)
+    assert list(split.keys())[0] == "train"
+    assert split["train"].x.shape == (8, 4)
