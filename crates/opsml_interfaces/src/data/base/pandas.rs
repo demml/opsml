489b4a521b35cf8e6bb1e2136352a03a61cbded9
Thorrester <sjforrester32@gmail.com>
2025-01-04 22:53:11 -0500

    Update pandas.rs

--- crates/opsml_interfaces/src/data/base/pandas.rs
+++ crates/opsml_interfaces/src/data/base/pandas.rs
@@ -1,1 +1,121 @@
+use crate::data::{generate_feature_schema, DataInterface, InterfaceSaveMetadata, SqlLogic};
+use crate::types::FeatureMap;
+use opsml_error::OpsmlError;
+use opsml_types::{DataType, SaveName, Suffix};
+use pyo3::prelude::*;
+use pyo3::types::PyDict;
+use pyo3::IntoPyObjectExt;
+use std::path::PathBuf;
 
+#[pyclass(extends=DataInterface, subclass)]
+pub struct PandasData {
+    #[pyo3(get)]
+    pub data_type: DataType,
+
+    #[pyo3(get, set)]
+    pub data: PyObject,
+}
+
+#[pymethods]
+impl PandasData {
+    #[new]
+    #[allow(clippy::too_many_arguments)]
+    #[pyo3(signature = (data=None, data_splits=None, dependent_vars=None, feature_map=None, sql_logic=None))]
+    fn new(
+        py: Python,
+        data: Option<&Bound<'_, PyAny>>, // data can be any pyobject
+        data_splits: Option<&Bound<'_, PyAny>>, //
+        dependent_vars: Option<&Bound<'_, PyAny>>,
+        feature_map: Option<FeatureMap>,
+        sql_logic: Option<SqlLogic>,
+    ) -> PyResult<(Self, DataInterface)> {
+        // check if data is a numpy array
+        let data = match data {
+            Some(data) => {
+                // check if data is a numpy array
+                // get type name of data
+                let pandas = py.import("pandas")?.getattr("DataFrame")?;
+
+                // check if data is a numpy array
+                if data.is_instance(&pandas).unwrap() {
+                    data.into_py_any(py)?
+                } else {
+                    return Err(OpsmlError::new_err("Data must be a numpy array"));
+                }
+            }
+            None => py.None(),
+        };
+
+        let data_interface = DataInterface::new(
+            py,
+            None,
+            data_splits,
+            dependent_vars,
+            feature_map,
+            sql_logic,
+        )?;
+        Ok((
+            PandasData {
+                data_type: DataType::Pandas,
+                data,
+            },
+            data_interface,
+        ))
+    }
+
+    #[pyo3(signature = (path, **kwargs))]
+    pub fn save_data<'py>(
+        mut self_: PyRefMut<'py, Self>,
+        py: Python,
+        path: PathBuf,
+        kwargs: Option<&Bound<'py, PyDict>>,
+    ) -> PyResult<InterfaceSaveMetadata> {
+        // check if data is None
+
+        if self_.data.is_none(py) {
+            return Err(OpsmlError::new_err(
+                "No data detected in interface for saving",
+            ));
+        }
+
+        // Save the data using parquet
+        let feature_map = generate_feature_schema(self_.data.bind(py), &DataType::Pandas)?;
+
+        let save_path = PathBuf::from(SaveName::Data.to_string()).with_extension(Suffix::Parquet);
+        let full_save_path = path.join(&save_path);
+
+        let _ = self_
+            .data
+            .call_method(py, "to_parquet", (full_save_path,), kwargs)
+            .map_err(|e| OpsmlError::new_err(e.to_string()))?;
+
+        let super_ = self_.as_super();
+        super_.feature_map = feature_map.clone();
+
+        Ok(InterfaceSaveMetadata {
+            data_type: self_.data_type.clone(),
+            feature_map,
+            data_save_path: save_path,
+            data_profile_save_path: None,
+        })
+    }
+
+    #[pyo3(signature = (path, **kwargs))]
+    pub fn load_data<'py>(
+        &mut self,
+        py: Python,
+        path: PathBuf,
+        kwargs: Option<&Bound<'py, PyDict>>,
+    ) -> PyResult<()> {
+        let load_path = path.join(SaveName::Data).with_extension(Suffix::Parquet);
+
+        let pandas = PyModule::import(py, "pandas")?;
+
+        // Load the data using polars
+        let data = pandas.call_method("read_parquet", (load_path,), kwargs)?;
+
+        self.data = data.into();
+
+        Ok(())
+    }
+}
