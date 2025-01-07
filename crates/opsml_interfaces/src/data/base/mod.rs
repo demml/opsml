242922c5dc9890edec31e0508051d81d251d6bb9
Thorrester <sjforrester32@gmail.com>
2025-01-06 21:02:11 -0500

    torch

--- crates/opsml_interfaces/src/data/base/mod.rs
+++ crates/opsml_interfaces/src/data/base/mod.rs
@@ -4,6 +4,7 @@ pub mod numpy;
 pub mod pandas;
 pub mod polars;
 pub mod sql;
+pub mod torch;
 pub mod types;
 
 pub use arrow::ArrowData;
@@ -12,4 +13,5 @@ pub use numpy::NumpyData;
 pub use pandas::PandasData;
 pub use polars::PolarsData;
 pub use sql::SqlData;
+pub use torch::TorchData;
 pub use types::*;
