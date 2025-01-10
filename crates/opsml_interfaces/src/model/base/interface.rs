3056552ccb32a4669c0e215a250878ad5e09f42c
Thorrester <sjforrester32@gmail.com>
2025-01-10 13:48:13 -0500

    implement save logic for sample data

--- crates/opsml_interfaces/src/model/base/interface.rs
+++ crates/opsml_interfaces/src/model/base/interface.rs
@@ -1,20 +1,24 @@
+use crate::data::generate_feature_schema;
+use crate::data::DataInterface;
 use crate::model::{SampleData, TaskType};
 use crate::types::FeatureSchema;
 use crate::Feature;
 use opsml_error::error::OpsmlError;
-use opsml_types::{DataType, InterfaceType, SaveName, Suffix};
+use opsml_types::{DataType, SaveName, Suffix};
 use opsml_utils::PyHelperFuncs;
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use pyo3::IntoPyObjectExt;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::path::PathBuf;
 use tracing::warn;
 
 #[pyclass]
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ModelDataInterfaceSaveMetadata {
+pub struct ModelInterfaceSaveMetadata {
     #[pyo3(get)]
-    pub trained_model_uri: String,
+    pub model_uri: PathBuf,
 
     #[pyo3(get)]
     pub preprocessor_uri: Option<String>,
@@ -23,7 +27,7 @@ pub struct ModelDataInterfaceSaveMetadata {
     pub preprocessor_name: Option<String>,
 
     #[pyo3(get)]
-    pub sample_data_uri: String,
+    pub sample_data_uri: Option<String>,
 
     #[pyo3(get)]
     pub onnx_model_uri: Option<String>,
@@ -33,24 +37,24 @@ pub struct ModelDataInterfaceSaveMetadata {
 }
 
 #[pymethods]
-impl ModelDataInterfaceSaveMetadata {
+impl ModelInterfaceSaveMetadata {
     #[new]
-    #[pyo3(signature = (trained_model_uri, sample_data_uri,  preprocessor_uri=None, preprocessor_name=None, onnx_model_uri=None, extra_metadata=None))]
+    #[pyo3(signature = (model_uri, sample_data_uri,  preprocessor_uri=None, preprocessor_name=None, onnx_model_uri=None, extra_metadata=HashMap::new()))]
     pub fn new(
-        trained_model_uri: String,
-        sample_data_uri: String,
+        model_uri: String,
+        sample_data_uri: Option<String>,
         preprocessor_uri: Option<String>,
         preprocessor_name: Option<String>,
         onnx_model_uri: Option<String>,
-        extra_metadata: Option<HashMap<String, String>>,
+        extra_metadata: HashMap<String, String>,
     ) -> Self {
-        ModelDataInterfaceSaveMetadata {
-            trained_model_uri,
+        ModelInterfaceSaveMetadata {
+            model_uri,
             sample_data_uri,
             preprocessor_uri,
             preprocessor_name,
             onnx_model_uri,
-            extra_metadata: extra_metadata.unwrap_or_default(),
+            extra_metadata,
         }
     }
 
@@ -60,85 +64,85 @@ impl ModelDataInterfaceSaveMetadata {
     }
 }
 
-#[pyclass]
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ModelInterfaceMetadata {
-    #[pyo3(get)]
-    pub task_type: String,
-    #[pyo3(get)]
-    pub model_type: String,
-    #[pyo3(get)]
-    pub data_type: String,
-    #[pyo3(get)]
-    pub modelcard_uid: String,
-    #[pyo3(get)]
-    pub feature_map: HashMap<String, Feature>,
-    #[pyo3(get)]
-    pub sample_data_interface_type: String,
-    #[pyo3(get)]
-    pub save_metadata: ModelDataInterfaceSaveMetadata,
-    #[pyo3(get)]
-    pub extra_metadata: HashMap<String, String>,
-}
+//#[pyclass]
+//#[derive(Debug, Serialize, Deserialize, Clone)]
+//pub struct ModelInterfaceMetadata {
+//    #[pyo3(get)]
+//    pub task_type: String,
+//    #[pyo3(get)]
+//    pub model_type: String,
+//    #[pyo3(get)]
+//    pub data_type: String,
+//    #[pyo3(get)]
+//    pub modelcard_uid: String,
+//    #[pyo3(get)]
+//    pub feature_map: HashMap<String, Feature>,
+//    #[pyo3(get)]
+//    pub sample_data_interface_type: String,
+//    #[pyo3(get)]
+//    pub save_metadata: ModelDataInterfaceSaveMetadata,
+//    #[pyo3(get)]
+//    pub extra_metadata: HashMap<String, String>,
+//}
 
-#[pymethods]
-impl ModelInterfaceMetadata {
-    #[new]
-    #[pyo3(signature = (interface, save_metadata, extra_metadata=None))]
-    fn new(
-        interface: &Bound<'_, PyAny>,
-        save_metadata: ModelDataInterfaceSaveMetadata,
-        extra_metadata: Option<HashMap<String, String>>,
-    ) -> PyResult<Self> {
-        let task_type: String = interface
-            .getattr("task_type")
-            .map_err(|e| OpsmlError::new_err(format!("Failed to get task_type: {}", e)))?
-            .to_string();
-
-        let model_type: String = interface
-            .getattr("model_type")
-            .map_err(|e| OpsmlError::new_err(format!("Failed to get task_type: {}", e)))?
-            .to_string();
-
-        let data_type: String = interface
-            .getattr("data_type")
-            .map_err(|e| OpsmlError::new_err(format!("Failed to get task_type: {}", e)))?
-            .to_string();
-
-        let modelcard_uid: String = interface
-            .getattr("modelcard_uid")
-            .map_err(|e| OpsmlError::new_err(format!("Failed to get task_type: {}", e)))?
-            .to_string();
-
-        let feature_map: HashMap<String, Feature> = interface
-            .getattr("feature_map")
-            .map_err(|e| OpsmlError::new_err(format!("Failed to get feature_map: {}", e)))?
-            .extract()?;
-
-        let sample_data_interface_type: String = interface
-            .getattr("sample_data_interface_type")
-            .map_err(|e| {
-                OpsmlError::new_err(format!("Failed to get sample_data_interface_type: {}", e))
-            })?
-            .to_string();
-
-        Ok(ModelInterfaceMetadata {
-            task_type,
-            model_type,
-            data_type,
-            modelcard_uid,
-            feature_map,
-            sample_data_interface_type,
-            save_metadata,
-            extra_metadata: extra_metadata.unwrap_or_default(),
-        })
-    }
-
-    pub fn __str__(&self) -> String {
-        // serialize the struct to a string
-        PyHelperFuncs::__str__(self)
-    }
-}
+//#[pymethods]
+//impl ModelInterfaceMetadata {
+//    #[new]
+//    #[pyo3(signature = (interface, save_metadata, extra_metadata=None))]
+//    fn new(
+//        interface: &Bound<'_, PyAny>,
+//        save_metadata: ModelDataInterfaceSaveMetadata,
+//        extra_metadata: Option<HashMap<String, String>>,
+//    ) -> PyResult<Self> {
+//        let task_type: String = interface
+//            .getattr("task_type")
+//            .map_err(|e| OpsmlError::new_err(format!("Failed to get task_type: {}", e)))?
+//            .to_string();
+//
+//        let model_type: String = interface
+//            .getattr("model_type")
+//            .map_err(|e| OpsmlError::new_err(format!("Failed to get task_type: {}", e)))?
+//            .to_string();
+//
+//        let data_type: String = interface
+//            .getattr("data_type")
+//            .map_err(|e| OpsmlError::new_err(format!("Failed to get task_type: {}", e)))?
+//            .to_string();
+//
+//        let modelcard_uid: String = interface
+//            .getattr("modelcard_uid")
+//            .map_err(|e| OpsmlError::new_err(format!("Failed to get task_type: {}", e)))?
+//            .to_string();
+//
+//        let feature_map: HashMap<String, Feature> = interface
+//            .getattr("feature_map")
+//            .map_err(|e| OpsmlError::new_err(format!("Failed to get feature_map: {}", e)))?
+//            .extract()?;
+//
+//        let sample_data_interface_type: String = interface
+//            .getattr("sample_data_interface_type")
+//            .map_err(|e| {
+//                OpsmlError::new_err(format!("Failed to get sample_data_interface_type: {}", e))
+//            })?
+//            .to_string();
+//
+//        Ok(ModelInterfaceMetadata {
+//            task_type,
+//            model_type,
+//            data_type,
+//            modelcard_uid,
+//            feature_map,
+//            sample_data_interface_type,
+//            save_metadata,
+//            extra_metadata: extra_metadata.unwrap_or_default(),
+//        })
+//    }
+//
+//    pub fn __str__(&self) -> String {
+//        // serialize the struct to a string
+//        PyHelperFuncs::__str__(self)
+//}
+//}
 
 #[pyclass(subclass)]
 pub struct ModelInterface {
@@ -205,6 +209,13 @@ impl ModelInterface {
         Ok(())
     }
 
+    /// Save the model to a file
+    ///
+    /// # Arguments
+    ///
+    /// * `path` - The path to save the model to
+    /// * `kwargs` - Additional keyword arguments to pass to the save
+    ///
     #[pyo3(signature = (path, **kwargs))]
     pub fn save_model(
         &mut self,
@@ -213,20 +224,95 @@ impl ModelInterface {
         kwargs: Option<&Bound<'_, PyDict>>,
     ) -> PyResult<PathBuf> {
         // check if data is None
-        if self.data.is_none(py) {
+        if self.model.is_none(py) {
             return Err(OpsmlError::new_err(
-                "No data detected in interface for saving",
+                "No model detected in interface for saving",
             ));
         }
 
-        let save_path =
-            PathBuf::from(SaveName::TrainedModel.to_string()).with_extension(Suffix::Joblib);
+        let save_path = PathBuf::from(SaveName::Model.to_string()).with_extension(Suffix::Joblib);
         let full_save_path = path.join(&save_path);
         let joblib = py.import("joblib")?;
 
         // Save the data using joblib
-        joblib.call_method("dump", (&self.data, full_save_path), kwargs)?;
+        joblib.call_method("dump", (&self.model, full_save_path), kwargs)?;
 
         Ok(save_path)
+    }
+
+    /// Load the model from a file
+    ///
+    /// # Arguments
+    ///
+    /// * `path` - The path to load the model from
+    /// * `kwargs` - Additional keyword arguments to pass to the load
+    ///
+    #[pyo3(signature = (path, **kwargs))]
+    pub fn load_model(
+        &mut self,
+        py: Python,
+        path: PathBuf,
+        kwargs: Option<&Bound<'_, PyDict>>,
+    ) -> PyResult<()> {
+        let load_path = path.join(SaveName::Data).with_extension(Suffix::Joblib);
+        let joblib = py.import("joblib")?;
+
+        // Load the data using joblib
+        self.model = joblib.call_method("load", (load_path,), kwargs)?.into();
+
+        Ok(())
+    }
+
+    /// Create a feature schema
+    ///
+    /// # Arguments
+    ///
+    /// * `name` - Name of the feature
+    ///
+    /// # Returns
+    ///
+    /// * `PyResult<FeatureMap>` - FeatureMap
+    pub fn create_feature_schema(&mut self, py: Python) -> PyResult<FeatureSchema> {
+        // Create and insert the feature
+
+        let mut data = self.sample_data.get_data(py)?.bind(py).clone();
+
+        // if data is instance of DataInterface, get the data
+        if data.is_instance_of::<DataInterface>() {
+            data = data.getattr("data")?;
+        }
+
+        let feature_map = generate_feature_schema(&data, &self.data_type)?;
+
+        self.schema = feature_map.clone();
+
+        Ok(feature_map)
+    }
+
+    /// Save the interface model
+    ///
+    /// # Arguments
+    ///
+    /// * `py` - Python interpreter
+    /// * `path` - Path to save the data
+    /// * `kwargs` - Additional save kwargs
+    ///
+    /// # Returns
+    ///
+    /// * `PyResult<DataInterfaceSaveMetadata>` - DataInterfaceSaveMetadata
+    #[pyo3(signature = (path, **kwargs))]
+    pub fn save(
+        &mut self,
+        py: Python,
+        path: PathBuf,
+        kwargs: Option<&Bound<'_, PyDict>>,
+    ) -> PyResult<ModelInterfaceSaveMetadata> {
+        // save data
+        let save_path = self.save_model(py, path.clone(), kwargs)?;
+        self.schema = self.create_feature_schema(py)?;
+
+        Ok(ModelInterfaceSaveMetadata {
+            model_uri: save_path,
+        })
     }
 }
