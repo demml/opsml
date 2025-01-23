b5a3bff278bbc49b8c19585750bff98679deee6f
Thorrester <sjforrester32@gmail.com>
2025-01-23 11:27:15 -0500

    adding in onnx

--- crates/opsml_interfaces/src/model/onnx/mod.rs
+++ crates/opsml_interfaces/src/model/onnx/mod.rs
@@ -3,9 +3,11 @@ pub mod lightgbm;
 pub mod sklearn;
 pub mod types;
 pub mod updater;
+pub mod xgboost;
 
 pub use converter::*;
 pub use lightgbm::*;
 pub use sklearn::*;
 pub use types::*;
 pub use updater::*;
+pub use xgboost::*;
