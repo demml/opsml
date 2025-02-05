a95f580cf33ac1edfa1a0c90583e904dd5e241ea
Thorrester <sjforrester32@gmail.com>
2025-02-05 14:05:31 -0500

    test

--- py-opsml/tests/interfaces/model/test_tensorflow.py
+++ py-opsml/tests/interfaces/model/test_tensorflow.py
@@ -8,6 +8,7 @@ import pytest
 IS_312 = sys.version_info >= (3, 12)
 
 
+@pytest.mark.tensorflow
 @pytest.mark.skipif((EXCLUDE or IS_312), reason="skipping")
 def test_tensorflow_model(tf_sequential_model: Tuple[tf.keras.Sequential, np.ndarray]):
     model, data = tf_sequential_model
