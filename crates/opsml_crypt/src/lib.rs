42bf9596c0362ecb52f3cf62ebdcf03e4d40d15a
Thorrester <sjforrester32@gmail.com>
2025-02-10 16:45:01 -0500

    working on encryption

--- crates/opsml_crypt/src/lib.rs
+++ crates/opsml_crypt/src/lib.rs
@@ -1,3 +1,5 @@
-pub mod decrypt;
-pub mod encrypt;
+pub mod file;
 pub mod key;
+
+pub use file::*;
+pub use key::*;
---
+++ crates/opsml_utils/src/progress.rs
@@ -0,0 +1,38 @@
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use opsml_error::ProgressError;
+
+pub struct Progress {
+    multi_progress: MultiProgress,
+    style: ProgressStyle,
+}
+
+impl Progress {
+    pub fn new() -> Self {
+        let m = MultiProgress::new();
+        let style = ProgressStyle::with_template(
+            "[{elapsed_precise}] {bar:40.green/magenta} {pos:>7}/{len:7} {msg}",
+        )
+        .unwrap()
+        .progress_chars("--");
+
+        Progress {
+            multi_progress: m,
+            style,
+        }
+    }
+
+    pub fn create_bar(&self, message: String, total: u64) -> ProgressBar {
+        let bar = self.multi_progress.add(ProgressBar::new(total));
+        bar.set_style(self.style.clone());
+        bar.set_message(message);
+        bar
+    }
+
+    pub fn finish(&self) -> Result<(), ProgressError> {
+        self.multi_progress
+            .clear()
+            .map_err(|e| ProgressError::Error(format!("Failed to clear progress bar: {}", e)))?;
+
+        Ok(())
+    }
+}
