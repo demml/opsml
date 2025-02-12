1fce087540dc520de8697d13398525c19aef61db
Thorrester <sjforrester32@gmail.com>
2025-02-12 15:36:54 -0500

    Update __init__.py

---
+++ py-opsml/python/opsml/registry/__init__.py
@@ -0,0 +1,8 @@
+# type: ignore
+from .. import registry
+
+Card = registry.Card
+CardList = registry.CardList
+RegistryType = registry.RegistryType
+RegistryMode = registry.RegistryMode
+CardRegistry = registry.CardRegistry
