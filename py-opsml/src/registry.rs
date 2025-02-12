a8a24187161ed853d8b2cd9894d30471eb8963f0
Thorrester <sjforrester32@gmail.com>
2025-02-12 15:38:42 -0500

    Update registry.rs

--- py-opsml/src/registry.rs
+++ py-opsml/src/registry.rs
@@ -1,4 +1,5 @@
 use opsml_registry::CardRegistry;
+use opsml_types::contracts::{Card, CardList};
 use opsml_types::{RegistryMode, RegistryType};
 use pyo3::prelude::*;
 
@@ -7,5 +8,7 @@ pub fn registry(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<CardRegistry>()?;
     m.add_class::<RegistryType>()?;
     m.add_class::<RegistryMode>()?;
+    m.add_class::<Card>()?;
+    m.add_class::<CardList>()?;
     Ok(())
 }
