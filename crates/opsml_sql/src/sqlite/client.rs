bd62d54a8b8aad3eeda09fe5f79fb9df6808fa2b
Thorrester <sjforrester32@gmail.com>
2025-02-19 09:21:12 -0500

    sqlite test

--- crates/opsml_sql/src/sqlite/client.rs
+++ crates/opsml_sql/src/sqlite/client.rs
@@ -2188,5 +2188,11 @@ mod tests {
 
         assert_eq!(key.uid, data_card.uid);
         assert_eq!(key.encrypted_key, encrypted_key);
+
+        // delete
+        client
+            .delete_artifact_key(&data_card.uid, &RegistryType::Data.to_string())
+            .await
+            .unwrap();
     }
 }
