0309e2bc648c5a3e62a2837b7355879766e62ca3
Thorrester <sjforrester32@gmail.com>
2025-02-19 22:35:06 -0500

    context manager

--- py-opsml/tests/interfaces/run/test_runcard.py
+++ py-opsml/tests/interfaces/run/test_runcard.py
@@ -4,5 +4,9 @@ from opsml.card import RunCard
 def test_runcard():
     card = RunCard(repository="test", name="test")
 
-    print(card.compute_environment)
+
+def test_runcard_context():
+    with RunCard.start_run(repository="test", name="test") as run:
+        print(run)
+
     a
