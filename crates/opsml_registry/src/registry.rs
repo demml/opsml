3d40c519028b56175429691a0531fae45b9fc556
Thorrester <sjforrester32@gmail.com>
2025-02-20 13:42:08 -0500

    moving things around

--- crates/opsml_registry/src/registry.rs
+++ crates/opsml_registry/src/registry.rs
@@ -1,10 +1,8 @@
 use crate::enums::OpsmlRegistry;
-use opsml_cards::{DataCard, ModelCard};
+use crate::utils::{download_card, upload_card_artifacts, verify_card};
 use opsml_colors::Colorize;
-use opsml_crypt::{decrypt_directory, encrypt_directory};
 use opsml_error::error::OpsmlError;
 use opsml_error::error::RegistryError;
-use opsml_error::UtilError;
 use opsml_semver::VersionType;
 use opsml_settings::config::OpsmlConfig;
 use opsml_storage::FileSystemStorage;
@@ -12,13 +10,64 @@ use opsml_types::*;
 use opsml_types::{cards::CardTable, contracts::*};
 use opsml_utils::{clean_string, unwrap_pystring};
 use pyo3::prelude::*;
-use pyo3::IntoPyObjectExt;
 use std::path::PathBuf;
 use std::sync::Arc;
 use tempfile::TempDir;
 use tokio::sync::Mutex;
 
 use tracing::{debug, error, instrument};
+
+/// Extract registry type from a PyAny object
+///
+/// # Arguments
+///
+/// * `registry_type` - PyAny object
+///
+/// # Returns
+///
+/// * `Result<RegistryType, OpsmlError>` - Result
+///
+/// # Errors
+///
+/// * `OpsmlError` - Error
+fn extract_registry_type(registry_type: &Bound<'_, PyAny>) -> PyResult<RegistryType> {
+    match registry_type.is_instance_of::<RegistryType>() {
+        true => registry_type.extract::<RegistryType>().map_err(|e| {
+            error!("Failed to extract registry type: {}", e);
+            OpsmlError::new_err(e.to_string())
+        }),
+        false => {
+            let registry_type = registry_type.extract::<String>().unwrap();
+            RegistryType::from_string(&registry_type).map_err(|e| {
+                error!("Failed to convert string to registry type: {}", e);
+                OpsmlError::new_err(e.to_string())
+            })
+        }
+    }
+}
+
+/// Initialize registry and file system storage
+///
+///
+/// # Arguments
+///
+/// * `registry_type` - RegistryType
+///
+/// # Returns
+///
+/// * `Result<(OpsmlRegistry, Arc<Mutex<FileSystemStorage>>), RegistryError>` - Result
+///
+/// # Errors
+///
+/// * `RegistryError` - Error
+async fn initialize_registry_and_fs(
+    registry_type: RegistryType,
+) -> Result<(OpsmlRegistry, Arc<Mutex<FileSystemStorage>>), RegistryError> {
+    let mut settings = OpsmlConfig::default().storage_settings()?;
+    let registry = OpsmlRegistry::new(registry_type).await?;
+    let fs = Arc::new(Mutex::new(FileSystemStorage::new(&mut settings).await?));
+    Ok((registry, fs))
+}
 
 pub struct CardArgs {
     pub uid: String,
@@ -46,27 +95,13 @@ impl CardRegistry {
         debug!("Creating new registry client");
 
         // check if registry_type is a valid RegistryType or String
-        let registry_type = if registry_type.is_instance_of::<RegistryType>() {
-            registry_type.extract::<RegistryType>().map_err(|e| {
-                error!("Failed to extract registry type: {}", e);
-                OpsmlError::new_err(e.to_string())
-            })?
-        } else {
-            let registry_type = registry_type.extract::<String>().unwrap();
-            RegistryType::from_string(&registry_type)?
-        };
+        let registry_type = extract_registry_type(registry_type)?;
 
         // Create a new tokio runtime for the registry (needed for async calls)
         let rt = Arc::new(tokio::runtime::Runtime::new().unwrap());
 
         let (registry, fs) = rt
-            .block_on(async {
-                let mut settings = OpsmlConfig::default().storage_settings()?;
-                let registry = OpsmlRegistry::new(registry_type.clone()).await?;
-                let fs = Arc::new(Mutex::new(FileSystemStorage::new(&mut settings).await?));
-
-                Ok::<_, RegistryError>((registry, fs))
-            })
+            .block_on(async { initialize_registry_and_fs(registry_type.clone()).await })
             .map_err(|e| OpsmlError::new_err(e.to_string()))?;
 
         Ok(Self {
@@ -156,33 +191,17 @@ impl CardRegistry {
         // Wrap all operations in a single block_on to handle async operations
         self.runtime
             .block_on(async {
-                // Verify card for registration
-                Self::verify_card(card, &self.registry_type).await?;
-
-                // register card
-                // (1) creates new version
-                // (2) Inserts card record into db
-                // (3) Creates encryption key and returns it
-                let create_response = Self::_register_card(
-                    &mut self.registry,
+                Self::verify_and_register_card(
                     card,
-                    &self.registry_type,
+                    &mut self.registry,
+                    &mut self.fs,
                     version_type,
                     pre_tag,
                     build_tag,
+                    save_kwargs,
+                    &self.registry_type,
                 )
-                .await?;
-
-                // Update card attributes
-                Self::update_card_with_server_response(&create_response, card)?;
-
-                // Save card artifacts to temp path
-                let tmp_path = Self::save_card_artifacts(card, save_kwargs).await?;
-
-                // Save artifacts
-                Self::upload_card_artifacts(tmp_path, &mut self.fs, &create_response.key).await?;
-
-                Ok(())
+                .await
             })
             .map_err(|e: RegistryError| OpsmlError::new_err(e.to_string()))
     }
@@ -228,7 +247,7 @@ impl CardRegistry {
                     })
                     .await?;
 
-                Self::download_card(py, key, &mut self.fs, &self.runtime, interface).await
+                download_card(py, key, &mut self.fs, &self.runtime, interface).await
             })
             .map_err(|e| OpsmlError::new_err(e.to_string()))?;
 
@@ -266,7 +285,7 @@ impl CardRegistry {
 
                 let tmp_path = Self::save_card(card).await?;
 
-                Self::upload_card_artifacts(tmp_path, &mut self.fs, &key).await?;
+                upload_card_artifacts(tmp_path, &mut self.fs, &key).await?;
 
                 Ok(())
             })
@@ -275,6 +294,42 @@ impl CardRegistry {
 }
 
 impl CardRegistry {
+    async fn verify_and_register_card(
+        card: &Bound<'_, PyAny>,
+        registry: &mut OpsmlRegistry,
+        fs: &mut Arc<Mutex<FileSystemStorage>>,
+        version_type: VersionType,
+        pre_tag: Option<String>,
+        build_tag: Option<String>,
+        save_kwargs: Option<&Bound<'_, PyAny>>,
+        registry_type: &RegistryType,
+    ) -> Result<(), RegistryError> {
+        // Verify card for registration
+        verify_card(card, registry_type).await?;
+
+        // Register card
+        let create_response = Self::_register_card(
+            registry,
+            card,
+            registry_type,
+            version_type,
+            pre_tag,
+            build_tag,
+        )
+        .await?;
+
+        // Update card attributes
+        Self::update_card_with_server_response(&create_response, card)?;
+
+        // Save card artifacts to temp path
+        let tmp_path = Self::save_card_artifacts(card, save_kwargs).await?;
+
+        // Save artifacts
+        upload_card_artifacts(tmp_path, fs, &create_response.key).await?;
+
+        Ok(())
+    }
+
     /// Update card with server response.
     /// These are attributes that are overwritten and set by the server
     ///
@@ -317,60 +372,6 @@ impl CardRegistry {
                 error!("Failed to set app_env: {}", e);
                 RegistryError::Error("Failed to set app_env".to_string())
             })?;
-
-        Ok(())
-    }
-
-    #[instrument(skip_all)]
-    pub async fn verify_card(
-        card: &Bound<'_, PyAny>,
-        registry_type: &RegistryType,
-    ) -> Result<(), RegistryError> {
-        if card.is_instance_of::<ModelCard>() {
-            let datacard_uid = card
-                .getattr("metadata")
-                .unwrap()
-                .getattr("datacard_uid")
-                .unwrap()
-                .extract::<Option<String>>()
-                .unwrap();
-
-            if let Some(datacard_uid) = datacard_uid {
-                // check if datacard exists in the registry
-                let exists = OpsmlRegistry::new(RegistryType::Data)
-                    .await?
-                    .check_card_uid(&datacard_uid)
-                    .await?;
-
-                if !exists {
-                    return Err(RegistryError::Error(
-                        "Datacard does not exist in the registry".to_string(),
-                    ));
-                }
-            }
-        }
-
-        let card_registry_type = card
-            .getattr("registry_type")
-            .map_err(|e| {
-                error!("Failed to get card type: {}", e);
-                RegistryError::Error("Failed to get card type".to_string())
-            })?
-            .extract::<RegistryType>()
-            .map_err(|e| {
-                error!("Failed to extract card type: {}", e);
-                RegistryError::Error("Failed to extract card type".to_string())
-            })?;
-
-        // assert that the card registry type is the same as the registry type
-        if card_registry_type != *registry_type {
-            return Err(RegistryError::Error(
-                "Card registry type does not match registry type".to_string(),
-            ));
-        }
-
-        debug!("Verified card");
-        println!("✓ {}", Colorize::green("verified card"));
 
         Ok(())
     }
@@ -439,48 +440,6 @@ impl CardRegistry {
             })?;
 
         Ok(tmp_path)
-    }
-
-    /// Save card artifacts to storage
-    /// Using a runtime, this method with
-    /// (1) create an artifact key to be used to encrypt data
-    /// (2) save the card to a temporary directory (with encryption)
-    /// (3) Transfer all files in the temporary directory to the storage system
-    ///
-    /// # Arguments
-    ///
-    /// * `py` - Python interpreter
-    /// * `card` - Card to save
-    /// * `save_kwargs` - Optional save kwargs
-    ///
-    /// # Returns
-    ///
-    /// * `Result<(), RegistryError>` - Result
-    #[instrument(skip_all)]
-    async fn upload_card_artifacts(
-        path: PathBuf,
-        fs: &mut Arc<Mutex<FileSystemStorage>>,
-        key: &ArtifactKey,
-    ) -> Result<(), RegistryError> {
-        // create temp path for saving
-        let encryption_key = key
-            .get_decrypt_key()
-            .map_err(|e| RegistryError::Error(e.to_string()))?;
-
-        encrypt_directory(&path, &encryption_key)?;
-        fs.lock()
-            .await
-            .put(&path, &key.storage_path(), true)
-            .await?;
-
-        println!(
-            "...✓ {}",
-            Colorize::green("saved card artifacts to storage")
-        );
-
-        debug!("Saved card artifacts to storage");
-
-        Ok(())
     }
 
     /// Register a card in the registry
@@ -608,101 +567,6 @@ impl CardRegistry {
         debug!("Successfully updated card");
 
         Ok(key)
-    }
-
-    async fn download_card<'py>(
-        py: Python<'py>,
-        key: ArtifactKey,
-        fs: &mut Arc<Mutex<FileSystemStorage>>,
-        rt: &Arc<tokio::runtime::Runtime>,
-        interface: Option<&Bound<'py, PyAny>>,
-        // given uid or (name, repo, versiont)
-        // get card uid
-        // get artifact_key from registry for a uid
-        // get storage_uri
-    ) -> Result<Bound<'py, PyAny>, RegistryError> {
-        let decryption_key = key.get_decrypt_key().map_err(|e| {
-            error!("Failed to get decryption key: {}", e);
-            RegistryError::Error(e.to_string())
-        })?;
-
-        let tmp_dir = TempDir::new().map_err(|e| {
-            error!("Failed to create temporary directory: {}", e);
-            RegistryError::Error("Failed to create temporary directory".to_string())
-        })?;
-
-        let tmp_path = tmp_dir.into_path();
-        let rpath = PathBuf::from(&key.storage_key);
-
-        let rpath = rpath.join(SaveName::Card).with_extension(Suffix::Json);
-
-        // add Card.json to tmp_path and rpath
-        let lpath = tmp_path.join(SaveName::Card).with_extension(Suffix::Json);
-
-        fs.lock().await.get(&lpath, &rpath, false).await?;
-        decrypt_directory(&tmp_path, &decryption_key)?;
-
-        let json_string = std::fs::read_to_string(&lpath).map_err(|e| {
-            error!("Failed to read card json: {}", e);
-            UtilError::ReadError
-        })?;
-
-        let card = Self::card_from_string(py, json_string, interface, key, fs, rt)?;
-
-        Ok(card)
-    }
-
-    fn card_from_string<'py>(
-        py: Python<'py>,
-        card_json: String,
-        interface: Option<&Bound<'py, PyAny>>,
-        key: ArtifactKey,
-        fs: &mut Arc<Mutex<FileSystemStorage>>,
-        rt: &Arc<tokio::runtime::Runtime>,
-    ) -> Result<Bound<'py, PyAny>, RegistryError> {
-        let card = match key.registry_type {
-            RegistryType::Model => {
-                let mut card =
-                    ModelCard::model_validate_json(py, card_json, interface).map_err(|e| {
-                        error!("Failed to validate model card: {}", e);
-                        RegistryError::Error(e.to_string())
-                    })?;
-
-                card.artifact_key = Some(key);
-                card.fs = Some(fs.clone());
-                card.rt = Some(rt.clone());
-
-                card.into_bound_py_any(py).map_err(|e| {
-                    error!("Failed to convert card to bound: {}", e);
-                    RegistryError::Error(e.to_string())
-                })?
-            }
-
-            RegistryType::Data => {
-                let mut card =
-                    DataCard::model_validate_json(py, card_json, interface).map_err(|e| {
-                        error!("Failed to validate data card: {}", e);
-                        RegistryError::Error(e.to_string())
-                    })?;
-
-                card.artifact_key = Some(key);
-                card.fs = Some(fs.clone());
-                card.rt = Some(rt.clone());
-
-                card.into_bound_py_any(py).map_err(|e| {
-                    error!("Failed to convert card to bound: {}", e);
-                    RegistryError::Error(e.to_string())
-                })?
-            }
-
-            _ => {
-                return Err(RegistryError::Error(
-                    "Registry type not supported".to_string(),
-                ));
-            }
-        };
-
-        Ok(card)
     }
 }
 
