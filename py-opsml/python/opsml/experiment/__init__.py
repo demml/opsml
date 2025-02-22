c957f81eda9ac01593e2c64f22c3aef61a734ba4
Thorrester <sjforrester32@gmail.com>
2025-02-22 13:36:24 -0500

    imports

--- py-opsml/python/opsml/experiment/__init__.py
+++ py-opsml/python/opsml/experiment/__init__.py
@@ -4,5 +4,7 @@ from .. import experiment  # noqa: F401
 
 Experiment = experiment.Experiment
 start_experiment = experiment.start_experiment
+Metric = experiment.Metric
+Parameter = experiment.Parameter
 
-__all__ = ["Experiment"]
+__all__ = ["Experiment", "start_experiment", "Metric", "Parameter"]
