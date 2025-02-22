06eb02bf9b46bf3f577a56c225f644642a64d6d7
Thorrester <sjforrester32@gmail.com>
2025-02-21 21:36:04 -0500

    hardware queue

---
+++ crates/opsml_experiment/src/hardware_queue.rs
@@ -0,0 +1,97 @@
+use chrono::Utc;
+use opsml_error::ExperimentError;
+use opsml_registry::enums::OpsmlRegistry;
+use opsml_types::{cards::HardwareMetricLogger, contracts::HardwareMetricRequest};
+use std::sync::Arc;
+use tokio::sync::{watch, Mutex};
+use tokio::time::{self, Duration};
+use tracing::Instrument;
+use tracing::{debug, error, info, info_span};
+
+async fn insert_metrics(
+    registry: Arc<Mutex<OpsmlRegistry>>,
+    hw_logger: &mut HardwareMetricLogger,
+    experiment_uid: &str,
+) -> Result<(), ExperimentError> {
+    debug!("Locking registry");
+    let mut registry = registry.lock().await;
+
+    debug!("Getting metrics");
+    let metrics = hw_logger.get_metrics();
+    let request = HardwareMetricRequest {
+        experiment_uid: experiment_uid.to_string(),
+        metrics: metrics.clone(),
+    };
+    registry.insert_hardware_metrics(request).await?;
+
+    Ok(())
+}
+
+fn start_background_task(
+    rt: Arc<tokio::runtime::Runtime>,
+    registry: Arc<Mutex<OpsmlRegistry>>,
+    mut stop_rx: watch::Receiver<()>,
+    duration: Duration,
+    experiment_uid: String,
+) -> Result<(), ExperimentError> {
+    let registry = registry.clone();
+    let handle = rt.clone();
+    let mut last_inserted = Utc::now().naive_utc();
+    let mut hw_logger = HardwareMetricLogger::new();
+
+    // spawn the background task using the already cloned handle
+    let future = async move {
+        loop {
+            tokio::select! {
+
+                _ = time::sleep(duration) => {
+
+                    debug!("Getting metrics");
+                    let now = Utc::now().naive_utc();
+                    let elapsed = now - last_inserted;
+
+                    if elapsed.num_seconds() >= 30 {
+                        let inserted = insert_metrics(registry.clone(), &mut hw_logger, &experiment_uid).await;
+
+                        if let Err(e) = inserted {
+                            error!("Error inserting metrics: {:?}", e);
+                        }
+
+                        last_inserted = now;
+                    }
+                },
+                _ = stop_rx.changed() => {
+                    info!("Stopping background task");
+
+                    break;
+                }
+            }
+        }
+    };
+
+    handle.spawn(future.instrument(info_span!("PSI Background Polling")));
+
+    Ok(())
+}
+
+pub struct HardwareQueue {
+    stop_tx: watch::Sender<()>,
+}
+
+impl HardwareQueue {
+    pub fn start(
+        rt: Arc<tokio::runtime::Runtime>,
+        duration: Duration,
+        registry: Arc<Mutex<OpsmlRegistry>>,
+        experiment_uid: String,
+    ) -> Result<Self, ExperimentError> {
+        let (stop_tx, stop_rx) = watch::channel(());
+
+        start_background_task(rt, registry, stop_rx, duration, experiment_uid)?;
+
+        Ok(Self { stop_tx })
+    }
+    pub fn stop(&mut self) {
+        let _ = self.stop_tx.send(());
+    }
+}
--- crates/opsml_experiment/src/lib.rs
+++ crates/opsml_experiment/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod experiment;
+pub mod hardware_queue;
 pub mod types;
 
 pub use experiment::*;
+pub use hardware_queue::*;
 pub use types::*;
