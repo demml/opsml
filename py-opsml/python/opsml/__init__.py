c4f504de203aa62f5bb318edbddd693ab53761d7
Thorrester <sjforrester32@gmail.com>
2025-02-23 08:09:06 -0500

    format

--- py-opsml/python/opsml/__init__.py
+++ py-opsml/python/opsml/__init__.py
@@ -1,4 +1,4 @@
 # type: ignore
 # pylint: disable=no-name-in-module
 
-from .opsml import card, core, data, model, scouter, test, experiment  # noqa: F401
+from .opsml import card, core, data, experiment, model, scouter, test  # noqa: F401
--- py-opsml/python/opsml/experiment/__init__.pyi
+++ py-opsml/python/opsml/experiment/__init__.pyi
@@ -1,6 +1,7 @@
-from typing import Optional, Union
-from pathlib import Path
 from datetime import datetime
+from pathlib import Path
+from typing import Optional, Union
+
 from ..card import ExperimentCard
 
 class Experiment:
