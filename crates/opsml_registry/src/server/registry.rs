3f72185e6855f3a29427f616020b5097fb97414b
Thorrester <sjforrester32@gmail.com>
2025-02-28 14:32:52 -0500

    Update registry.rs

--- crates/opsml_registry/src/server/registry.rs
+++ crates/opsml_registry/src/server/registry.rs
@@ -90,6 +90,10 @@ pub mod server_logic {
                     let cards = data.into_iter().map(convert_auditcard).collect();
                     Ok(cards)
                 }
+                CardResults::Prompt(data) => {
+                    let cards = data.into_iter().map(convert_promptcard).collect();
+                    Ok(cards)
+                }
             }
         }
 
@@ -239,6 +243,19 @@ pub mod server_logic {
                     );
                     ServerCard::Audit(server_card)
                 }
+                Card::Prompt(client_card) => {
+                    let server_card = PromptCardRecord::new(
+                        client_card.name,
+                        client_card.repository,
+                        version,
+                        client_card.tags,
+                        client_card.prompt_type,
+                        client_card.experimentcard_uid,
+                        client_card.auditcard_uid,
+                        client_card.username,
+                    );
+                    ServerCard::Prompt(server_card)
+                }
             };
 
             self.sql_client
@@ -386,6 +403,33 @@ pub mod server_logic {
                         username: client_card.username,
                     };
                     ServerCard::Audit(server_card)
+                }
+
+                Card::Prompt(client_card) => {
+                    let version = Version::parse(&client_card.version).map_err(|e| {
+                        error!("Failed to parse version: {}", e);
+                        RegistryError::Error("Failed to parse version".to_string())
+                    })?;
+
+                    let server_card = PromptCardRecord {
+                        uid: client_card.uid,
+                        created_at: client_card.created_at,
+                        app_env: client_card.app_env,
+                        name: client_card.name,
+                        repository: client_card.repository,
+                        major: version.major as i32,
+                        minor: version.minor as i32,
+                        patch: version.patch as i32,
+                        pre_tag: Some(version.pre.to_string()),
+                        build_tag: Some(version.build.to_string()),
+                        version: client_card.version,
+                        tags: SqlxJson(client_card.tags),
+                        prompt_type: client_card.prompt_type,
+                        experimentcard_uid: client_card.experimentcard_uid,
+                        auditcard_uid: client_card.auditcard_uid,
+                        username: client_card.username,
+                    };
+                    ServerCard::Prompt(server_card)
                 }
             };
 
