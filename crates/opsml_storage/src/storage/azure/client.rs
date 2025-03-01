4439841570cc0af6b10747f186fb9582c880d697
Thorrester <sjforrester32@gmail.com>
2025-03-01 13:32:47 -0500

    Update client.rs

--- crates/opsml_storage/src/storage/azure/client.rs
+++ crates/opsml_storage/src/storage/azure/client.rs
@@ -657,7 +657,7 @@ mod tests {
     use opsml_error::error::StorageError;
     use opsml_settings::config::OpsmlConfig;
     use rand::distr::Alphanumeric;
-    use rand::thread_rng;
+    use rand::rng;
     use rand::Rng;
     use std::path::Path;
     use tempfile::TempDir;
@@ -666,7 +666,7 @@ mod tests {
         let mut file = File::create(name).expect("Could not create sample file.");
 
         while file.metadata().unwrap().len() <= chunk_size * 2 {
-            let rand_string: String = thread_rng()
+            let rand_string: String = rng()
                 .sample_iter(&Alphanumeric)
                 .take(256)
                 .map(char::from)
