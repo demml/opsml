c3037d8cf02fbcdd498b99addca79533b4827489
Thorrester <sjforrester32@gmail.com>
2025-03-01 08:04:09 -0500

    tying for potato_head

--- py-opsml/python/opsml/__init__.py
+++ py-opsml/python/opsml/__init__.py
@@ -1,7 +1,7 @@
 # type: ignore
 # pylint: disable=no-name-in-module
 
-from .opsml import card, core, data, experiment, model, scouter, test  # noqa: F401
+from .opsml import card, core, data, experiment, model, scouter, test, potato_head  # noqa: F401
 
 CardRegistry = card.CardRegistry
 RegistryType = card.RegistryType
