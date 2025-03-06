1b7d76400ca099afd4ec82da4cdeea6405a5d35d
Thorrester <sjforrester32@gmail.com>
2025-03-06 10:30:00 -0500

    warning

---
+++ crates/opsml_server/opsml_ui/src/lib/components/auth/LoginWarning.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+import {OctagonAlert } from 'lucide-svelte';
+let { errorMessage = 'unset'} = $props();
+
+</script>
+
+<aside class="mb-10 alert mx-auto shadow rounded-2xl bg-rose-500 md:w-1/3 items-center text-white">
+  <!-- Icon -->
+  <div>
+      <OctagonAlert color="#ffffff" />
+  </div>
+  <!-- Message -->
+  <div class="alert-message">
+      <h3 class="h4">Problem Encountered</h3>
+      <p class="text-sm">{errorMessage}</p>
+  </div>
+  <!-- Actions -->
+</aside>
---
+++ crates/opsml_server/opsml_ui/src/routes/opsml/auth/login/+page.svelte
@@ -0,0 +1,88 @@
+
+<script lang="ts">
+  import { goto, invalidateAll } from "$app/navigation";
+  import logo from "$lib/images/opsml-logo.png";
+  import LoginWarning from "$lib/components/LoginWarning.svelte";
+  import { CommonPaths } from "$lib/scripts/types";
+  import { goTop, sleep } from "$lib/scripts/utils";
+  import { authManager, loggedIn } from "$lib/scripts/auth/authManager";
+
+  let username = '';
+  let password = '';
+
+  let showLoginError: boolean = false;
+
+  /** @type {import('./$types').PageData} */
+  export let data;
+  let previousPath = data.previousPath;
+
+  async function handleLogin() {
+    // Handle login logic here
+    let authenticated = await authManager.login(username, password);
+
+    if (authenticated === true) {
+      loggedIn.set({ isLoggedIn: true });
+
+      // need to reload the page to update the nav bar
+      if (previousPath) {
+        goto(previousPath);
+      } else {
+        goto(CommonPaths.HOME);
+      }
+    } else {
+      showLoginError = true;
+      goTop();
+    }
+
+  }
+
+ 
+
+
+</script>
+
+  <section class="pt-24 border-gray-100 col-span-full flex-1 pb-16 md:pb-0 items-center">
+
+    {#if showLoginError}
+      <LoginWarning
+      errorMessage="Invalid username or password"
+      />
+    {/if}
+
+    <form class="z-10 mx-auto rounded-2xl bg-slate-100 border shadow p-4 md:w-96 md:px-5" on:submit|preventDefault={handleLogin}>
+
+      <img alt="OpsML logo" class="mx-auto -mt-12 mb-2 w-20" src={logo}>
+      <h1 class="pt-1 text-center text-3xl font-bold text-primary-500">Log In</h1>
+      <p class="mb-6 text-center text-gray-500">New to OpsML?
+        <a class="underline hover:text-primary-700" href={CommonPaths.REGISTER}>Register</a>
+      </p>
+
+      <div class="mb-8 grid grid-cols-1 gap-3">
+        <label class="text-primary-500">Username
+          <input
+            class="input rounded-lg bg-slate-200 hover:bg-slate-100"
+            type="text" 
+            placeholder="Username"
+            bind:value={username}
+          />
+        </label>
+
+        <label class="text-primary-500">Password
+          <input
+            class="input rounded-lg bg-slate-200 hover:bg-slate-100"
+            type="text" 
+            placeholder="Password"
+            bind:value={password}
+
+          />
+        </label>
+      </div>
+
+      <div class="grid justify-items-center">
+        <button type="submit" class="btn bg-primary-500 text-white rounded-lg md:w-72 justify-self-center mb-2">
+          <span>Login</span>
+        </button>
+        <a class="text-primary-500 hover:text-primary-700" href={CommonPaths.FORGOT}>Forgot password?</a>
+      </div>
+    </form>
+  </section>
