8b9d54beecf4d60ce14e1afa618b8e62eb445c9c
Thorrester <sjforrester32@gmail.com>
2025-03-12 22:38:46 -0400

    working on readme

--- crates/opsml_server/opsml_ui/src/lib/components/readme/Readme.svelte
+++ crates/opsml_server/opsml_ui/src/lib/components/readme/Readme.svelte
@@ -33,14 +33,13 @@
 
   if (toggle === 'edit') {
     // show the editor
-    document.getElementById("editor")!.style.display = "block";
+    //document.getElementById("editor")!.style.display = "block";
 
     mode = 'edit';
 
   } else {
     // hide the editor
-    document.getElementById("editor")!.style.display = "none";
-
+    //document.getElementById("editor")!.style.display = "none";
     content = editor.state.doc.toString();
 
     mode = 'preview';
@@ -90,44 +89,46 @@
           editor.destroy();
       }
   });
+
 </script>
 
-<div class="flex items-center justify-center pt-8">
-  <div class="justify-center w-3/4">
-      <div class="flex rounded-t-lg border border-gray px-3 py-2 min-w-96 justify-between">
-          <div class="flex gap-4">
-              <button 
-                  class="font-semibold {mode === 'edit' ? 'border-b-2 border-primary-500' : ''}"
-                  onclick={() => toggle('edit')}
-              >
-                  Edit
-              </button>
-              <button 
-                  class="font-semibold {mode === 'preview' ? 'border-b-2 border-primary-500' : ''}"
-                  onclick={() => toggle('preview')}
-              >
-                  Preview
-              </button>
-          </div>
+<div class="flex flex-col">
+  <div class="flex px-3 py-2 min-w-96 justify-between border-black border-b-2 pb-4 bg-primary-300">
+    <div class="flex gap-4 justify-start">
+      <button 
+          class="btn btn-md bg-primary-500 border-black border-black border-2 text-black {mode === 'edit' ? '' : 'shadow shadow-hover'}"
+          onclick={() => toggle('edit')}
+      >
+          Edit
+      </button>
+      <button 
+          class="btn btn-md bg-primary-500 border-black border-black border-2 text-black {mode === 'preview' ? '' : 'shadow shadow-hover'}"
+          onclick={() => toggle('preview')}
+      >
+          Preview
+      </button>
+    </div>
 
-          <button 
-              type="button" 
-              class="btn bg-primary-500 text-white" 
-              onclick={saveReadme}
-          >
-              Save
-          </button>
-      </div>
-
-      <div class="min-w-96 w-full border border-gray overflow-scroll">
-          {#if mode === 'edit'}
-            <div id="editor"></div>
-          {:else}
-              <Markdown source={$content} />
-          {/if}
-      </div>
+    <div class="justify-end">
+      <button 
+          type="button" 
+          class="btn btn-md bg-primary-500 border-black border-black border-2 text-black shadow shadow-hover" 
+          onclick={saveReadme}
+        >Save
+      </button>
+    </div>
   </div>
+
+  <div class="min-w-96 w-full border border-gray overflow-scroll">
+    {#if mode === 'edit'}
+      <div id="editor"></div>
+    {:else}
+        <Markdown source={$content} />
+    {/if}
+  </div>
+  
 </div>
+
 
 <style>
 
