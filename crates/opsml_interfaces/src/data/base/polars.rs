e7baa0857e8fef76f0e4e57b430cc6ad8ef34497
Thorrester <sjforrester32@gmail.com>
2025-03-19 13:50:28 -0400

    update data loading logic

--- crates/opsml_interfaces/src/data/base/polars.rs
+++ crates/opsml_interfaces/src/data/base/polars.rs
@@ -213,14 +213,15 @@ impl PolarsData {
         ))
     }
 
-    #[pyo3(signature = (path, load_kwargs=None))]
+    #[pyo3(signature = (path, metadata, load_kwargs=None))]
     pub fn load(
         mut self_: PyRefMut<'_, Self>,
         py: Python,
         path: PathBuf,
+        metadata: DataInterfaceSaveMetadata,
         load_kwargs: Option<DataLoadKwargs>,
     ) -> PyResult<()> {
-        let load_path = path.join(SaveName::Data).with_extension(Suffix::Parquet);
+        let load_path = path.join(metadata.data_uri);
         let load_kwargs = load_kwargs.unwrap_or_default();
 
         let polars = PyModule::import(py, "polars")?;
