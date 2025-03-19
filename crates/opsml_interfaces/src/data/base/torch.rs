f77b57c00a1de37850edb945a8789a5275cc5afc
Thorrester <sjforrester32@gmail.com>
2025-03-19 13:51:45 -0400

    loading for torch

--- crates/opsml_interfaces/src/data/base/torch.rs
+++ crates/opsml_interfaces/src/data/base/torch.rs
@@ -133,14 +133,15 @@ impl TorchData {
         ))
     }
 
-    #[pyo3(signature = (path, load_kwargs=None))]
+    #[pyo3(signature = (path, metadata, load_kwargs=None))]
     pub fn load(
         mut self_: PyRefMut<'_, Self>,
         py: Python,
         path: PathBuf,
+        metadata: DataInterfaceSaveMetadata,
         load_kwargs: Option<DataLoadKwargs>,
     ) -> PyResult<()> {
-        let load_path = path.join(SaveName::Data).with_extension(Suffix::Pt);
+        let load_path = path.join(metadata.data_uri);
         let torch = PyModule::import(py, "torch")?;
         let load_kwargs = load_kwargs.unwrap_or_default();
 
