e7af4f4b7171ff036393e595d46e90c15abebb64
Thorrester <sjforrester32@gmail.com>
2025-03-24 13:59:18 -0400

    fixing routes for completion

--- crates/opsml_storage/src/storage/local/client.rs
+++ crates/opsml_storage/src/storage/local/client.rs
@@ -5,7 +5,7 @@ use crate::storage::filesystem::FileSystem;
 use async_trait::async_trait;
 use opsml_error::error::StorageError;
 use opsml_settings::config::OpsmlStorageSettings;
-use opsml_types::contracts::MultipartCompleteParts;
+use opsml_types::contracts::CompleteMultipartUpload;
 use opsml_types::{contracts::FileInfo, StorageType};
 use std::fs;
 use std::path::{Path, PathBuf};
@@ -495,10 +495,7 @@ impl FileSystem for LocalFSStorageClient {
 
     async fn complete_multipart_upload(
         &self,
-        _upload_id: &str,
-        _rpath: &str,
-        _parts: MultipartCompleteParts,
-        _cancel: bool,
+        _request: CompleteMultipartUpload,
     ) -> Result<(), StorageError> {
         Ok(())
     }
