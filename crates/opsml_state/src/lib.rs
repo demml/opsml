f9648ac06d5e163d31adba333a2e4d21b20a9666
Thorrester <sjforrester32@gmail.com>
2025-03-24 11:23:50 -0400

    fix state

--- crates/opsml_state/src/lib.rs
+++ crates/opsml_state/src/lib.rs
@@ -1,16 +1,14 @@
-use futures::FutureExt;
+use futures::future::FutureExt;
 use opsml_client::base::{build_api_client, OpsmlApiClient};
 use opsml_error::error::StateError;
 use opsml_settings::OpsmlConfig;
-use opsml_storage::FileSystemStorage;
 use std::sync::Arc;
 use std::sync::OnceLock;
-use tokio::sync::Mutex;
+use tracing::error;
 
 pub struct OpsmlState {
-    api_client: Arc<OpsmlApiClient>,
-    storage: Arc<FileSystemStorage>,
-    mode: OpsmlMode,
+    pub config: OpsmlConfig,
+    pub api_client: Arc<OpsmlApiClient>,
 }
 
 impl OpsmlState {
@@ -30,34 +28,15 @@ impl OpsmlState {
 
         let api_client = Arc::new(api_client);
 
-        // Initialize storage
-        let mut settings = config.storage_settings().map_err(|e| {
-            error!("Failed to get storage settings: {}", e);
-            StateError::Error(format!("Failed to get storage settings with error: {}", e))
-        })?;
-
-        let storage = FileSystemStorage::new(&mut settings, &api_client, &config.mode)
-            .await
-            .expect("Failed to create file system storage");
-        let storage = Arc::new(Mutex::new(storage));
-
-        Self {
-            api_client,
-            storage,
-            mode: config.mode,
-        }
+        Ok(Self { config, api_client })
     }
 
     pub fn api_client(&self) -> &Arc<OpsmlApiClient> {
         &self.api_client
     }
 
-    pub fn storage(&self) -> &Arc<Mutex<FileSystemStorage>> {
-        &self.storage
-    }
-
-    pub fn mode(&self) -> &OpsmlMode {
-        &self.mode
+    pub fn config(&self) -> &OpsmlConfig {
+        &self.config
     }
 }
 
