c7412f416fb84a804056374424934822cb6b564a
Thorrester <sjforrester32@gmail.com>
2025-03-26 11:35:47 -0400

    initial setup

---
+++ crates/opsml_cli/src/cli/arg.rs
@@ -0,0 +1,41 @@
+use clap::Args;
+use opsml_types::RegistryType;
+
+#[derive(Args)]
+pub struct ListCards {
+    /// Name of the registry (data, model, experiment, prompt etc)
+    #[arg(long = "registry")]
+    pub registry: String,
+
+    /// repository name
+    #[arg(long = "repository")]
+    pub repository: Option<String>,
+
+    /// Name given to a card
+    #[arg(long = "name")]
+    pub name: Option<String>,
+
+    /// Card version
+    #[arg(long = "version")]
+    pub version: Option<String>,
+
+    /// Card uid
+    #[arg(long = "uid")]
+    pub uid: Option<String>,
+
+    /// Card limit
+    #[arg(long = "limit")]
+    pub limit: Option<i32>,
+
+    /// Tag name
+    #[arg(long = "tags", use_value_delimiter = true, value_delimiter = ',')]
+    pub tags: Option<Vec<String>>,
+
+    /// max date
+    #[arg(long = "max_date")]
+    pub max_date: Option<String>,
+
+    /// ignore release candidate
+    #[arg(long = "sort_by_timestamp", default_value = "true")]
+    pub sort_by_timestamp: bool,
+}
