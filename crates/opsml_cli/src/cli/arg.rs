82a2d2fe578d8b7254d4bcb522a19aebbfffa536
Thorrester <sjforrester32@gmail.com>
2025-03-26 11:40:22 -0400

    Update arg.rs

--- crates/opsml_cli/src/cli/arg.rs
+++ crates/opsml_cli/src/cli/arg.rs
@@ -1,5 +1,4 @@
 use clap::Args;
-use opsml_types::RegistryType;
 
 #[derive(Args)]
 pub struct ListCards {
@@ -38,4 +37,38 @@ pub struct ListCards {
     /// ignore release candidate
     #[arg(long = "sort_by_timestamp", default_value = "true")]
     pub sort_by_timestamp: bool,
+}
+
+#[derive(Args)]
+pub struct DownloadArtifacts {
+    /// Name of the registry (data, model, experiment, prompt etc)
+    #[arg(long = "registry")]
+    pub registry: String,
+
+    /// Card repository
+    #[arg(long = "repository")]
+    pub repository: Option<String>,
+
+    /// Name given to card
+    #[arg(long = "name")]
+    pub name: Option<String>,
+
+    /// Card version
+    #[arg(long = "version")]
+    pub version: Option<String>,
+
+    /// Card uid
+    #[arg(long = "uid")]
+    pub uid: Option<String>,
+
+    /// Write directory
+    #[arg(long = "write-dir", default_value = "artifacts")]
+    pub write_dir: String,
+}
+
+#[derive(Args)]
+pub struct LaunchServer {
+    /// Default port to use with the opsml server
+    #[arg(long = "port", default_value = "8888")]
+    pub port: i32,
 }
