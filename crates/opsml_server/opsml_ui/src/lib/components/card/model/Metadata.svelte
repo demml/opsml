9571db3be44dd35a0a3824cfeb615c7f559e66a4
Thorrester <sjforrester32@gmail.com>
2025-03-31 16:01:57 -0400

    update types

--- crates/opsml_server/opsml_ui/src/lib/components/card/model/Metadata.svelte
+++ crates/opsml_server/opsml_ui/src/lib/components/card/model/Metadata.svelte
@@ -102,7 +102,7 @@ modelcard.load()
     <div class="inline-flex items-center overflow-hidden rounded-lg border-2 border-primary-700 w-fit">
       <div class="border-r border-primary-70 px-2 text-primary-950 bg-primary-100 italic">OpsML Version</div> 
       <div class="flex px-1.5 bg-surface-50 border-surface-300 hover:bg-gradient-to-b from-surface-50 to-surface-100 text-primary-950">
-        {metadata.metadata.interface_metadata.opsml_version}
+        {metadata.opsml_version}
       </div>
     </div>
 
