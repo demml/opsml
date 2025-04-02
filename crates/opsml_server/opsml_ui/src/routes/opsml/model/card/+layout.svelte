5f9f18f3f5e2c870da9416a93f12fb364e8bd927
Thorrester <sjforrester32@gmail.com>
2025-04-02 16:21:55 -0400

    adding version folder to other cards

--- crates/opsml_server/opsml_ui/src/routes/opsml/model/card/+layout.svelte
+++ crates/opsml_server/opsml_ui/src/routes/opsml/model/card/+layout.svelte
@@ -20,9 +20,6 @@
     goto(`/opsml/${registry}/card/${activeTab}?repository=${repository}&name=${name}&version=${version}`);
   };
 
-  function navigateToVersionPage() {
-    goto(`/opsml/${registry}?space=${repository}&name=${name}`);
-  }
 
   let { data, children }: LayoutProps = $props();
 
@@ -68,7 +65,7 @@
           <Activity color="#8059b6"/>
           <span>Monitoring</span>
         </button>
-        <button class="flex items-center gap-x-2 border-b-3 {activeTab === 'versions' ? 'border-secondary-500' : 'border-transparent'} hover:border-secondary-500 hover:border-b-3" onclick={() => navigateToVersionPage()}>
+        <button class="flex items-center gap-x-2 border-b-3 {activeTab === 'versions' ? 'border-secondary-500' : 'border-transparent'} hover:border-secondary-500 hover:border-b-3" onclick={() => navigateTab('versions')}>
           <Tag color="#8059b6" fill="#8059b6"/>
           <span>Versions</span>
         </button>
