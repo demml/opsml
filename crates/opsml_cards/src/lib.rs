d070896de71b0f2c2e5569d1e36958a3819a0a98
Thorrester <sjforrester32@gmail.com>
2025-04-09 17:53:42 -0400

    Move base args
    - move base arg function to cards
    - replace runtime env checking with toml config

--- crates/opsml_cards/src/lib.rs
+++ crates/opsml_cards/src/lib.rs
@@ -3,9 +3,11 @@ pub mod deck;
 pub mod experiment;
 pub mod model;
 pub mod prompt;
+pub mod utils;
 
 pub use data::*;
 pub use deck::*;
 pub use experiment::*;
 pub use model::*;
 pub use prompt::*;
+pub use utils::*;
