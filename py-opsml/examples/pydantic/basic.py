3f37e046e723fc63e5cfb4e9fe1cc5564051a633
Thorrester <sjforrester32@gmail.com>
2025-04-11 22:57:24 -0400

    update docs

--- py-opsml/examples/pydantic/basic.py
+++ py-opsml/examples/pydantic/basic.py
@@ -1,6 +1,5 @@
 from pydantic_ai import Agent
 from opsml import PromptCard, Prompt, CardRegistry, RegistryType
-from pydantic_ai.models.test import TestModel
 
 # Creating the card here for demonstration purposes
 # In practice, you would create the card, register it and then load it from the registry
@@ -9,20 +8,19 @@ card = PromptCard(
     space="opsml",
     name="my_prompt",
     prompt=Prompt(
-        model="google-gla:gemini-1.5-flash",
+        model="openai:gpt-4o",
         system_prompt="Be concise, reply with one sentence.",
     ),
 )
 
 agent = Agent(
     card.prompt.model,
-    system_prompt=card.prompt.system_prompt[0],
+    system_prompt=card.prompt.system_prompt[0].unwrap(),
 )
 
-with agent.override(model=TestModel()):
-    result = agent.run_sync('Where does "hello world" come from?')
-    print(result.data)
+result = agent.run_sync('Where does "hello world" come from?')
+print(result.data)
 
 
 registry = CardRegistry(RegistryType.Prompt)
-registry.register(card)
+registry.register_card(card)
