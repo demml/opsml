b63b59eb0d326437498d683b76ac1987430ef107
Thorrester <sjforrester32@gmail.com>
2025-04-23 20:43:26 -0400

    typing

--- py-opsml/python/opsml/__init__.pyi
+++ py-opsml/python/opsml/__init__.pyi
@@ -47,3 +47,4 @@ from .model import TorchModel as TorchModel
 from .model import XGBoostModel as XGBoostModel
 from .potato_head import Message as Message
 from .potato_head import Prompt as Prompt
+from .potato_head import ModelSettings as ModelSettings
