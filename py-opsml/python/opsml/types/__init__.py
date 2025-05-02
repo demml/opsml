21f6f92e80be8dc7ea91e55556f34116d728a01d
Thorrester <sjforrester32@gmail.com>
2025-05-02 10:02:38 -0400

    changing import structure for better organization

---
+++ py-opsml/python/opsml/types/__init__.py
@@ -0,0 +1,17 @@
+# type: ignore
+
+from .. import core
+
+CommonKwargs = core.CommonKwargs
+SaveName = core.SaveName
+SaverPath = core.SaverPath
+Suffix = core.Suffix
+VersionType = core.VersionType
+
+
+__all__ = [
+    "CommonKwargs",
+    "SaveName",
+    "Suffix",
+    "VersionType",
+]
---
+++ py-opsml/src/types.rs
@@ -0,0 +1,13 @@
+use opsml_semver::VersionType;
+use opsml_types::{CommonKwargs, SaveName, Suffix};
+use pyo3::prelude::*;
+
+#[pymodule]
+pub fn types(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<CommonKwargs>()?;
+    m.add_class::<SaveName>()?;
+    m.add_class::<Suffix>()?;
+    m.add_class::<VersionType>()?;
+
+    Ok(())
+}
