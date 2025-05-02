22675e41926d38714a94be68b2b217ac265e35af
Thorrester <sjforrester32@gmail.com>
2025-05-02 18:24:22 -0400

    missing imports

--- crates/opsml_interfaces/src/lib.rs
+++ crates/opsml_interfaces/src/lib.rs
@@ -3,6 +3,6 @@ pub mod model;
 pub mod types;
 
 pub use data::base::{DataLoadKwargs, DataSaveKwargs};
-pub use model::base::{ModelLoadKwargs, ModelSaveKwargs};
+pub use model::base::{ExtraMetadata, ModelLoadKwargs, ModelSaveKwargs};
 pub use model::*;
 pub use types::*;
