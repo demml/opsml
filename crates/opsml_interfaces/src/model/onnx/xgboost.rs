b193ae4f8a7fb7b6d8b91c77ade06b2a8f1d507f
Thorrester <sjforrester32@gmail.com>
2025-05-05 13:32:24 -0400

    Update xgboost.rs

--- crates/opsml_interfaces/src/model/onnx/xgboost.rs
+++ crates/opsml_interfaces/src/model/onnx/xgboost.rs
@@ -1,6 +1,6 @@
+use crate::error::OnnxError;
 use crate::model::base::utils::OnnxExtension;
 use crate::model::onnx::OnnxSession;
-use opsml_error::OpsmlError;
 use opsml_types::ModelType;
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
@@ -23,7 +23,7 @@ impl XGBoostOnnxModelConverter {
         &self,
         onnx_model: &Bound<'_, PyAny>,
         feature_names: Vec<String>,
-    ) -> PyResult<OnnxSession> {
+    ) -> Result<OnnxSession, OnnxError> {
         let py = onnx_model.py();
 
         let onnx_version = py
@@ -31,19 +31,7 @@ impl XGBoostOnnxModelConverter {
             .getattr("__version__")?
             .extract::<String>()?;
 
-        let onnx_bytes = onnx_model
-            .call_method("SerializeToString", (), None)
-            .map_err(|e| OpsmlError::new_err(format!("Failed to serialize ONNX model: {}", e)))?;
-
-        OnnxSession::new(
-            py,
-            onnx_version,
-            onnx_bytes.extract::<Vec<u8>>()?,
-            "onnx".to_string(),
-            Some(feature_names),
-            Some(onnx_model),
-        )
-        .map_err(|e| OpsmlError::new_err(format!("Failed to create ONNX session: {}", e)))
+        OnnxSession::from_onnx_session(onnx_version, onnx_model, Some(feature_names))
     }
 
     pub fn convert_model<'py, T>(
@@ -53,13 +41,11 @@ impl XGBoostOnnxModelConverter {
         model_type: &ModelType,
         sample_data: &T,
         kwargs: Option<&Bound<'py, PyDict>>,
-    ) -> PyResult<OnnxSession>
+    ) -> Result<OnnxSession, OnnxError>
     where
         T: OnnxExtension,
     {
-        let onnxmltools = py
-            .import("onnxmltools")
-            .map_err(|e| OpsmlError::new_err(format!("Failed to import onnxmltools: {}", e)))?;
+        let onnxmltools = py.import("onnxmltools").map_err(OnnxError::ImportError)?;
 
         let type_helper = py
             .import("skl2onnx")
@@ -83,7 +69,7 @@ impl XGBoostOnnxModelConverter {
 
         let onnx_model = onnxmltools
             .call_method("convert_xgboost", (model,), Some(&kwargs))
-            .map_err(|e| OpsmlError::new_err(format!("Failed to convert model to ONNX: {}", e)))?;
+            .map_err(OnnxError::PyOnnxConversionError)?;
 
         debug!("Step 3: Extracting ONNX schema");
         let onnx_session = self.get_onnx_session(&onnx_model, sample_data.get_feature_names(py)?);
