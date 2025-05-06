b52b83b4d24cb8cfbf9bc70e85826e6edfc23799
Thorrester <sjforrester32@gmail.com>
2025-05-06 16:09:33 -0400

    updating opsml_crypt

---
+++ crates/opsml_crypt/src/error.rs
@@ -0,0 +1,33 @@
+use opsml_utils::error::UtilError;
+use thiserror::Error;
+use tracing::error;
+
+#[derive(Error, Debug)]
+pub enum CryptError {
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+
+    #[error("Failed to encrypt file: {0}")]
+    EncryptError(String),
+
+    #[error("Failed to decrypt file: {0}")]
+    DecryptError(String),
+
+    #[error(transparent)]
+    UtilError(#[from] UtilError),
+
+    #[error("Failed to derive key: {0}")]
+    DeriveKeyError(String),
+
+    #[error("Failed to generate salt")]
+    GenerateSaltError,
+
+    #[error("Failed to create AES-256-GCM key: {0}")]
+    AesCreateError(String),
+
+    #[error("Failed to encrypt key using AES-256-GCM")]
+    EncryptKeyError,
+
+    #[error("Failed to decrypt key using AES-256-GCM")]
+    DecryptKeyError,
+}
