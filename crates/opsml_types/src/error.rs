9f327e53d963bf755d5d49bfd7f6e4de7ecf7334
Thorrester <sjforrester32@gmail.com>
2025-05-06 18:27:53 -0400

    updating errors

---
+++ crates/opsml_types/src/error.rs
@@ -0,0 +1,29 @@
+use opsml_crypt::error::CryptError;
+use opsml_utils::error::UtilError;
+use pyo3::exceptions::PyRuntimeError;
+use pyo3::PyErr;
+use thiserror::Error;
+use tracing::error;
+
+#[derive(Error, Debug)]
+pub enum TypeError {
+    #[error(transparent)]
+    UtilError(#[from] UtilError),
+
+    #[error(transparent)]
+    CryptError(#[from] CryptError),
+
+    #[error("Invalid RegistryType")]
+    InvalidRegistryType,
+
+    #[error("Invalid type")]
+    InvalidType,
+}
+
+impl From<TypeError> for PyErr {
+    fn from(err: TypeError) -> PyErr {
+        let msg = err.to_string();
+        error!("{}", msg);
+        PyRuntimeError::new_err(msg)
+    }
+}
--- crates/opsml_types/src/lib.rs
+++ crates/opsml_types/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod api;
 pub mod cards;
 pub mod contracts;
+pub mod error;
 pub mod interfaces;
 pub mod types;
 
