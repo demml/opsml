91b83aa3fca07a634d9747f0f6fe6b9cc88b3f01
Thorrester <sjforrester32@gmail.com>
2025-05-07 09:24:02 -0400

    refactor StateError

---
+++ crates/opsml_storage/src/storage/error.rs
@@ -0,0 +1,9 @@
+use opsml_utils::error::UtilError;
+use thiserror::Error;
+use tracing::error;
+
+#[derive(Error, Debug)]
+pub enum StorageError {
+    #[error(transparent)]
+    UtilError(#[from] UtilError),
+}
