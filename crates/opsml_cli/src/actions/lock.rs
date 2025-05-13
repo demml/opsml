bf58a962b4639ba357c130db49668d0c1f0e43f1
Thorrester <sjforrester32@gmail.com>
2025-05-13 13:06:05 -0400

    drift registration option in toml definition

--- crates/opsml_cli/src/actions/lock.rs
+++ crates/opsml_cli/src/actions/lock.rs
@@ -2,11 +2,12 @@ use crate::actions::utils::register_card_deck;
 use crate::cli::arg::DownloadCard;
 use crate::download_deck;
 use crate::error::CliError;
+use opsml_cards::CardDeck;
 use opsml_colors::Colorize;
 use opsml_registry::error::RegistryError;
 use opsml_registry::{CardRegistries, CardRegistry};
 use opsml_toml::{
-    toml::{AppConfig, Card},
+    toml::{Card, DeckConfig},
     LockArtifact, LockFile, PyProjectToml,
 };
 use opsml_types::{
@@ -14,13 +15,15 @@ use opsml_types::{
     RegistryType,
 };
 use pyo3::prelude::*;
+use scouter_client::{DriftType, ProfileStatusRequest};
 use std::path::PathBuf;
+use std::str::FromStr;
 use tracing::{debug, error, instrument};
 
 /// Helper function to get cards from registry
 fn get_deck_from_registry(
     registry: &CardRegistry,
-    config: &AppConfig,
+    config: &DeckConfig,
 ) -> Result<Option<CardRecord>, CliError> {
     let cards = registry.list_cards(
         None,
@@ -33,11 +36,6 @@ fn get_deck_from_registry(
         1,
     )?;
     Ok(cards.cards.first().cloned())
-}
-
-/// Helper function to validate app configuration
-fn validate_app_cards(config: &AppConfig) -> Result<&Vec<Card>, CliError> {
-    config.cards.as_ref().ok_or(CliError::MissingDeckCards)
 }
 
 /// Helper function to get latest card from appropriate registry
@@ -82,39 +80,80 @@ fn get_latest_card(registries: &CardRegistries, card: &Card) -> Result<CardRecor
     })
 }
 
-/// Process deck cards and check for version updates
+/// Helper for any postprocessing needed after card deck registration
+/// As an example, modelcards can activate drift profile via a drift_config in the toml
+#[instrument(skip_all)]
+fn postprocess_card_deck(
+    toml_cards: &[Card],
+    deck: &CardDeck,
+    registry: &CardRegistry,
+) -> Result<(), CliError> {
+    toml_cards
+        .iter()
+        .filter_map(|card| card.drift.as_ref().map(|drift| (card, drift)))
+        .try_for_each(|(card, drift_config)| -> Result<(), CliError> {
+            let current_card = deck.get_card(&card.alias)?;
+
+            drift_config.drift_type.iter().try_for_each(|drift_type| {
+                let drift_type = DriftType::from_str(drift_type)?;
+
+                let request = ProfileStatusRequest {
+                    space: current_card.space.clone(),
+                    name: current_card.name.clone(),
+                    version: current_card.version.clone(),
+                    active: drift_config.active,
+                    drift_type: Some(drift_type),
+                    deactivate_others: drift_config.deactivate_others,
+                };
+
+                registry.registry.update_drift_profile_status(&request)?;
+                debug!("Drift profile status updated for card: {:?}", request);
+                Ok(())
+            })
+        })?;
+
+    Ok(())
+}
+
+/// Process card deck and check for version updates
+/// The goal of this function is to:
+/// 1. For each card defined in the toml file, find the latest card in the registry
+/// 2. Check if the already registered deck has the same card
+/// 3. If an existing card of same space and name is found, check if the uid is the same
+/// 4. If the uid is different, return true (needs refresh)
+/// 5. If the uid is the same, return false (no refresh needed)
+/// 6. If no existing card is found, return true (needs refresh)
 ///
 /// # Arguments
-/// * `card_entries` - Vec<CardEntry> - List of cards associated with the most recent registered deck
-/// * `app_cards` - &[DeckCard] - List of cards found in toml
+/// * `deck_cards` - &[CardEntry] - List of cards associated with the most recent registered deck
+/// * `toml_cards` - &[DeckCard] - List of cards found in toml
 /// * `registries` - &RustRegistries - Rust variant of Opsml registries
 ///
 /// # Returns
 /// * `Result<bool, CliError>` - True if a version refresh is needed, false otherwise
 #[instrument(skip_all)]
 fn process_deck_cards(
-    card_entries: Vec<CardEntry>,
-    app_cards: &[Card],
+    deck_cards: &[CardEntry],
+    toml_cards: &[Card],
     registries: &CardRegistries,
 ) -> Result<bool, CliError> {
     debug!("Processing deck cards");
-    for app_card in app_cards {
+    for card in toml_cards {
         // Find the latest card given the constraints provided in toml file
-        let latest_card = get_latest_card(registries, app_card)?;
+        let latest_card = get_latest_card(registries, card)?;
 
-        // Find the entry in the card entries
+        // Find the card in the deck
         // If the entry is not found - return true (need to increment version)
         // If the entry is found - check if the uid is different from latest card - if different - return true
         // Otherwise - return false
         // Find matching entry in existing deck
-        let existing_entry = card_entries
-            .iter()
-            .find(|entry| entry.alias == app_card.alias);
+        let current_card = deck_cards.iter().find(|card| card.alias == card.alias);
 
-        match existing_entry {
-            Some(entry) => {
+        // Check
+        match current_card {
+            Some(card) => {
                 // Check if existing entry UID matches latest card UID
-                if entry.uid != latest_card.uid() {
+                if card.uid != latest_card.uid() {
                     return Ok(true);
                 }
             }
@@ -128,37 +167,39 @@ fn process_deck_cards(
 
 /// create lock entry for a deck
 /// # Arguments
-/// * `config` - AppConfig
-fn lock_deck(config: AppConfig) -> Result<LockArtifact, CliError> {
+/// * `config` - DeckConfig
+fn lock_deck(config: DeckConfig) -> Result<LockArtifact, CliError> {
     debug!("Locking deck with config: {:?}", config);
 
-    // Validate app configuration
-    let app_cards = validate_app_cards(&config)?;
+    // Get app cards from deck config
+    let toml_cards = config.cards.as_ref().ok_or(CliError::MissingDeckCards)?;
+
     let registries = CardRegistries::new()?;
 
-    // Initialize registry and get deck
+    // Initialize registry and get deck from registry
     let deck = get_deck_from_registry(&registries.deck, &config)?;
 
     // Handle deck creation if it doesn't exist
     if deck.is_none() {
         let card = register_card_deck(&config, &registries.deck)?;
+
         return Ok(LockArtifact {
             space: card.space.clone(),
             name: card.name.clone(),
             version: card.version.clone(),
             uid: card.uid.clone(),
             registry_type: RegistryType::Deck,
-            write_dir: config.write_dir.unwrap_or("opsml_app".to_string()),
+            write_dir: config.write_dir.unwrap_or("opsml_deck".to_string()),
         });
     }
     //
-    //// Process existing deck
+    //// Process existing deck (need to compare existing deck cards to those degfined in toml)
     let deck = deck.unwrap();
     //
     //// Get card UIDs from deck
-    let card_entries = deck.cards().ok_or(CliError::MissingCardEntriesError)?;
-    let needs_refresh = process_deck_cards(card_entries, app_cards, &registries)?;
-    //
+    let deck_cards = deck.cards().ok_or(CliError::MissingCardEntriesError)?;
+    let needs_refresh = process_deck_cards(&deck_cards, toml_cards, &registries)?;
+
     let lock_entry = match needs_refresh {
         true => {
             // If refresh is needed, register the deck again
@@ -189,7 +230,7 @@ fn lock_deck(config: AppConfig) -> Result<LockArtifact, CliError> {
 }
 
 /// Create the the lock file for the app
-fn lock_app(app: AppConfig) -> Result<LockArtifact, CliError> {
+fn lock_app(app: DeckConfig) -> Result<LockArtifact, CliError> {
     // Create a lock file for the app
     // Only support's  deck currently
     match app.registry_type {
@@ -256,10 +297,8 @@ pub fn lock_project(path: Option<PathBuf>, toml_name: Option<&str>) -> Result<()
     debug!("Locking project with path: {:?}", path);
 
     let pyproject = PyProjectToml::load(path.as_deref(), toml_name)?;
-
     let tools = pyproject.get_tools().ok_or(CliError::MissingToolsError)?;
-
-    let apps = tools.get_apps().ok_or(CliError::MissingAppError)?;
+    let apps = tools.get_decks().ok_or(CliError::MissingAppError)?;
 
     // Create a lock file
     let lock_file = LockFile {
