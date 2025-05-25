8484cbac5a640085b3af1809320f7d9705acc859
Thorrester <sjforrester32@gmail.com>
2025-05-25 12:17:51 -0400

    login page

--- crates/opsml_server/opsml_ui/src/routes/opsml/user/profile/+page.svelte
+++ crates/opsml_server/opsml_ui/src/routes/opsml/user/profile/+page.svelte
@@ -2,7 +2,7 @@
   import type { UserResponse } from "$lib/components/user/types";
   import type { PageProps } from './$types';
   import Pill from "$lib/components/utils/Pill.svelte";
-  import { Cog, ShieldEllipsis } from 'lucide-svelte';
+  import { Cog, ShieldEllipsis, Star } from 'lucide-svelte';
   import { userStore } from "$lib/components/user/user.svelte";
   
 
@@ -13,41 +13,57 @@
 
 <div class="col-span-12 md:col-start-5 md:col-span-4 flex items-center justify-center px-4">
 
-  <div class="rounded-2xl bg-surface-50 border-black border-2 shadow p-4 md:w-96 md:px-5">
+  <div class="rounded-2xl bg-surface-50 border-black border-2 shadow p-4 w-lg md:w-xl lg:w-2xl md:px-5">
 
-    <div class="flex flex-row items-center pt-2">
+    <div class="flex flex-row items-center py-2">
       <Cog color="#8059b6"/>
       <header class="pl-2 text-primary-950 text-2xl font-bold">User Profile</header>
     </div>
 
-    <div class="flex flex-col space-y-1 text-base mb-1">
+    <div class="flex flex-col gap-2 text-base mb-1">
       <Pill key="Username" value={userInfo.username} textSize="text-base"/>
       <Pill key="Email" value={userInfo.email} textSize="text-base"/>
     </div>
 
-    <div class="flex flex-row items-center mb-1 border-b-2 border-black">
+    <div class="flex flex-row items-center pt-2 mb-2 border-b-2 border-black">
       <ShieldEllipsis color="#8059b6"/>
       <header class="pl-2 text-primary-900 text-lg font-bold">Permissions</header>
     </div>
 
-    <div class="flex flex-wrap gap-1">
+    <div class="flex flex-wrap gap-2">
       {#each userStore.getPermissions() as perm}
         <Pill key={perm[0]} value={perm[1]} textSize="text-base"/>
       {/each}
     </div>
 
-    <div class="flex flex-row items-center mb-1 border-b-2 border-black">
+    <div class="flex flex-row items-center pt-2 mb-2 border-b-2 border-black">
       <ShieldEllipsis color="#8059b6"/>
       <header class="pl-2 text-primary-900 text-lg font-bold">Group Permissions</header>
     </div>
 
-    <div class="flex flex-wrap gap-1">
+    <div class="flex flex-wrap gap-2">
       {#each userStore.group_permissions as perm}
-        <div class="inline-flex items-center overflow-hidden rounded-lg bg-primary-100 border border-primary-800 text-sm w-fit px-2 text-primary-900">
+        <div class="inline-flex items-center overflow-hidden rounded-lg bg-primary-100 border-2 border-primary-800 text-base w-fit px-2 text-primary-900">
           {perm}
         </div>
       {/each}
     </div>
+
+
+    <div class="flex flex-row items-center pt-2 mb-2 border-b-2 border-black">
+      <Star fill="#8059b6"/>
+      <header class="pl-2 text-primary-900 text-lg font-bold">Spaces</header>
+    </div>
+    
+    {#if userStore.favorite_spaces.length >= 0}
+      <div class="flex flex-wrap gap-2">
+        {#each userStore.favorite_spaces as space}
+          <div class="inline-flex items-center overflow-hidden rounded-lg bg-primary-100 border-2 border-primary-800 text-base w-fit px-2 text-primary-900">
+            {space}
+          </div>
+        {/each}
+      </div>
+    {/if}
 
   </div>
 </div>
