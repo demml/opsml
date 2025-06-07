04ebd9e1076c13e239c1e1eb83daa034352382d0
Thorrester <sjforrester32@gmail.com>
2025-06-07 14:05:18 -0400

    tests

--- crates/opsml_server/tests/api/login.rs
+++ crates/opsml_server/tests/api/login.rs
@@ -24,7 +24,7 @@ async fn test_opsml_server_login() {
 }
 
 #[tokio::test]
-async fn test_opsml_server_sso_programmatic_login() {
+async fn test_opsml_server_keycloak_sso_programmatic_login() {
     let helper = TestHelper::new(Some("keycloak".to_string())).await;
 
     let request = Request::builder()
@@ -36,13 +36,31 @@ async fn test_opsml_server_sso_programmatic_login() {
     assert_eq!(response.status(), StatusCode::OK);
 
     // Check if the SSO login was successful
-    assert!(helper.verify_sso_called());
+    assert!(helper.verify_keycloak_sso_called());
 
     helper.cleanup();
 }
 
 #[tokio::test]
-async fn test_opsml_server_sso_ui_login() {
+async fn test_opsml_server_okta_sso_programmatic_login() {
+    let helper = TestHelper::new(Some("okta".to_string())).await;
+
+    let request = Request::builder()
+        .uri("/opsml/api/healthcheck")
+        .body(Body::empty())
+        .unwrap();
+
+    let response = helper.send_oneshot(request).await;
+    assert_eq!(response.status(), StatusCode::OK);
+
+    // Check if the SSO login was successful
+    assert!(helper.verify_okta_sso_called());
+
+    helper.cleanup();
+}
+
+#[tokio::test]
+async fn test_opsml_server_keycloak_sso_ui_login() {
     let helper = TestHelper::new(Some("keycloak".to_string())).await;
     let mock_url = helper.sso_server.as_ref().unwrap().url.clone();
 
@@ -70,13 +88,74 @@ async fn test_opsml_server_sso_ui_login() {
 
     // build the expected URL
     let expected_url = format!(
-        "{}/realms/opsml/protocol/openid-connect/auth?client_id=opsml-client&response_type=code&scope=openid profile email&redirect_uri=http://localhost:8080/callback&state=test_state",
+        "{}/realms/opsml/protocol/openid-connect/auth?client_id=opsml-client&response_type=code&scope=openid email profile&redirect_uri=http://localhost:8080/callback&state=test_state",
         mock_url
     );
 
     assert_eq!(sso_auth_url.url, expected_url);
 
-    assert!(helper.verify_sso_called());
+    assert!(helper.verify_keycloak_sso_called());
+
+    // (2) Test SSO login callback to verify authentication
+    let code = "mock_code";
+    let parms = SsoCallbackParams {
+        code: code.to_string(),
+    };
+
+    let query_string = serde_qs::to_string(&parms).unwrap();
+    let request = Request::builder()
+        .uri(format!("/opsml/api/auth/sso/callback?{}", query_string))
+        .method("GET")
+        .body(Body::empty())
+        .unwrap();
+
+    let response = helper.send_oneshot(request).await;
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body_bytes = response.into_body().collect().await.unwrap().to_bytes();
+    let login: LoginResponse = serde_json::from_slice(&body_bytes).unwrap();
+
+    assert!(login.authenticated);
+
+    helper.cleanup();
+}
+
+#[tokio::test]
+async fn test_opsml_server_okta_sso_ui_login() {
+    let helper = TestHelper::new(Some("okta".to_string())).await;
+    let mock_url = helper.sso_server.as_ref().unwrap().url.clone();
+
+    let params = SsoAuthUrlParams {
+        state: "test_state".to_string(),
+    };
+
+    let query_string = serde_qs::to_string(&params).unwrap();
+
+    // (1) Test SSO login URL generation
+    let request = Request::builder()
+        .uri(format!(
+            "/opsml/api/auth/sso/authorization?{}",
+            query_string
+        ))
+        .method("GET")
+        .body(Body::empty())
+        .unwrap();
+
+    let response = helper.send_oneshot(request).await;
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body_bytes = response.into_body().collect().await.unwrap().to_bytes();
+    let sso_auth_url: SsoAuthUrl = serde_json::from_slice(&body_bytes).unwrap();
+
+    // build the expected URL
+    let expected_url = format!(
+        "{}/oauth2/v1/authorize?client_id=opsml-client&response_type=code&scope=openid email profile&redirect_uri=http://localhost:8080/callback&state=test_state",
+        mock_url
+    );
+
+    assert_eq!(sso_auth_url.url, expected_url);
+
+    assert!(helper.verify_okta_sso_called());
 
     // (2) Test SSO login callback to verify authentication
     let code = "mock_code";
