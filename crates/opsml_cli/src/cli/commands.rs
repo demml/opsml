2690e936b4ea486910a987f2937f9682c3ed7381
Thorrester <sjforrester32@gmail.com>
2025-06-11 22:42:17 -0400

    adding stop cli command

--- crates/opsml_cli/src/cli/commands.rs
+++ crates/opsml_cli/src/cli/commands.rs
@@ -121,10 +121,10 @@ pub enum Commands {
         command: ScouterCommands,
     },
 
-    /// Start commands for Opsml. Current use case is for start a local UI
-    Start {
+    /// Start commands for Opsml
+    Ui {
         #[command(subcommand)]
-        command: StartCommands,
+        command: UiCommands,
     },
 }
 
@@ -174,12 +174,18 @@ pub enum ScouterCommands {
 }
 
 #[derive(Subcommand)]
-pub enum StartCommands {
+pub enum UiCommands {
     /// Start a local OpsML UI
     ///
     /// # Example
     /// opsml start ui
-    Ui(UiArgs),
+    Start(UiArgs),
+
+    /// Stop the currently running OpsML UI
+    ///
+    /// # Example
+    /// opsml stop ui
+    Stop,
 }
 
 pub const LOGO_TEXT: &str = "
