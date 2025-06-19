f968b2c1ab4ac8744c4b7429a9577ebad583cce7
Thorrester <sjforrester32@gmail.com>
2025-06-19 11:53:24 -0400

    add deck page

---
+++ crates/opsml_server/opsml_ui/src/lib/components/card/deck/CardButton.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+
+  import { goto } from "$app/navigation";
+  import {CircuitBoard, KeyIcon, Tag, FlaskConical, Table, BrainCircuit, NotebookText } from 'lucide-svelte';
+
+  let {
+    name,
+    space,
+    version,
+    alias,
+    registry,
+    iconColor,
+    badgeColor,
+  } = $props<{
+    name: string;
+    space: string;
+    version: string;
+    alias: string;
+    registry: string;
+    iconColor: string;
+    badgeColor: string;
+  }>();
+
+  // function to navigate to the card page
+  function navigateToCardPage() {
+
+    let registry_name = registry.toLowerCase();
+
+    // if registry_name == "prompt" change to genai
+    // this is only temporary until we start having both agent and prompt registries
+    if (registry_name === "prompt") {
+      registry_name = "genai";
+    }
+
+    // navigate to the card page
+    goto(`/opsml/${registry_name}/card/home?space=${space}&name=${name}&version=${version}`);
+  }
+
+
+</script>
+
+<button class="text-black rounded-base shadow border-2 border-black bg-surface-300 w-full max-w-[30em] h-[5em] overflow-auto whitespace-nowrap hover:translate-x-[4px] hover:translate-y-[4px] hover:shadow-none" onclick={navigateToCardPage}>
+  <div class="flex items-center justify-between">
+    <div class="flex items-center justify-start gap-2 mb-1">
+      <div class="ml-2">
+        <CircuitBoard color={iconColor} />
+      </div>
+      <div><h4 class="truncate font-bold text-smd">{space}/{name}</h4></div>
+    </div>
+    <div class="mr-2">
+
+      {#if registry === "model"}
+        <BrainCircuit color={badgeColor} />
+      {:else if registry === "data"}
+        <Table color={badgeColor} />
+      {:else if registry === "prompt"}
+        <NotebookText color={badgeColor} />
+      {:else if registry === "experiment"}
+        <FlaskConical color={badgeColor} />
+      {/if}
+  
+    </div>
+  </div>
+  <div class="flex items-center justify-start gap-2 whitespace-nowrap text-xs">
+    <div class="flex items-center gap-1 ">
+      <div class="ml-2">
+        <KeyIcon color={iconColor} />
+      </div>
+      <div>
+        <div class="text-black">Alias: {alias}</div>
+      </div>
+    </div>
+    <div class="flex items-center gap-1 ">
+      <div class="ml-2">
+        <Tag color={iconColor} />
+      </div>
+      <div class="text-black">Version: {version}</div>
+    </div>
+  </div>
+</button>
+
---
+++ crates/opsml_server/opsml_ui/src/lib/components/card/deck/CardList.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+
+  import  CardButton from "./CardButton.svelte";
+  import { type CardList } from "../card_interfaces/carddeck";
+
+  let badgeColor = "#40328b";
+  let iconColor = "#40328b";
+
+
+  let {
+    cardList,
+  } = $props<{
+    cardList: CardList;
+  }>();
+
+</script>
+  
+  {#each cardList.cards as card}
+    <CardButton
+      iconColor={iconColor} 
+      badgeColor={badgeColor}
+      name={card.name}
+      space={card.space}
+      version={card.version}
+      registry={card.registry_type.toLowerCase()}
+      alias={card.alias}
+    />
+  {/each}
+    
+ 
