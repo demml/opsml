976919b47bdc5e40415077df6b0800be1cbc9981
Thorrester <sjforrester32@gmail.com>
2025-06-19 10:14:30 -0400

    Adding deck page

--- crates/opsml_server/opsml_ui/src/lib/components/nav/Navbar.svelte
+++ crates/opsml_server/opsml_ui/src/lib/components/nav/Navbar.svelte
@@ -27,7 +27,7 @@
 
   let imageLoaded = $state(false);
 
-  const names = ["Spaces", "Models", "Data", "GenAI", "Experiments"];
+  const names = ["Spaces", "Models", "Data", "GenAI", "Experiments", "Decks"];
 
 
   onMount(() => {
