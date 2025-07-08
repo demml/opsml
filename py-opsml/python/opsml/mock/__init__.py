4549b260975f44068bec9d6258314e5fe9e67dbd
Thorrester <sjforrester32@gmail.com>
2025-07-08 09:25:33 -0400

    add mock config

--- py-opsml/python/opsml/mock/__init__.py
+++ py-opsml/python/opsml/mock/__init__.py
@@ -4,5 +4,6 @@ from .. import mock
 OpsmlTestServer = mock.OpsmlTestServer
 OpsmlServerContext = mock.OpsmlServerContext
 OpenAITestServer = mock.OpenAITestServer
+MockConfig = mock.MockConfig
 
-__all__ = ["OpsmlTestServer", "OpsmlServerContext", "OpenAITestServer"]
+__all__ = ["OpsmlTestServer", "OpsmlServerContext", "OpenAITestServer", "MockConfig"]
