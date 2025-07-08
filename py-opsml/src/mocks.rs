6882a6c3b7959e2e4dc7628147bacb53ebcdb954
Thorrester <sjforrester32@gmail.com>
2025-07-08 09:25:33 -0400

    add mock config

--- py-opsml/src/mocks.rs
+++ py-opsml/src/mocks.rs
@@ -1,10 +1,12 @@
 use opsml_mocks::{OpenAITestServer, OpsmlServerContext, OpsmlTestServer};
 use pyo3::prelude::*;
+use scouter_client::MockConfig;
 
 #[pymodule]
 pub fn mock(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<OpsmlTestServer>()?;
     m.add_class::<OpsmlServerContext>()?;
     m.add_class::<OpenAITestServer>()?;
+    m.add_class::<MockConfig>()?;
     Ok(())
 }
