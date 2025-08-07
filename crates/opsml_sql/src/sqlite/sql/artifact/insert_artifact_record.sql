bfb1ed094f4a334e4d3a8dc911b3de830ef1b9d1
Thorrester <sjforrester32@gmail.com>
2025-08-06 21:19:03 -0400

    add insert logic

---
+++ crates/opsml_sql/src/sqlite/sql/artifact/insert_artifact_record.sql
@@ -0,0 +1,29 @@
+INSERT INTO opsml_artifact_registry (
+    uid,
+    created_at,
+    app_env,
+    space,
+    name,
+    major,
+    minor,
+    patch,
+    pre_tag,
+    build_tag,
+    version,
+    filename,
+    data_type
+) VALUES (
+    ?, 
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,         
+    ?,
+    ?,
+    ?,
+    ?,
+    ?
+);
