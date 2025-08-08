d58369d7d43d3e22bda095d50297b4af16fd2de4
Thorrester <sjforrester32@gmail.com>
2025-08-08 11:09:37 -0400

    lock down model routes

--- crates/opsml_server/opsml_ui/src/routes/opsml/experiment/card/[space]/[name]/[version]/metrics/+page.ts
+++ crates/opsml_server/opsml_ui/src/routes/opsml/experiment/card/[space]/[name]/[version]/metrics/+page.ts
@@ -2,12 +2,13 @@ export const ssr = false;
 
 import {
   getCardMetricNames,
-  getCardParameters,
   getRecentExperiments,
 } from "$lib/components/card/experiment/util";
 import type { PageLoad } from "./$types";
+import { validateUserOrRedirect } from "$lib/components/user/user.svelte";
 
 export const load: PageLoad = async ({ parent }) => {
+  await validateUserOrRedirect();
   const { metadata } = await parent();
 
   // get metric names, parameters
