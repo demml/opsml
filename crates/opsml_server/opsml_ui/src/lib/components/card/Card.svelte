c8cd9ca9f9dc7c34fad01c8a25085d1548a3fa34
Thorrester <sjforrester32@gmail.com>
2025-08-08 12:02:19 -0400

    move to anchor

--- crates/opsml_server/opsml_ui/src/lib/components/card/Card.svelte
+++ crates/opsml_server/opsml_ui/src/lib/components/card/Card.svelte
@@ -23,13 +23,13 @@
   }>();
 
 
-  let cardUrl = $state(`/opsml/${registry.toLowerCase()}/card/${space}/${name}/${version}`);
+  let cardUrl = $state(`/opsml/${registry.toLowerCase()}/card/${space}/${name}/${version}/card`);
 
 
 </script>
 
 <a 
-  class="text-black rounded-base shadow border-2 border-black bg-surface-300 w-full max-w-[30em] h-[5em] overflow-auto whitespace-nowrap hover:translate-x-[4px] hover:translate-y-[4px] hover:shadow-none" 
+  class="p-2 text-black rounded-base shadow border-2 border-black bg-surface-300 w-full max-w-[30em] h-[5em] overflow-auto whitespace-nowrap hover:translate-x-[4px] hover:translate-y-[4px] hover:shadow-none" 
   href={cardUrl}
   data-sveltekit-preload-data="hover"
   >
