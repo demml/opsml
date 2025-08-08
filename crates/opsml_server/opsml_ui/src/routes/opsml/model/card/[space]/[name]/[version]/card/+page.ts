4d27ba03178cc1e977ddb3e8d151cd1a9b1b3d6c
Thorrester <sjforrester32@gmail.com>
2025-08-08 12:02:19 -0400

    move to anchor

--- crates/opsml_server/opsml_ui/src/routes/opsml/model/card/[space]/[name]/[version]/card/+page.ts
+++ crates/opsml_server/opsml_ui/src/routes/opsml/model/card/[space]/[name]/[version]/card/+page.ts
@@ -5,6 +5,7 @@ import { validateUserOrRedirect } from "$lib/components/user/user.svelte";
 
 export const load: PageLoad = async ({ parent }) => {
   await validateUserOrRedirect();
+  console.log("Loading card page...");
   const { metadata, registryType, readme, registryPath } = await parent();
 
   return { metadata, registryType, readme, registryPath };
