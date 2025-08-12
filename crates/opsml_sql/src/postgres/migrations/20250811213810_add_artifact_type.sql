cee5f391c2cc7e197d28387cf52c6e0790bc5a32
Thorrester <sjforrester32@gmail.com>
2025-08-12 09:31:35 -0400

    Update 20250811213810_add_artifact_type.sql

--- crates/opsml_sql/src/postgres/migrations/20250811213810_add_artifact_type.sql
+++ crates/opsml_sql/src/postgres/migrations/20250811213810_add_artifact_type.sql
@@ -1,3 +1,5 @@
 ALTER TABLE opsml_artifact_registry ADD COLUMN IF NOT EXISTS artifact_type TEXT NOT NULL DEFAULT 'generic';
 
 UPDATE opsml_artifact_registry SET artifact_type = 'generic' WHERE artifact_type IS NULL;
+
+CREATE INDEX idx_opsml_artifact_registry_uid_artifact_type ON opsml_artifact_registry (uid, artifact_type);
