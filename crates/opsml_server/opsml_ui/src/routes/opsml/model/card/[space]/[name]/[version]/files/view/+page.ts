43b4da3f01a8e2655f92d369de47d9e6cb49afd3
Thorrester <sjforrester32@gmail.com>
2025-08-14 10:57:33 -0400

    add figure tab for experiments

--- crates/opsml_server/opsml_ui/src/routes/opsml/model/card/[space]/[name]/[version]/files/view/+page.ts
+++ crates/opsml_server/opsml_ui/src/routes/opsml/model/card/[space]/[name]/[version]/files/view/+page.ts
@@ -4,13 +4,15 @@ export const prerender = false;
 import type { PageLoad } from "./$types";
 import { getRawFile } from "$lib/components/files/utils";
 import { validateUserOrRedirect } from "$lib/components/user/user.svelte";
+import { registry } from "chart.js";
+import { RegistryType } from "$lib/utils";
 
 export const load: PageLoad = async ({ parent, url }) => {
   await validateUserOrRedirect();
   const { metadata, registryPath } = await parent();
   const viewPath = (url as URL).searchParams.get("path") as string;
 
-  let rawFile = await getRawFile(viewPath, metadata.uid, registryPath);
+  let rawFile = await getRawFile(viewPath, metadata.uid, RegistryType.Model);
   let splitPath = viewPath.split("/");
 
   return { rawFile, viewPath, splitPath };
