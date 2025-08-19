4ac89c395457b84eedac868ae1608496feeb14f2
Thorrester <sjforrester32@gmail.com>
2025-08-19 16:06:56 -0400

    refactor service download

--- crates/opsml_registry/src/download.rs
+++ crates/opsml_registry/src/download.rs
@@ -1,6 +1,7 @@
 use crate::async_base::AsyncOpsmlRegistry;
 use crate::base::OpsmlRegistry;
 use crate::error::RegistryError;
+use opsml_cards::Card;
 use opsml_cards::ModelCard;
 use opsml_cards::PromptCard;
 use opsml_cards::ServiceCard;
@@ -198,11 +199,12 @@ pub fn download_service_from_registry(
     Ok(())
 }
 
-pub async fn async_download_service_from_registry(
+/// Downloads a service card
+async fn get_service_card(
     args: &CardQueryArgs,
     write_path: &Path,
     registry: &AsyncOpsmlRegistry,
-) -> Result<(), RegistryError> {
+) -> Result<ServiceCard, RegistryError> {
     let key = registry.get_key(args).await?;
 
     // delete directory if it exists
@@ -215,7 +217,6 @@ pub async fn async_download_service_from_registry(
 
     // read Card.json file
     let service = ServiceCard::load_service_json(write_path).map_err(RegistryError::CardError)?;
-
     let service_name = format!("{}/{}/v{}", service.space, service.name, service.version);
 
     println!(
@@ -224,81 +225,111 @@ pub async fn async_download_service_from_registry(
         Colorize::green(write_path.to_str().unwrap())
     );
 
+    Ok(service)
+}
+
+/// Downloads a card to path
+async fn download_card(
+    card: &Card,
+    write_path: &Path,
+    registry: &AsyncOpsmlRegistry,
+    current_dir: &Path,
+    mapping: &mut ServiceCardMapping,
+) -> Result<(), RegistryError> {
+    let query_args = CardQueryArgs {
+        uid: Some(card.uid.clone()),
+        name: Some(card.name.clone()),
+        space: Some(card.space.clone()),
+        version: Some(card.version.clone()),
+        registry_type: card.registry_type.clone(),
+        ..Default::default()
+    };
+
+    let key = registry.get_key(&query_args).await?;
+    let card_path = write_path
+        .strip_prefix(current_dir)
+        .unwrap_or(write_path)
+        .join(&card.alias);
+
+    // Download card artifacts
+    download_card_artifacts(&key, &card_path)?;
+    mapping.add_card_path(&card.alias, &card_path);
+
+    // check if card has drift profiles and add them to map
+    if card.registry_type == RegistryType::Model || card.registry_type == RegistryType::Prompt {
+        process_drift_paths(card, &card_path, mapping)?;
+    }
+
+    Ok(())
+}
+
+fn process_drift_paths(
+    card: &Card,
+    card_path: &Path,
+    mapping: &mut ServiceCardMapping,
+) -> Result<(), RegistryError> {
+    let card_json_path = card_path.join(SaveName::Card).with_extension(Suffix::Json);
+    let json_string = std::fs::read_to_string(&card_json_path)?;
+
+    let drift_paths = match card.registry_type {
+        RegistryType::Model => {
+            let modelcard: ModelCard = serde_json::from_str(&json_string)?;
+            modelcard
+                .metadata
+                .interface_metadata
+                .save_metadata
+                .drift_profile_uri_map
+        }
+        RegistryType::Prompt => {
+            let promptcard: PromptCard = serde_json::from_str(&json_string)?;
+            promptcard.metadata.drift_profile_uri_map
+        }
+        _ => {
+            debug!(
+                "Card {} is not a ModelCard or PromptCard, skipping drift paths",
+                card.alias
+            );
+            None
+        }
+    };
+
+    if let Some(paths) = drift_paths {
+        for (alias, path) in paths {
+            // create drift alias and path
+            // {card_path}/{uri} - uri is relative to parent card path in the profile uri map
+            let drift_path = card_path.join(path.uri);
+            mapping.add_drift_path(&alias, &drift_path);
+        }
+    } else {
+        debug!("ModelCard {} has no drift paths", card.alias);
+    }
+
+    Ok(())
+}
+
+/// Async equivalent of downloading a service from the registry
+/// # Arguments
+/// * `args` - The arguments for the card query
+/// * `write_path` - The path to write the downloaded service
+/// * `registry` - The async opsml registry
+pub async fn async_download_service_from_registry(
+    args: &CardQueryArgs,
+    write_path: &Path,
+    registry: &AsyncOpsmlRegistry,
+) -> Result<(), RegistryError> {
+    // 1. Get the ServiceCard
+    let service = get_service_card(args, write_path, registry).await?;
+
+    // 2. Create a mapping for the service card
     let mut mapping = ServiceCardMapping::new();
     let current_dir = std::env::current_dir()?;
 
-    // Download each card in the service
-    service
-        .cards
-        .iter()
-        .try_for_each(|card| -> Result<(), RegistryError> {
-            let query_args = CardQueryArgs {
-                uid: Some(card.uid.clone()),
-                name: Some(card.name.clone()),
-                space: Some(card.space.clone()),
-                version: Some(card.version.clone()),
-                registry_type: card.registry_type.clone(),
-                ..Default::default()
-            };
+    // 3. Download card
+    for card in &service.cards {
+        download_card(card, write_path, registry, &current_dir, &mut mapping).await?;
+    }
 
-            let key = registry.get_key(&query_args).await?;
-            let card_path = write_path
-                .strip_prefix(&current_dir)
-                .unwrap_or(write_path)
-                .join(&card.alias);
-
-            // Download card artifacts
-            download_card_artifacts(&key, &card_path)?;
-            mapping.add_card_path(&card.alias, &card_path);
-
-            // If model card, load and process drift paths
-            if card.registry_type == RegistryType::Model
-                || card.registry_type == RegistryType::Prompt
-            {
-                let card_json_path = card_path.join(SaveName::Card).with_extension(Suffix::Json);
-                let json_string = std::fs::read_to_string(&card_json_path)?;
-
-                let drift_paths = match card.registry_type {
-                    RegistryType::Model => {
-                        let modelcard: ModelCard = serde_json::from_str(&json_string)?;
-                        modelcard
-                            .metadata
-                            .interface_metadata
-                            .save_metadata
-                            .drift_profile_uri_map
-                    }
-                    RegistryType::Prompt => {
-                        let promptcard: PromptCard = serde_json::from_str(&json_string)?;
-                        promptcard.metadata.drift_profile_uri_map
-                    }
-                    _ => {
-                        debug!(
-                            "Card {} is not a ModelCard or PromptCard, skipping drift paths",
-                            card.alias
-                        );
-                        None
-                    }
-                };
-
-                match drift_paths {
-                    Some(paths) => {
-                        for (alias, path) in paths {
-                            // create drift alias and path
-                            // {card_path}/{uri} - uri is relative to parent card path in the profile uri map
-                            let drift_path = card_path.join(path.uri);
-                            mapping.add_drift_path(&alias, &drift_path);
-                        }
-                    }
-                    None => {
-                        debug!("ModelCard {} has no drift paths", card.alias);
-                    }
-                }
-                // Optionally: do something with drift_paths
-            }
-            Ok(())
-        })?;
-
-    // save mapping to card root dir
+    // 4. Save mapping to card root dir
     let mapping_path = write_path
         .join(SaveName::CardMap)
         .with_extension(Suffix::Json);
