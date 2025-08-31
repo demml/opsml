4bd1e8a8449bbcf09d6cbd98786fc627fbd96795
Thorrester <sjforrester32@gmail.com>
2025-08-31 14:33:12 -0400

    add loading from eval_metric

--- crates/opsml_evaluate/src/llm.rs
+++ crates/opsml_evaluate/src/llm.rs
@@ -1,39 +1,20 @@
 use core::fmt::Debug;
+use opsml_interfaces::llm::workflow::PyWorkflow;
+use opsml_types::cards::LLMEvalMetric;
 use opsml_utils::PyHelperFuncs;
 use potato_head::prompt::ResponseType;
 use potato_head::Prompt;
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use serde::{Deserialize, Serialize};
 
 use crate::error::EvaluationError;
 
-#[pyclass]
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct LLMEvalMetric {
-    #[pyo3(get, set)]
-    pub name: String,
-
-    #[pyo3(get)]
-    pub prompt: Prompt,
-}
-
-#[pymethods]
-impl LLMEvalMetric {
-    #[new]
-    #[pyo3(signature = (name, prompt))]
-    pub fn new(name: &str, prompt: Prompt) -> Result<Self, EvaluationError> {
-        // assert that the prompt is a scoring prompt
-        if prompt.response_type != ResponseType::Score {
-            return Err(EvaluationError::InvalidResponseError);
-        }
-        Ok(Self {
-            name: name.to_lowercase(),
-            prompt,
-        })
-    }
-
-    pub fn __str__(&self) -> String {
-        // serialize the struct to a string
-        PyHelperFuncs::__str__(self)
-    }
+pub fn evaluate_llm(
+    py: Python,
+    data: Option<Bound<'_, PyDict>>,
+    metrics: Vec<LLMEvalMetric>,
+) -> Result<(), EvaluationError> {
+    let workflow = PyWorkflow::from_eval_metrics(metrics, "LLM Evaluation")?;
+    Ok(())
 }
