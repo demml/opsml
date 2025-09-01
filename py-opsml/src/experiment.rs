d73dfe22228a4188fd1f945679490c8861f53091
Thorrester <sjforrester32@gmail.com>
2025-09-01 09:56:05 -0400

    add pyo3 imports

--- py-opsml/src/experiment.rs
+++ py-opsml/src/experiment.rs
@@ -1,14 +1,19 @@
+use opsml_evaluate::llm::{evaluate_llm, LLMEvalResults};
 use opsml_experiment::{
     download_artifact, get_experiment_metrics, get_experiment_parameters, start_experiment,
     Experiment,
 };
-use opsml_types::cards::experiment::{EvalMetrics, Metric, Metrics, Parameter, Parameters};
+use opsml_types::cards::experiment::{
+    EvalMetrics, LLMEvalMetric, Metric, Metrics, Parameter, Parameters,
+};
 use pyo3::prelude::*;
 
 #[pymodule]
 pub fn experiment(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Experiment>()?;
     m.add_class::<Metric>()?;
+    m.add_class::<LLMEvalMetric>()?;
+    m.add_class::<LLMEvalResults>()?;
     m.add_class::<Parameter>()?;
     m.add_class::<Parameters>()?;
     m.add_class::<Metrics>()?;
@@ -17,5 +22,6 @@ pub fn experiment(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(get_experiment_parameters, m)?)?;
     m.add_function(wrap_pyfunction!(download_artifact, m)?)?;
     m.add_function(wrap_pyfunction!(start_experiment, m)?)?;
+    m.add_function(wrap_pyfunction!(evaluate_llm, m)?)?;
     Ok(())
 }
