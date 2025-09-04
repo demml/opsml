a0523a5cc1a886da8e6644d0b16643fbcea3f006
Thorrester <sjforrester32@gmail.com>
2025-09-04 15:45:44 -0400

    update imports

--- py-opsml/python/opsml/experiment/__init__.py
+++ py-opsml/python/opsml/experiment/__init__.py
@@ -2,32 +2,40 @@
 
 from .. import experiment  # noqa: F401
 
+LLMEvalRecord = experiment.LLMEvalRecord
+evaluate_llm = experiment.evaluate_llm
+LLMEvaluator = experiment.LLMEvaluator
 Experiment = experiment.Experiment
 start_experiment = experiment.start_experiment
+LLMEvalMetric = experiment.LLMEvalMetric
+EvalResult = experiment.EvalResult
+LLMEvalResults = experiment.LLMEvalResults
 Metric = experiment.Metric
 Metrics = experiment.Metrics
-LLMEvalMetric = experiment.LLMEvalMetric
-LLMEvalResults = experiment.LLMEvalResults
 EvalMetrics = experiment.EvalMetrics
 Parameter = experiment.Parameter
 Parameters = experiment.Parameters
 get_experiment_metrics = experiment.get_experiment_metrics
 get_experiment_parameters = experiment.get_experiment_parameters
 download_artifact = experiment.download_artifact
-evaluate_llm = experiment.evaluate_llm
-LLMEvalRecord = experiment.LLMEvalRecord
+
 
 __all__ = [
+    "LLMEvalRecord",
+    "evaluate_llm",
+    "LLMEvaluator",
     "Experiment",
     "start_experiment",
+    "LLMEvalMetric",
+    "EvalResult",
+    "LLMEvalResults",
     "Metric",
+    "Metrics",
+    "EvalMetrics",
+    "LLMEvalMetric",
     "Parameter",
+    "Parameters",
     "get_experiment_metrics",
     "get_experiment_parameters",
     "download_artifact",
-    "EvalMetrics",
-    "LLMEvalMetric",
-    "LLMEvalResults",
-    "evaluate_llm",
-    "LLMEvalRecord",
 ]
