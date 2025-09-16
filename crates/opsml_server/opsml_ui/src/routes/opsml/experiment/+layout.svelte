f35967efd62c3a0d0ce33356d62b3d4261bd5463
Thorrester <sjforrester32@gmail.com>
2025-09-16 19:59:45 -0400

    experiment layout

--- crates/opsml_server/opsml_ui/src/routes/opsml/experiment/+layout.svelte
+++ crates/opsml_server/opsml_ui/src/routes/opsml/experiment/+layout.svelte
@@ -3,7 +3,7 @@
     import Sidebar from "$lib/components/nav/Sidebar.svelte";
 </script>
 
-<div class="h-full overflow-auto">
+<div class="h-full">
   <Sidebar>
       {@render children()}
   </Sidebar>
