b73c019370881b4f792888dbc46355d80f4d6639
Thorrester <sjforrester32@gmail.com>
2025-09-15 21:59:10 -0400

    update spacing

--- crates/opsml_server/opsml_ui/src/routes/opsml/model/card/[space]/[name]/[version]/+layout.svelte
+++ crates/opsml_server/opsml_ui/src/routes/opsml/model/card/[space]/[name]/[version]/+layout.svelte
@@ -17,8 +17,8 @@
 </script>
 
 <div class="h-screen flex flex-col">
-  <div class="flex-none pt-16 pb-1 border-b-2 border-black bg-slate-100">
-    <div class="flex flex-col mx-auto w-11/12 justify-start">
+  <div class="flex-none pt-2 pb-1 border-b-2 border-black bg-slate-100">
+    <div class="flex flex-col mx-auto justify-start px-4">
       <h1 class="flex flex-row flex-wrap items-center">
         <div class="group flex flex-none items-center">
           <a class="font-semibold text-black hover:text-secondary-500" href="/opsml/space/{data.metadata.space}">{data.metadata.space}</a>
