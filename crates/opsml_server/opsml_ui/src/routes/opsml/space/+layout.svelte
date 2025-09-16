b4b639c0d2e9109747870bdb1b992364bede9503
Thorrester <sjforrester32@gmail.com>
2025-09-15 20:56:41 -0400

    adjust login

---
+++ crates/opsml_server/opsml_ui/src/routes/opsml/space/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let { children } = $props();
+    import Sidebar from "$lib/components/nav/Sidebar.svelte";
+</script>
+
+<div class="h-full overflow-auto">
+  <Sidebar>
+      {@render children()}
+  </Sidebar>
+</div>
