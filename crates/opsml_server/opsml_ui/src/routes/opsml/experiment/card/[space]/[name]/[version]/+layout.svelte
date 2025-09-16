46ec55044565237522d62da46c3a6612e5a2833e
Thorrester <sjforrester32@gmail.com>
2025-09-16 15:34:56 -0400

    experiment layout

--- crates/opsml_server/opsml_ui/src/routes/opsml/experiment/card/[space]/[name]/[version]/+layout.svelte
+++ crates/opsml_server/opsml_ui/src/routes/opsml/experiment/card/[space]/[name]/[version]/+layout.svelte
@@ -16,7 +16,7 @@
 
 <div class="h-screen flex flex-col">
   <div class="sticky top-0 z-10 flex-none pt-2 pb-1 border-b-2 border-black bg-surface-100">
-    <div class="flex flex-col mx-auto w-11/12 justify-start">
+    <div class="flex flex-col mx-auto justify-start px-4">
       <h1 class="flex flex-row flex-wrap items-center">
         <div class="group flex flex-none items-center">
           <a class="font-semibold text-black hover:text-secondary-500" href="/opsml/space/{data.metadata.space}">{data.metadata.space}</a>
