8a04ba238c9856e1e232a422834a4c0474d38f02
Thorrester <sjforrester32@gmail.com>
2025-09-16 10:16:51 -0400

    sticky subheader

--- crates/opsml_server/opsml_ui/src/routes/opsml/data/card/[space]/[name]/[version]/+layout.svelte
+++ crates/opsml_server/opsml_ui/src/routes/opsml/data/card/[space]/[name]/[version]/+layout.svelte
@@ -21,7 +21,8 @@
 </script>
 
 <div class="h-screen flex flex-col">
-  <div class="flex-none pt-2 pb-1 border-b-2 border-black bg-slate-100">
+  <!-- Header with navigation tabs -->
+  <div class="sticky top-0 z-10 flex-none pt-2 pb-1 border-b-2 border-black bg-slate-100">
     <div class="flex flex-col mx-auto justify-start px-4">
       <h1 class="flex flex-row flex-wrap items-center">
         <div class="group flex flex-none items-center">
--- crates/opsml_server/opsml_ui/src/routes/opsml/experiment/card/[space]/[name]/[version]/+layout.svelte
+++ crates/opsml_server/opsml_ui/src/routes/opsml/experiment/card/[space]/[name]/[version]/+layout.svelte
@@ -16,7 +16,7 @@
 </script>
 
 <div class="h-screen flex flex-col">
-  <div class="flex-none pt-16 pb-1 border-b-2 border-black bg-slate-100">
+  <div class="sticky top-0 z-10 flex-none pt-2 pb-1 border-b-2 border-black bg-slate-100">
     <div class="flex flex-col mx-auto w-11/12 justify-start">
       <h1 class="flex flex-row flex-wrap items-center">
         <div class="group flex flex-none items-center">
