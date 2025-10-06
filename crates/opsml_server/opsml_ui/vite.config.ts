883dd24f883033829220b7b992d0bbb5be0c9fca
Thorrester <sjforrester32@gmail.com>
2025-10-06 15:05:37 -0400

    Update vite.config.ts

--- crates/opsml_server/opsml_ui/vite.config.ts
+++ crates/opsml_server/opsml_ui/vite.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from "@tailwindcss/vite";
-import { svelteTesting } from "@testing-library/svelte/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
