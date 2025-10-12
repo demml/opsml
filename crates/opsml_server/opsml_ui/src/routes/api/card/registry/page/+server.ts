58616b452fe7ad40ba6d4747cf61034941b484b7
Thorrester <sjforrester32@gmail.com>
2025-10-12 09:10:13 -0400

    update uid routes

--- crates/opsml_server/opsml_ui/src/routes/api/card/registry/page/+server.ts
+++ crates/opsml_server/opsml_ui/src/routes/api/card/registry/page/+server.ts
@@ -1,14 +1,12 @@
 import type { RequestHandler } from "./$types";
 import { getRegistryPage } from "$lib/server/card/utils";
 import { json } from "@sveltejs/kit";
-import { logger } from "$lib/server/logger";
 
 /** Get Registry Page Data
  */
-export const POST: RequestHandler = async ({ request, cookies, fetch }) => {
+export const POST: RequestHandler = async ({ request, fetch }) => {
   const { registry_type, sort_by, spaces, searchTerm, tags, page } =
     await request.json();
-  const jwt_token = cookies.get("jwt_token");
   const response = await getRegistryPage(
     fetch,
     registry_type,
