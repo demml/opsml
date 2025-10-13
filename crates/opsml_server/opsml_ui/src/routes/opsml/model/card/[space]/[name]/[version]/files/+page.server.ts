1d94a5bf16d82f3abefb3334632330b35028217c
Thorrester <sjforrester32@gmail.com>
2025-10-13 10:09:38 -0400

    refactor files

--- crates/opsml_server/opsml_ui/src/routes/opsml/model/card/[space]/[name]/[version]/files/+page.server.ts
+++ crates/opsml_server/opsml_ui/src/routes/opsml/model/card/[space]/[name]/[version]/files/+page.server.ts
@@ -1,17 +1,14 @@
-export const ssr = false;
-
-import type { PageLoad } from "./$types";
-import { getFileTree } from "$lib/components/files/utils";
+import type { PageServerLoad } from "./$types";
+import { getFileTree } from "$lib/server/card/files/utils";
 import { getRegistryTableName } from "$lib/utils";
-import { validateUserOrRedirect } from "$lib/components/user/user.svelte";
 
-export const load: PageLoad = async ({ parent }) => {
+export const load: PageServerLoad = async ({ parent, fetch }) => {
   const { metadata, registryType } = await parent();
 
   let tableName = getRegistryTableName(registryType);
   let basePath = `${tableName}/${metadata.space}/${metadata.name}/v${metadata.version}`;
 
-  let fileTree = await getFileTree(basePath);
+  let fileTree = await getFileTree(basePath, fetch);
 
   return { fileTree, previousPath: basePath, isRoot: true };
 };
