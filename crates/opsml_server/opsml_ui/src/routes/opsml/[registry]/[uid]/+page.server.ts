22bb560cb342c8d19271f5f7808324249a82eb81
Thorrester <sjforrester32@gmail.com>
2025-10-15 19:12:01 -0400

    rename to registry

--- crates/opsml_server/opsml_ui/src/routes/opsml/[registry]/[uid]/+page.server.ts
+++ crates/opsml_server/opsml_ui/src/routes/opsml/[registry]/[uid]/+page.server.ts
@@ -1,22 +1,27 @@
 import { redirect } from "@sveltejs/kit";
-import type { PageLoad } from "./$types";
+import type { PageServerLoad } from "./$types";
 import { getRegistryTypeLowerCase } from "$lib/utils";
 import type { ServiceCard } from "$lib/components/card/card_interfaces/servicecard";
 import { getCardMetadata } from "$lib/server/card/utils";
+import type { BaseCard } from "$lib/components/home/types";
 
-export const load: PageLoad = async ({ parent, params, fetch }) => {
+export const load: PageServerLoad = async ({ parent, params, fetch }) => {
   let { registryType } = await parent();
 
-  let resp = await getCardMetadata(
+  console.log("uid param:", params.uid);
+
+  if (!registryType) {
+    throw redirect(307, "/opsml/home");
+  }
+
+  let metadata = (await getCardMetadata(
     undefined,
     undefined,
     undefined,
     params.uid,
     registryType,
     fetch
-  );
-
-  let metadata = (await resp.json()) as ServiceCard;
+  )) as BaseCard;
 
   redirect(
     301,
