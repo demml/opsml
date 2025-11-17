784e08adc165239fe57e6bfdad45f6bf368fadfc
Thorrester <sjforrester32@gmail.com>
2025-11-17 18:37:47 -0500

    no options

--- crates/opsml_interfaces/src/data/base/pandas.rs
+++ crates/opsml_interfaces/src/data/base/pandas.rs
@@ -262,8 +262,8 @@ impl PandasData {
             py,
             self_.data.as_ref().unwrap().bind(py),
             data_type,
-            bin_size,
-            compute_correlations,
+            Some(bin_size),
+            Some(compute_correlations),
         )?;
 
         self_.as_super().data_profile = Some(profile.clone());
