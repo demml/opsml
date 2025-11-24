45b0ccc5ff15cfe1e9f3207c3230976427ded754
Thorrester <sjforrester32@gmail.com>
2025-11-24 13:41:46 -0500

    Delete test_scratch.py

--- py-opsml/test_scratch.py
+++
@@ -1,7 +0,0 @@
-from opsml.card import Card
-
-
-def test_card_creation():
-    card = Card(name="test_card", space="test_space")
-    assert card.name == "test_card"
-    assert card.space == "test_space"
