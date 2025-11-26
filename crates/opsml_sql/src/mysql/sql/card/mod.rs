c0378dc77dc24bf794b37dc84b2e32a1a7db2f8d
Thorrester <sjforrester32@gmail.com>
2025-11-26 15:26:54 -0500

    update card

--- crates/opsml_sql/src/mysql/sql/card/mod.rs
+++ crates/opsml_sql/src/mysql/sql/card/mod.rs
@@ -447,6 +447,7 @@ impl CardLogicTrait for CardLogicMySqlClient {
                         .bind(&record.build_tag)
                         .bind(&record.username)
                         .bind(&record.opsml_version)
+                        .bind(&record.status)
                         .bind(&record.uid)
                         .execute(&self.pool)
                         .await?;
