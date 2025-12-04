5d81f79b3d9c0dc53528a8e377d80c654fa52b23
Thorrester <sjforrester32@gmail.com>
2025-12-04 15:54:36 -0500

    mysql update

--- crates/opsml_sql/src/mysql/sql/experiment/get_experiment_metric.sql
+++ crates/opsml_sql/src/mysql/sql/experiment/get_experiment_metric.sql
@@ -1,1 +1,7 @@
-SELECT * FROM opsml_experiment_metric WHERE experiment_uid = ?
+SELECT 
+    *
+FROM opsml_experiment_metric 
+WHERE experiment_uid = ?
+    AND (? IS NULL OR FIND_IN_SET(name, ?))
+    AND (? IS NULL OR is_eval = ?)
+ORDER BY name ASC, COALESCE(step, 999999) ASC, created_at ASC
