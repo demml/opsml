ae54ec5eacde1cae31deafa9a861e933fb035a4d
Thorrester <sjforrester32@gmail.com>
2025-12-04 16:06:35 -0500

    sorting experiment and hardware metrics

--- crates/opsml_sql/src/sqlite/sql/experiment/get_experiment_metric.sql
+++ crates/opsml_sql/src/sqlite/sql/experiment/get_experiment_metric.sql
@@ -1,1 +1,7 @@
-SELECT * FROM opsml_experiment_metric WHERE experiment_uid = ?
+SELECT 
+    *
+FROM opsml_experiment_metric 
+WHERE experiment_uid = ?
+    AND (? IS NULL OR name IN (SELECT value FROM json_each(?)))
+    AND (? IS NULL OR is_eval = ?)
+ORDER BY name ASC, COALESCE(step, 999999) ASC, created_at ASC
