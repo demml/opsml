5c8e56eeedeb73b17a0f3ac4f5ae2855a58fa569
Thorrester <sjforrester32@gmail.com>
2026-01-14 10:31:31 -0500

    adding genai mock tests

--- crates/opsml_server/tests/api/scouter/profile.rs
+++ crates/opsml_server/tests/api/scouter/profile.rs
@@ -15,8 +15,7 @@ use opsml_types::{
 use rand::Rng;
 use reqwest::header;
 use scouter_client::{
-    DriftRequest, DriftType, ProfileRequest, ProfileStatusRequest, SpcDriftProfile, TimeInterval,
-    VersionRequest,
+    DriftType, ProfileRequest, ProfileStatusRequest, SpcDriftProfile, VersionRequest,
 };
 use std::path::PathBuf;
 
@@ -150,75 +149,6 @@ async fn test_scouter_routes_update_profile() {
         .method("PUT")
         .header(header::CONTENT_TYPE, "application/json")
         .body(Body::from(body))
-        .unwrap();
-
-    let response = helper.send_oneshot(request).await;
-    assert_eq!(response.status(), StatusCode::OK);
-}
-
-#[tokio::test]
-async fn test_scouter_routes_spc_drift_features() {
-    let helper = TestHelper::new(None).await;
-
-    let drift_request = DriftRequest {
-        space: helper.space.clone(),
-        uid: "mocked_uid".to_string(),
-        time_interval: TimeInterval::OneHour,
-        max_data_points: 100,
-        ..Default::default()
-    };
-
-    let query_string = serde_qs::to_string(&drift_request).unwrap();
-    let request = Request::builder()
-        .uri(format!("/opsml/api/scouter/drift/spc?{query_string}"))
-        .method("GET")
-        .body(Body::empty())
-        .unwrap();
-
-    let response = helper.send_oneshot(request).await;
-    assert_eq!(response.status(), StatusCode::OK);
-}
-
-#[tokio::test]
-async fn test_scouter_routes_psi_drift_features() {
-    let helper = TestHelper::new(None).await;
-
-    let drift_request = DriftRequest {
-        uid: "mocked_uid".to_string(),
-        space: helper.space.clone(),
-        time_interval: TimeInterval::OneHour,
-        max_data_points: 100,
-        ..Default::default()
-    };
-
-    let query_string = serde_qs::to_string(&drift_request).unwrap();
-    let request = Request::builder()
-        .uri(format!("/opsml/api/scouter/drift/psi?{query_string}"))
-        .method("GET")
-        .body(Body::empty())
-        .unwrap();
-
-    let response = helper.send_oneshot(request).await;
-    assert_eq!(response.status(), StatusCode::OK);
-}
-
-#[tokio::test]
-async fn test_scouter_routes_custom_drift_features() {
-    let helper = TestHelper::new(None).await;
-
-    let drift_request = DriftRequest {
-        space: helper.space.clone(),
-        uid: "mocked_uid".to_string(),
-        time_interval: TimeInterval::OneHour,
-        max_data_points: 100,
-        ..Default::default()
-    };
-
-    let query_string = serde_qs::to_string(&drift_request).unwrap();
-    let request = Request::builder()
-        .uri(format!("/opsml/api/scouter/drift/custom?{query_string}"))
-        .method("GET")
-        .body(Body::empty())
         .unwrap();
 
     let response = helper.send_oneshot(request).await;
