b6f09fb2602b8daa34f335f63b726d6df2a82572
Thorrester <sjforrester32@gmail.com>
2026-02-24 10:23:43 -0500

    trace dashboard

--- crates/opsml_server/opsml_ui/src/lib/components/trace/TraceDetailSidebar.svelte
+++ crates/opsml_server/opsml_ui/src/lib/components/trace/TraceDetailSidebar.svelte
@@ -33,7 +33,7 @@
 
 <!-- Backdrop -->
 <div
-  class="fixed inset-0 bg-opacity-30 z-40 transition-opacity duration-300"
+  class="fixed inset-x-0 bottom-0 top-14 bg-black/30 z-40 transition-opacity duration-300"
   class:opacity-0={isClosing}
   onclick={handleClose}
   onkeydown={(e) => e.key === 'Escape' && handleClose()}
@@ -44,7 +44,7 @@
 
 <!-- Side Panel -->
 <div
-  class="fixed top-0 right-0 h-full w-full lg:w-4/5 xl:w-3/4 bg-white border-l-4 border-black shadow-2xl z-50 flex flex-col transition-transform duration-300"
+  class="fixed top-14 right-0 h-[calc(100%-3.5rem)] w-full lg:w-4/5 xl:w-3/4 bg-white border-l-4 border-black shadow-2xl z-50 flex flex-col transition-transform duration-300"
   class:translate-x-full={isClosing}
 >
   <TraceDetailContent {trace} {traceSpans} onClose={handleClose} showCloseButton={true} />
