4b9a83aeb97d6d64f678f460b678a5faae48892b
Thorrester <sjforrester32@gmail.com>
2026-02-25 10:07:32 -0500

    update file ui

--- crates/opsml_server/opsml_ui/src/lib/components/files/ImageViewer.svelte
+++ crates/opsml_server/opsml_ui/src/lib/components/files/ImageViewer.svelte
@@ -7,10 +7,12 @@
 </script>
 
 
-<div class="flex justify-center p-4">
-  <img 
-    src={`data:${mimeType};base64,${content}`} 
-    alt="File preview"
-    class="max-w-full h-auto rounded-lg"
-  />
+<div class="flex justify-center p-6">
+  <div class="rounded-base border-2 border-black shadow-small overflow-hidden inline-block">
+    <img 
+      src={`data:${mimeType};base64,${content}`} 
+      alt="File preview"
+      class="max-w-full h-auto block"
+    />
+  </div>
 </div>
