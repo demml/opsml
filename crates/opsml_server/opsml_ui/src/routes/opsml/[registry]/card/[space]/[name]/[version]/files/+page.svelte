0611223a91cadd8f77c299d5acf30090cec72a14
Thorrester <sjforrester32@gmail.com>
2026-03-03 19:30:42 -0500

    refactor file view

--- crates/opsml_server/opsml_ui/src/routes/opsml/[registry]/card/[space]/[name]/[version]/files/+page.svelte
+++ crates/opsml_server/opsml_ui/src/routes/opsml/[registry]/card/[space]/[name]/[version]/files/+page.svelte
@@ -1,24 +1,20 @@
 <script lang="ts">
-  import FileTree from '$lib/components/files/FileTree.svelte'
+  import FileExplorer from '$lib/components/files/FileExplorer.svelte';
   import type { PageProps } from './$types';
-  import { page } from '$app/state';
 
   let { data }: PageProps = $props();
+</script>
 
- 
-  </script>
-  
-  <div class="mx-auto w-9/12 px-4 py-4 max-h-[calc(100vh-140px)]">
-    <div class="w-full">
-      <FileTree 
-        files={data.fileTree.files} 
-        currentPath={page.url.pathname} 
-        previousPath={data.previousPath} 
-        space={data.metadata.space}
-        name={data.metadata.name}
-        version={data.metadata.version}
-        isRoot={data.isRoot}
-        registryType={data.registryType}
-        />
-    </div>  
-  </div>
+<div class="mx-auto px-4 py-4 pb-2 w-full">
+  <FileExplorer
+    initialTree={data.fileTree.files}
+    rawFile={data.rawFile}
+    viewPath={data.viewPath}
+    basePath={data.basePath}
+    uid={data.metadata.uid}
+    registryType={data.registryType}
+    space={data.metadata.space}
+    name={data.metadata.name}
+    version={data.metadata.version}
+  />
+</div>
