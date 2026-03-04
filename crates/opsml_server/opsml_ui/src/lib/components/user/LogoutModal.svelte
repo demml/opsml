2dad5982efed3a4f57a1fc2129403fe299467fdd
Thorrester <sjforrester32@gmail.com>
2026-03-03 20:39:34 -0500

    spacing

--- crates/opsml_server/opsml_ui/src/lib/components/user/LogoutModal.svelte
+++ crates/opsml_server/opsml_ui/src/lib/components/user/LogoutModal.svelte
@@ -13,8 +13,8 @@
 
 
   <div class="modal block overflow-y-auto bg-surface-100-800-token w-fit h-auto p-4 space-y-4 rounded-container-token shadow-xl">
-    <header class="modal-header text-2xl font-bold text-darkpurple">Usage</header> 
-    <article class="modal-body max-h-[200px] overflow-hidden">Are you sure you want to sign out of your account?</article> 
+    <header class="modal-header text-2xl font-bold text-darkpurple">Usage</header>
+    <article class="modal-body max-h-[200px] overflow-hidden">Are you sure you want to sign out of your account?</article>
     <footer class="modal-footer flex justify-end space-x-2">
       <button class="btn variant-filled" onclick={logOutHandler}>
         Logout
